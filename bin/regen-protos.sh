4bb308c80c6e7fc1588e60d93ea7ffd11144c8c8
Kevin Hester <kevinh@geeksville.com>
2020-03-02 09:56:21 -0800

    Pull protobufs from submodule instead of a symbolic link Fix #2

--- bin/regen-protos.sh
+++ bin/regen-protos.sh
@@ -1,1 +1,1 @@
-/home/kevinh/packages/nanopb-0.4.1-linux-x86/generator-bin/protoc --nanopb_out=-v:src -I=../MeshUtil/app/src/main/proto mesh.proto
+/home/kevinh/packages/nanopb-0.4.1-linux-x86/generator-bin/protoc --nanopb_out=-v:src -I=proto mesh.proto
