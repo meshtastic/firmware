d1b7b6c5c547eb27a70bf30aa38ee771c978b13e
geeksville <kevinh@geeksville.com>
2020-03-04 15:56:37 -0800

    oops - I was previously stopping bluetooth before stopping the mesh bluetooth service
    which was bad bad

--- src/MeshBluetoothService.h
+++ src/MeshBluetoothService.h
@@ -12,8 +12,4 @@ void destroyMeshBluetoothService();
  */
 void bluetoothNotifyFromNum(uint32_t newValue);
 
-/**
- * Super skanky FIXME - when we start a software update we force the mesh service to shutdown.
- * If the sw update fails, the user will have to manually reset the board to get things running again.
- */
 void stopMeshBluetoothService();
