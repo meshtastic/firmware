077fe71f8a5fdea6e734187b68b3af634aae61a0
geeksville <kevinh@geeksville.com>
2020-04-12 10:54:27 -0700

    fix bin paths

--- bin/program-release-tbeam.sh
+++ bin/program-release-tbeam.sh
@@ -3,4 +3,4 @@ set -e
 
 source bin/version.sh
 
-esptool.py --baud 921600 write_flash 0x10000 release/latest/firmware-TBEAM-US-$VERSION.bin
+esptool.py --baud 921600 write_flash 0x10000 release/latest/bins/firmware-tbeam-US-$VERSION.bin
