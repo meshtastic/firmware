e8e0418266a4bf865ef3878136590859641b234b
geeksville <kevinh@geeksville.com>
2020-05-19 14:54:47 -0700

    one hop reliable ready for testing

--- src/mesh/ReliableRouter.h
+++ src/mesh/ReliableRouter.h
@@ -2,6 +2,54 @@
 
 #include "FloodingRouter.h"
 #include "PeriodicTask.h"
+#include <unordered_map>
+
+/**
+ * An identifier for a globalally unique message - a pair of the sending nodenum and the packet id assigned
+ * to that message
+ */
+struct GlobalPacketId {
+    NodeNum node;
+    PacketId id;
+
+    bool operator==(const GlobalPacketId &p) const { return node == p.node && id == p.id; }
+
+    GlobalPacketId(const MeshPacket *p)
+    {
+        node = p->from;
+        id = p->id;
+    }
+
+    GlobalPacketId(NodeNum _from, PacketId _id)
+    {
+        node = _from;
+        id = _id;
+    }
+};
+
+/**
+ * A packet queued for retransmission
+ */
+struct PendingPacket {
+    MeshPacket *packet;
+
+    /** The next time we should try to retransmit this packet */
+    uint32_t nextTxMsec;
+
+    /** Starts at NUM_RETRANSMISSIONS -1(normally 3) and counts down.  Once zero it will be removed from the list */
+    uint8_t numRetransmissions;
+
+    PendingPacket() {}
+    PendingPacket(MeshPacket *p);
+
+    void setNextTx() { nextTxMsec = millis() + random(10 * 1000, 12 * 1000); }
+};
+
+class GlobalPacketIdHashFunction
+{
+  public:
+    size_t operator()(const GlobalPacketId &p) const { return (hash<NodeNum>()(p.node)) ^ (hash<PacketId>()(p.id)); }
+};
 
 /**
  * This is a mixin that extends Router with the ability to do (one hop only) reliable message sends.
@@ -9,6 +57,8 @@
 class ReliableRouter : public FloodingRouter
 {
   private:
+    unordered_map<GlobalPacketId, PendingPacket, GlobalPacketIdHashFunction> pending;
+
   public:
     /**
      * Constructor
@@ -50,6 +100,7 @@ class ReliableRouter : public FloodingRouter
      * Stop any retransmissions we are doing of the specified node/packet ID pair
      */
     void stopRetransmission(NodeNum from, PacketId id);
+    void stopRetransmission(GlobalPacketId p);
 
     /**
      * Add p to the list of packets to retransmit occasionally.  We will free it once we stop retransmitting.
