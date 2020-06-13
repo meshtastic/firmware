8caa075bc60fda49e149c936457a797ea848de9d
geeksville <kevinh@geeksville.com>
2020-06-13 11:05:36 -0700

    used fixed pool allocator for now - since that's how we've been testing

--- src/mesh/Router.cpp
+++ src/mesh/Router.cpp
@@ -24,8 +24,8 @@
     (MAX_RX_TOPHONE + MAX_RX_FROMRADIO + 2 * MAX_TX_QUEUE +                                                                      \
      2) // max number of packets which can be in flight (either queued from reception or queued for sending)
 
-// static MemoryPool<MeshPacket> staticPool(MAX_PACKETS);
-static MemoryDynamic<MeshPacket> staticPool;
+static MemoryPool<MeshPacket> staticPool(MAX_PACKETS);
+// static MemoryDynamic<MeshPacket> staticPool;
 
 Allocator<MeshPacket> &packetPool = staticPool;
 
