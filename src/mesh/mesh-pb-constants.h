f35b15b09c2c29b37dfcdf43df8f21b89034894f
geeksville <kevinh@geeksville.com>
2020-06-16 15:26:30 -0700

    Shrink devicestate for small mcus

--- src/mesh/mesh-pb-constants.h
+++ src/mesh/mesh-pb-constants.h
@@ -8,7 +8,9 @@
 #define member_size(type, member) sizeof(((type *)0)->member)
 
 /// max number of packets which can be waiting for delivery to android - note, this value comes from mesh.options protobuf
-#define MAX_RX_TOPHONE (member_size(DeviceState, receive_queue) / member_size(DeviceState, receive_queue[0]))
+// FIXME - max_count is actually 32 but we save/load this as one long string of preencoded MeshPacket bytes - not a big array in RAM
+// #define MAX_RX_TOPHONE (member_size(DeviceState, receive_queue) / member_size(DeviceState, receive_queue[0]))
+#define MAX_RX_TOPHONE 32
 
 /// max number of nodes allowed in the mesh
 #define MAX_NUM_NODES (member_size(DeviceState, node_db) / member_size(DeviceState, node_db[0]))
