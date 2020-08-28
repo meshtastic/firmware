22beabff21c299bcbb259da5a4316e8bfd2c5abc
geeksville <kevinh@geeksville.com>
2020-08-28 15:33:33 -0700

    TFT kinda correct now - but slow because of bit banging

--- src/graphics/TFT.cpp
+++ src/graphics/TFT.cpp
@@ -1,25 +1,33 @@
 #include "configuration.h"
 
 #ifdef ST7735_CS
-
 #include "TFT.h"
+#include "graphics/configs.h"
 #include <SPI.h>
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
 
-
 TFTDisplay::TFTDisplay(uint8_t address, int sda, int scl)
 {
-    setGeometry(
-        GEOMETRY_128_64); // FIXME - currently we lie and claim 128x64 because I'm not yet sure other resolutions will work
+    setGeometry(GEOMETRY_128_64); // FIXME - currently we lie and claim 128x64 because I'm not yet sure other resolutions will
+                                  // work ie GEOMETRY_RAWMODE
 }
 
 // Write the buffer to the display memory
 void TFTDisplay::display(void)
 {
     // FIXME - only draw bits have changed (use backbuf similar to the other displays)
-    tft.drawBitmap(0, 0, buffer, 128, 64, TFT_YELLOW, TFT_BLACK);
+    // tft.drawBitmap(0, 0, buffer, 128, 64, TFT_YELLOW, TFT_BLACK);
+    for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
+        for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
+
+            // get src pixel in the page based ordering the OLED lib uses FIXME, super inefficent
+            auto b = buffer[x + (y / 8) * SCREEN_WIDTH];
+            auto isset = b & (1 << (y & 7));
+            tft.drawPixel(x, y, isset ? TFT_WHITE : TFT_BLACK);
+        }
+    }
 }
 
 // Send a command to the display (low level function)
@@ -41,7 +49,7 @@ bool TFTDisplay::connect()
 
     tft.init();
     tft.setRotation(3); // Orient horizontal and wide underneath the silkscreen name label
-    tft.fillScreen(TFT_BLUE);
+    // tft.fillScreen(TFT_BLUE);
     // tft.drawRect(0, 0, 40, 10, TFT_PURPLE); // wide rectangle in upper left
 
     return true;
