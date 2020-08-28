338445d175d69290a313c8aed6c8982b1fe854c1
geeksville <kevinh@geeksville.com>
2020-08-28 14:24:22 -0700

    basic init of the Corvus TFT display works

--- src/graphics/TFT.cpp
+++ src/graphics/TFT.cpp
@@ -9,9 +9,16 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
 
 void TFTinit()
 {
+    DEBUG_MSG("Doing TFT init\n");
+
+#ifdef ST7735_BACKLIGHT_EN
+    digitalWrite(ST7735_BACKLIGHT_EN, HIGH);
+    pinMode(ST7735_BACKLIGHT_EN, OUTPUT);
+#endif
+
     tft.init();
     tft.setRotation(1);
-    tft.fillScreen(TFT_BLUE);
+    tft.fillScreen(TFT_GREEN);
 }
 
 #endif
