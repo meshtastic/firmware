94e4b301259840e1d43966367e0fefc3bb468021
geeksville <kevinh@geeksville.com>
2020-09-06 16:07:32 -0700

    add crude sim getmacaddr

--- src/portduino/PortduinoGlue.cpp
+++ src/portduino/PortduinoGlue.cpp
@@ -4,9 +4,21 @@
 
 // FIXME - move getMacAddr/setBluetoothEnable into a HALPlatform class
 
+uint32_t hwId; // fixme move into portduino
+
 void getMacAddr(uint8_t *dmac)
 {
-    notImplemented("getMacAddr");
+    if (!hwId) {
+        notImplemented("getMacAddr");
+        hwId = random();
+    }
+
+    dmac[0] = 0x80;
+    dmac[1] = 0;
+    dmac[2] = 0;
+    dmac[3] = hwId >> 16;
+    dmac[4] = hwId >> 8;
+    dmac[5] = hwId & 0xff;
 }
 
 void setBluetoothEnable(bool on)
