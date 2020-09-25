afd8659d88b6b9dc319eba53aeabc88c4f9589fc
geeksville <kevinh@geeksville.com>
2020-09-25 12:52:08 -0700

    move region into userpreferences, to end region specific firmwares

--- src/mesh/mesh.pb.c
+++ src/mesh/mesh.pb.c
@@ -27,7 +27,7 @@ PB_BIND(MeshPacket, MeshPacket, 2)
 PB_BIND(ChannelSettings, ChannelSettings, AUTO)
 
 
-PB_BIND(RadioConfig, RadioConfig, 2)
+PB_BIND(RadioConfig, RadioConfig, AUTO)
 
 
 PB_BIND(RadioConfig_UserPreferences, RadioConfig_UserPreferences, 2)
@@ -52,6 +52,7 @@ PB_BIND(ToRadio, ToRadio, 2)
 
 
 PB_BIND(ManufacturingData, ManufacturingData, AUTO)
+
 
 
 
