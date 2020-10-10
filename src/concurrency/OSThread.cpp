8949b3a47b90e815c248e6d8edae5b06f0a46af8
Kevin Hester <kevinh@geeksville.com>
2020-10-10 08:28:00 +0800

    concurrency wip

--- src/concurrency/OSThread.cpp
+++ src/concurrency/OSThread.cpp
@@ -4,4 +4,37 @@
 namespace concurrency
 {
 
+ThreadController mainController, timerController;
+
+void OSThread::setup()
+{
+    mainController.ThreadName = "mainController";
+    timerController.ThreadName = "timerController";
+}
+
+OSThread::OSThread(const char *_name, uint32_t period, ThreadController *_controller)
+    : Thread(NULL, period), controller(_controller)
+{
+    ThreadName = _name;
+
+    if (controller)
+        controller->add(this);
+}
+
+OSThread::~OSThread()
+{
+    if (controller)
+        controller->remove(this);
+}
+
+void OSThread::run()
+{
+    auto newDelay = runOnce();
+
+    runned();
+
+    if (newDelay != 0)
+        setInterval(newDelay);
+}
+
 } // namespace concurrency
