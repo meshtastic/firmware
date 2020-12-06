8f5a1f19d3af4fb1d268b13d94c4c09661e156c1
Kevin Hester <kevinh@geeksville.com>
2020-12-06 18:33:42 +0800

    add remote gpio control as an example plugin https://github.com/meshtastic/Meshtastic-device/issues/182

--- bin/regen-protos.sh
+++ bin/regen-protos.sh
@@ -5,7 +5,7 @@ set -e
 echo "This script requires https://jpa.kapsi.fi/nanopb/download/ version 0.4.1"
 # the nanopb tool seems to require that the .options file be in the current directory!
 cd proto
-../../nanopb-0.4.1-linux-x86/generator-bin/protoc --nanopb_out=-v:../src/mesh -I=../proto portnums.proto mesh.proto
+../../nanopb-0.4.1-linux-x86/generator-bin/protoc --nanopb_out=-v:../src/mesh -I=../proto *.proto
 
 echo "Regenerating protobuf documentation - if you see an error message"
 echo "you can ignore it unless doing a new protobuf release to github."
