5d70c972d35c3accf5cf7d9c92cc040e449696a6
Kevin Hester <kevinh@geeksville.com>
2020-12-10 12:44:35 +0800

    fix CI build - make projectdir relative

--- bin/platformio-custom.py
+++ bin/platformio-custom.py
@@ -1,15 +1,13 @@
+
+Import("projenv")
+
 import configparser
-
 config = configparser.RawConfigParser()
-config.read('version.properties')
-
+config.read(projenv["PROJECT_DIR"] + "/version.properties")
 version = dict(config.items('VERSION'))
-
 verStr = "{}.{}.{}".format(version["major"], version["minor"], version["build"])
 
 print(f"Using meshtastic platform-custom.py, firmare version {verStr}")
-
-Import("env", "projenv")
 
 # General options that are passed to the C and C++ compilers
 projenv.Append(CCFLAGS=[
