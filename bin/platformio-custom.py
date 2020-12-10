99c8df8e7d5f32459ac0f38c8cfd6d58ef6bea15
Kevin Hester <kevinh@geeksville.com>
2020-12-10 13:17:43 +0800

    try to fix CI again

--- bin/platformio-custom.py
+++ bin/platformio-custom.py
@@ -2,10 +2,16 @@
 Import("projenv")
 
 import configparser
-config = configparser.RawConfigParser()
-config.read(projenv["PROJECT_DIR"] + "/version.properties")
-version = dict(config.items('VERSION'))
-verStr = "{}.{}.{}".format(version["major"], version["minor"], version["build"])
+prefsLoc = projenv["PROJECT_DIR"] + "/version.properties"
+print(f"Preferences in {prefsLoc}")
+try:
+	config = configparser.RawConfigParser()
+	config.read(prefsLoc)
+	version = dict(config.items('VERSION'))
+	verStr = "{}.{}.{}".format(version["major"], version["minor"], version["build"])
+except:
+	print("Can't read preferences, using 0.0.0")
+	verStr = "0.0.0"
 
 print(f"Using meshtastic platform-custom.py, firmare version {verStr}")
 
