c361c1fab7d3de4118d611cc8b163d422608d17d
Kevin Hester <kevinh@geeksville.com>
2020-12-11 08:31:41 +0800

    Update to nanopb 0.4.4

--- bin/regen-protos.sh
+++ bin/regen-protos.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-echo "This script requires https://jpa.kapsi.fi/nanopb/download/ version 0.4.1"
+echo "This script requires https://jpa.kapsi.fi/nanopb/download/ version 0.4.4 to be located in the"
+echo "meshtastic-device root directory if the following step fails, you should download the correct"
+echo "prebuilt binaries for your computer into nanopb-0.4.4"
+
 # the nanopb tool seems to require that the .options file be in the current directory!
 cd proto
-../../nanopb-0.4.1-linux-x86/generator-bin/protoc --nanopb_out=-v:../src/mesh -I=../proto *.proto
+../nanopb-0.4.4/generator-bin/protoc --nanopb_out=-v:../src/mesh -I=../proto *.proto
 
 echo "Regenerating protobuf documentation - if you see an error message"
 echo "you can ignore it unless doing a new protobuf release to github."
