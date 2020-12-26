8d04410f4582f5752cbf5d900ce2f66f26dd0d26
Kevin Hester <kevinh@geeksville.com>
2020-12-26 13:55:59 +0800

    improve error descriptions

--- src/error.h
+++ src/error.h
@@ -9,15 +9,23 @@
  */
 enum CriticalErrorCode { 
     NoError = 0, 
+
+    /// A software bug was detected while trying to send lora packets
     ErrTxWatchdog = 1, 
+
+    /// A software bug was detected on entry to sleep
     ErrSleepEnterWait = 2, 
 
-    // No Lora radio hardware could be found
+    /// No Lora radio hardware could be found
     ErrNoRadio = 3, 
+
+    /// Not normally used
     ErrUnspecified = 4, 
+
+    /// We failed while configuring a UBlox GPS
     ErrUBloxInitFailed = 5,
 
-    // This board was expected to have a power management chip and it is missing or broken
+    /// This board was expected to have a power management chip and it is missing or broken
     ErrNoAXP192 = 6 
     };
 
