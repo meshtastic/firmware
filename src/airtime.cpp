ded2b86e55fe86545390f9de054475e2c8b31f93
Jm Casler <jm@casler.org>
2020-12-25 16:10:38 -0800

    Calculate TX air time duty cycles #588 -- UNTESTED

--- src/airtime.cpp
+++ src/airtime.cpp
@@ -1,31 +1,80 @@
-/*
-We will keep hourly stats for 48 hours and daily stats for 7 days
+#include "airtime.h"
+#include <Arduino.h>
 
-3600 seconds in an hour, uint16_t per hour
-86400 seconds in a day, uint32_t per day
+#define hoursToLog 48
 
-Memory to store 48 hours = 96 bytes
-Memory to store 7 days = 28 bytes
+// Here for convience and to avoid magic numbers.
+uint16_t secondsPerHour = 3600;
 
+// Don't read out of this directly. Use the helper functions.
+struct airtimeStruct {
+    uint16_t hourTX[hoursToLog];
+    uint16_t hourRX[hoursToLog];
+    uint16_t hourRX_ALL[hoursToLog];
+    uint8_t lastHourIndex;
+} airtimes;
 
-*/
-
-void logTXairtime(uint32_t airtime_ms)
+void logAirtime(reportTypes reportType, uint32_t airtime_ms)
 {
-    /*
+    currentHourIndexReset();
 
-    How many hours since power up?
-
-    Millis / 1000 / 3600
-
-
-
-    */
+    if (reportType == TX_LOG) {
+        airtimes.hourTX[currentHourIndex()] = airtimes.hourTX[currentHourIndex()] + round(airtime_ms / 1000);
+    } else if (reportType == RX_LOG) {
+        airtimes.hourRX[currentHourIndex()] = airtimes.hourRX[currentHourIndex()] + round(airtime_ms / 1000);
+    } else if (reportType == RX_ALL_LOG) {
+        airtimes.hourRX_ALL[currentHourIndex()] = airtimes.hourRX_ALL[currentHourIndex()] + round(airtime_ms / 1000);
+    } else {
+        // Unknown report type
+        
+    }
 }
 
-void logRXairtime()
+// This will let us easily switch away from using millis at some point.
+//  todo: Don't use millis, instead maintain our own count of time since
+//        boot in seconds.
+uint32_t secondsSinceBoot()
 {
-    /*
+    return millis() / 1000;
+}
 
-    */
+uint8_t currentHourIndex()
+{
+    // return ((secondsSinceBoot() - (secondsSinceBoot() / (hoursToLog * secondsPerHour))) / secondsPerHour);
+    return ((secondsSinceBoot() / secondsPerHour) % hoursToLog);
+}
+
+// currentHourIndexReset() should be called every time we receive a packet to log (either RX or TX)
+//   and every time we are asked to report on airtime usage.
+void currentHourIndexReset()
+{
+    if (airtimes.lastHourIndex != currentHourIndex()) {
+        airtimes.hourTX[currentHourIndex()] = 0;
+        airtimes.hourRX[currentHourIndex()] = 0;
+        airtimes.hourRX_ALL[currentHourIndex()] = 0;
+
+        airtimes.lastHourIndex = currentHourIndex();
+    }
+}
+
+uint16_t *airtimeReport(reportTypes reportType)
+{
+    static uint16_t array[hoursToLog];
+
+    currentHourIndexReset();
+
+    for (int i = 0; i < hoursToLog; i++) {
+        if (reportType == TX_LOG) {
+            array[i] = airtimes.hourTX[(airtimes.lastHourIndex + i) % hoursToLog];
+        } else if (reportType == RX_LOG) {
+            array[i] = airtimes.hourRX[(airtimes.lastHourIndex + i) % hoursToLog];
+        } else if (reportType == RX_ALL_LOG) {
+            array[i] = airtimes.hourRX_ALL[(airtimes.lastHourIndex + i) % hoursToLog];
+        } else {
+            // Unknown report type
+            return array;
+        }
+    }
+
+    return array;
 }
--- src/airtime.h
+++ src/airtime.h
@@ -4,3 +4,13 @@
 #include <Arduino.h>
 #include <functional>
 
+enum reportTypes { TX_LOG, RX_LOG, RX_ALL_LOG };
+
+void logAirtime(reportTypes reportType, uint32_t airtime_ms);
+
+void currentHourIndexReset();
+uint8_t currentHourIndex();
+
+uint32_t secondsSinceBoot();
+
+uint16_t *airtimeReport(reportTypes reportType);
