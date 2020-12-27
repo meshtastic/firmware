19e633ef3d4623cd34e9973a29a56b8daad4b5ab
Jm Casler <jm@casler.org>
2020-12-27 09:29:48 -0800

    Add RX and RX_ALL analytics for #588

--- src/airtime.h
+++ src/airtime.h
@@ -4,6 +4,24 @@
 #include <Arduino.h>
 #include <functional>
 
+/*
+  TX_LOG      - Time on air this device has transmitted
+
+  RX_LOG      - Time on air used by valid and routable mesh packets, does not include
+                TX air time
+
+  RX_ALL_LOG  - Time of all received lora packets. This includes packets that are not
+                for meshtastic devices. Does not include TX air time.
+
+  Example analytics:
+
+  TX_LOG + RX_LOG = Total air time for a perticular meshtastic channel.
+
+  TX_LOG + RX_LOG = Total air time for a perticular meshtastic channel, including
+                    other lora radios. 
+
+  RX_ALL_LOG - RX_LOG = Other lora radios on our frequency channel.
+*/
 enum reportTypes { TX_LOG, RX_LOG, RX_ALL_LOG };
 
 void logAirtime(reportTypes reportType, uint32_t airtime_ms);
