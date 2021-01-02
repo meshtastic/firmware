a7f93de3ad62914d74bc172290934d60aa01dbe1
Kevin Hester <kevinh@geeksville.com>
2021-01-02 18:20:51 +0800

    add a software based cross platform AES-CTR implementation

---
+++ src/portduino/CrossPlatformCryptoEngine.cpp
@@ -0,0 +1,82 @@
+#include "AES.h"
+#include "CTR.h"
+#include "CryptoEngine.h"
+#include "configuration.h"
+
+/** A platform independent AES engine implemented using Tiny-AES
+ */
+class CrossPlatformCryptoEngine : public CryptoEngine
+{
+
+    CTRCommon *ctr = NULL;
+
+    /// How many bytes in our key
+    uint8_t keySize = 0;
+
+  public:
+    CrossPlatformCryptoEngine() {}
+
+    ~CrossPlatformCryptoEngine() {}
+
+    /**
+     * Set the key used for encrypt, decrypt.
+     *
+     * As a special case: If all bytes are zero, we assume _no encryption_ and send all data in cleartext.
+     *
+     * @param numBytes must be 16 (AES128), 32 (AES256) or 0 (no crypt)
+     * @param bytes a _static_ buffer that will remain valid for the life of this crypto instance (i.e. this class will cache the
+     * provided pointer)
+     */
+    virtual void setKey(size_t numBytes, uint8_t *bytes)
+    {
+        keySize = numBytes;
+        DEBUG_MSG("Installing AES%d key!\n", numBytes * 8);
+        if (ctr) {
+            delete ctr;
+            ctr = NULL;
+        }
+        if (numBytes != 0) {
+            if (numBytes == 16)
+                ctr = new CTR<AES128>();
+            else
+                ctr = new CTR<AES256>();
+
+            ctr->setKey(bytes, numBytes);
+        }
+    }
+
+    /**
+     * Encrypt a packet
+     *
+     * @param bytes is updated in place
+     */
+    virtual void encrypt(uint32_t fromNode, uint64_t packetNum, size_t numBytes, uint8_t *bytes)
+    {
+        if (keySize != 0) {
+            uint8_t stream_block[16];
+            static uint8_t scratch[MAX_BLOCKSIZE];
+            size_t nc_off = 0;
+
+            // DEBUG_MSG("ESP32 encrypt!\n");
+            initNonce(fromNode, packetNum);
+            assert(numBytes <= MAX_BLOCKSIZE);
+            memcpy(scratch, bytes, numBytes);
+            memset(scratch + numBytes, 0,
+                   sizeof(scratch) - numBytes); // Fill rest of buffer with zero (in case cypher looks at it)
+
+            ctr->setIV(nonce, sizeof(nonce));
+            ctr->setCounterSize(4);
+            ctr->encrypt(bytes, scratch, numBytes);
+        }
+    }
+
+    virtual void decrypt(uint32_t fromNode, uint64_t packetNum, size_t numBytes, uint8_t *bytes)
+    {
+        // For CTR, the implementation is the same
+        encrypt(fromNode, packetNum, numBytes, bytes);
+    }
+
+  private:
+};
+
+CryptoEngine *crypto = new CrossPlatformCryptoEngine();
