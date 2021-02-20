50ee915b83462595945807e4bf8ab44d480afcbe
Jm <jm@casler.org>
2021-02-20 00:34:25 -0800

    #697 GPIO 12 is an odd pin.

--- src/configuration.h
+++ src/configuration.h
@@ -354,15 +354,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define VEXT_ENABLE 21 // active low, powers the oled display and the lora antenna boost
 #define LED_PIN 25     // If defined we will blink this LED
-#define BUTTON_PIN                                                                                                               \
-    12 // If defined, this will be used for user button presses, if your board doesn't have a physical switch, you can wire one
-       // between this pin and ground
+#define BUTTON_PIN 12  // If defined, this will be used for user button presses,
 
-/* Don't enable the internal pull up on the TLORA_V2_1_16. GPIO 12 is connected to a bidirectional
-   diode. If someone needs to use a button, they should connect an external pull up that's stronger
-   than what's built into the esp32
-*/ 
-// #define BUTTON_NEED_PULLUP
+#define BUTTON_NEED_PULLUP
 
 #define USE_RF95
 #define LORA_DIO0 26 // a No connect on the SX1262 module
@@ -380,15 +374,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define GPS_RESET_N 10
 #define GPS_EXTINT 23 // On MAX-M8 module pin name is EXTINT. On L70 module pin name is STANDBY.
 
-#define BATTERY_PIN 39 // A battery voltage measurement pin, voltage divider connected here to measure battery voltage
+#define BATTERY_PIN 39    // A battery voltage measurement pin, voltage divider connected here to measure battery voltage
 #define BATTERY_EN_PIN 14 // Voltage voltage divider enable pin connected to mosfet
 
 #define I2C_SDA 4 // I2C pins for this board
 #define I2C_SCL 2
 
-#define LED_PIN 12     // If defined we will blink this LED
-//#define BUTTON_PIN 36  // If defined, this will be used for user button presses (ToDo problem on that line on debug screen -->   Long press start!)
-//#define BUTTON_NEED_PULLUP //GPIOs 34 to 39 are GPIs – input only pins. These pins don’t have internal pull-ups or pull-down resistors. 
+#define LED_PIN 12 // If defined we will blink this LED
+//#define BUTTON_PIN 36  // If defined, this will be used for user button presses (ToDo problem on that line on debug screen -->
+//Long press start!) #define BUTTON_NEED_PULLUP //GPIOs 34 to 39 are GPIs – input only pins. These pins don’t have internal
+//pull-ups or pull-down resistors.
 
 #define USE_RF95
 #define LORA_DIO0 38 // a No connect on the SX1262 module
