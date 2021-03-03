e9026e8f325f7664f64392f8547603cb28541777
Kevin Hester <kevinh@geeksville.com>
2021-03-03 11:46:09 +0800

    release to devs

--- bin/program-release-tbeam.sh
+++ bin/program-release-tbeam.sh
@@ -2,6 +2,7 @@
 set -e
 
 VERSION=`bin/buildinfo.py`
+FILENAME=release/latest/bins/universal/firmware-tbeam-$VERSION.bin
 
-echo Installing release/latest/bins/firmware-tbeam-US-$VERSION.bin
-esptool.py --baud 921600 write_flash 0x10000 release/latest/bins/firmware-tbeam-US-$VERSION.bin
+echo Installing $FILENAME
+esptool.py --baud 921600 write_flash 0x10000 $FILENAME
