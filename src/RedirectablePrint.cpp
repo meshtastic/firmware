772f2a15fffc0c528f71bc7bebaa4aa035d22b75
Kevin Hester <kevinh@geeksville.com>
2021-03-09 16:45:40 +0800

    check more error codes

--- src/RedirectablePrint.cpp
+++ src/RedirectablePrint.cpp
@@ -1,9 +1,9 @@
 #include "RedirectablePrint.h"
+#include "concurrency/OSThread.h"
 #include "configuration.h"
 #include <assert.h>
 #include <sys/time.h>
 #include <time.h>
-#include "concurrency/OSThread.h"
 
 /**
  * A printer that doesn't go anywhere
@@ -38,7 +38,7 @@ size_t RedirectablePrint::vprintf(const char *format, va_list arg)
         va_end(arg);
         return 0;
     };
-    if (len >= (int) printBufLen) {
+    if (len >= (int)printBufLen) {
         delete[] printBuf;
         printBufLen *= 2;
         printBuf = new char[printBufLen];
@@ -55,47 +55,52 @@ size_t RedirectablePrint::vprintf(const char *format, va_list arg)
 
 size_t RedirectablePrint::logDebug(const char *format, ...)
 {
-    va_list arg;
-    va_start(arg, format);
-
-    // Cope with 0 len format strings, but look for new line terminator
-    bool hasNewline = *format && format[strlen(format) - 1] == '\n';
-
     size_t r = 0;
 
-    // If we are the first message on a report, include the header
-    if (!isContinuationMessage) {
-        struct timeval tv;
-        if (!gettimeofday(&tv, NULL)) {
-            long hms = tv.tv_sec % SEC_PER_DAY;
-            //hms += tz.tz_dsttime * SEC_PER_HOUR;
-            //hms -= tz.tz_minuteswest * SEC_PER_MIN;
-            // mod `hms` to ensure in positive range of [0...SEC_PER_DAY)
-            hms = (hms + SEC_PER_DAY) % SEC_PER_DAY;
+    if (!inDebugPrint) {
+        inDebugPrint = true;
 
-            // Tear apart hms into h:m:s
-            int hour = hms / SEC_PER_HOUR;
-            int min = (hms % SEC_PER_HOUR) / SEC_PER_MIN;
-            int sec = (hms % SEC_PER_HOUR) % SEC_PER_MIN; // or hms % SEC_PER_MIN
+        va_list arg;
+        va_start(arg, format);
 
-            r += printf("%02d:%02d:%02d %u ", hour, min, sec, millis() / 1000);
-        } else
-            r += printf("??:??:?? %u ", millis() / 1000);
+        // Cope with 0 len format strings, but look for new line terminator
+        bool hasNewline = *format && format[strlen(format) - 1] == '\n';
 
-        auto thread = concurrency::OSThread::currentThread;
-        if(thread) {
-            print("[");
-            // printf("%p ", thread);
-            // assert(thread->ThreadName.length());
-            print(thread->ThreadName);
-            print("] ");
+        // If we are the first message on a report, include the header
+        if (!isContinuationMessage) {
+            struct timeval tv;
+            if (!gettimeofday(&tv, NULL)) {
+                long hms = tv.tv_sec % SEC_PER_DAY;
+                // hms += tz.tz_dsttime * SEC_PER_HOUR;
+                // hms -= tz.tz_minuteswest * SEC_PER_MIN;
+                // mod `hms` to ensure in positive range of [0...SEC_PER_DAY)
+                hms = (hms + SEC_PER_DAY) % SEC_PER_DAY;
+
+                // Tear apart hms into h:m:s
+                int hour = hms / SEC_PER_HOUR;
+                int min = (hms % SEC_PER_HOUR) / SEC_PER_MIN;
+                int sec = (hms % SEC_PER_HOUR) % SEC_PER_MIN; // or hms % SEC_PER_MIN
+
+                r += printf("%02d:%02d:%02d %u ", hour, min, sec, millis() / 1000);
+            } else
+                r += printf("??:??:?? %u ", millis() / 1000);
+
+            auto thread = concurrency::OSThread::currentThread;
+            if (thread) {
+                print("[");
+                // printf("%p ", thread);
+                // assert(thread->ThreadName.length());
+                print(thread->ThreadName);
+                print("] ");
+            }
         }
+
+        r += vprintf(format, arg);
+        va_end(arg);
+
+        isContinuationMessage = !hasNewline;
+        inDebugPrint = false;
     }
-
-    r += vprintf(format, arg);
-    va_end(arg);
-
-    isContinuationMessage = !hasNewline;
 
     return r;
 }
