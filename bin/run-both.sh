2e5452f4839513d82735768841880a9acdd39c2f
Kevin Hester <kevinh@geeksville.com>
2021-04-05 16:27:44 +0800

    run-both should target tbeams

--- bin/run-both.sh
+++ bin/run-both.sh
@@ -1,11 +1,13 @@
 set -e 
 
-pio run
+TARG=tbeam
+
+pio run -e $TARG
 
 echo uploading to usb1
-pio run --upload-port /dev/ttyUSB1 -t upload &
+pio run --upload-port /dev/ttyUSB1 -t upload -e $TARG &
 
 echo uploading to usb0
-pio run --upload-port /dev/ttyUSB0 -t upload &
+pio run --upload-port /dev/ttyUSB0 -t upload -e $TARG &
 
 wait
