640cc821036547fcfee8bfacf2a285e1c0d608ee
Kevin Hester <kevinh@geeksville.com>
2021-04-16 15:28:28 +0800

    1.2.29 get ready for automated releases

---
+++ bin/promote-release.sh
@@ -0,0 +1,9 @@
+echo "This script is only for developers who are publishing new builds on github.  Most users don't need it"
+
+VERSION=`bin/buildinfo.py`
+
+# Must have a V prefix to trigger github
+git tag "v${VERSION}"
+git push root "v${VERSION}" # push the tag
+
+echo "Tag ${VERSION} pushed to github, github actions should now be building the draft release.  If it seems good, click to publish it"
