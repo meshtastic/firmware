361556a6a7913104c513c8f8029efe090267bb9f
Kevin Hester <kevinh@geeksville.com>
2021-04-16 10:59:46 +0800

    use sha in build filenames

--- bin/platformio-custom.py
+++ bin/platformio-custom.py
@@ -4,32 +4,14 @@ import subprocess
 import configparser
 import traceback
 import sys
+from readprops import readProps
 
 Import("projenv")
 
 prefsLoc = projenv["PROJECT_DIR"] + "/version.properties"
-config = configparser.RawConfigParser()
-config.read(prefsLoc)
-version = dict(config.items('VERSION'))
-
-# Try to find current build SHA if if the workspace is clean.  This could fail if git is not installed
-try:
-    sha = subprocess.check_output(
-        ['git', 'rev-parse', '--short', 'HEAD']).decode("utf-8").strip()
-    isDirty = subprocess.check_output(
-        ['git', 'diff', 'HEAD']).decode("utf-8").strip()
-    suffix = sha
-    if isDirty:
-        suffix = sha + "-dirty"
-    verStr = "{}.{}.{}.{}".format(
-        version["major"], version["minor"], version["build"], suffix)
-except:
-    print("Unexpected error:", sys.exc_info()[0])
-    traceback.print_exc()
-    verStr = "{}.{}.{}".format(
-        version["major"], version["minor"], version["build"])
-
+verStr = readProps(prefsLoc)
 print("Using meshtastic platform-custom.py, firmare version " + verStr)
+# print("path is" + ','.join(sys.path))
 
 # General options that are passed to the C and C++ compilers
 projenv.Append(CCFLAGS=[
