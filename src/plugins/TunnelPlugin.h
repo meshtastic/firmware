50125c8828fc977955bbe649392204ab1286011c
Sam Reading-Thompson <samreadingthompson@gmail.com>
2021-04-30 03:09:30 +1000

    Changed boot icon and plugin

--- src/plugins/TunnelPlugin.h
+++ src/plugins/TunnelPlugin.h
@@ -6,6 +6,17 @@
 #include <Arduino.h>
 #include <functional>
 
+#ifdef HAS_EINK
+// The screen is bigger so use bigger fonts
+#define FONT_SMALL ArialMT_Plain_16
+#define FONT_MEDIUM ArialMT_Plain_24
+#define FONT_LARGE ArialMT_Plain_24
+#else
+#define FONT_SMALL ArialMT_Plain_10
+#define FONT_MEDIUM ArialMT_Plain_16
+#define FONT_LARGE ArialMT_Plain_24
+#endif
+
 class TunnelPlugin : private concurrency::OSThread
 {
     bool firstTime = 1;
@@ -39,7 +50,15 @@ class TunnelPluginRadio : public SinglePortPlugin
      * Send our payload into the mesh
      */
     void sendPayload(NodeNum dest = NODENUM_BROADCAST, bool wantReplies = false);
-
+  
+    #ifndef NO_SCREEN
+      virtual void drawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) { 
+        display->setTextAlignment(TEXT_ALIGN_CENTER);
+        display->setFont(FONT_SMALL);
+        display->drawString(64 + x, y, "Tunnel");
+       }
+    #endif
+  
   protected:
     virtual MeshPacket *allocReply();
 
@@ -49,7 +68,8 @@ class TunnelPluginRadio : public SinglePortPlugin
     */
     virtual bool handleReceived(const MeshPacket &mp);
 
-    virtual bool wantUIFrame() { return false; }
+    virtual bool wantUIFrame() { return true; }
+
 };
 
 extern TunnelPluginRadio *tunnelPluginRadio;
