ad1d7db35e2e154d10c276c855acffd84b5d790f
Kevin Hester <kevinh@geeksville.com>
2021-05-25 08:45:47 +0800

    use long versions for release names

--- bin/promote-release.sh
+++ bin/promote-release.sh
@@ -2,10 +2,10 @@ set -e
 
 echo "This script is only for developers who are publishing new builds on github.  Most users don't need it"
 
-VERSION=`bin/buildinfo.py short`
+VERSION=`bin/buildinfo.py long`
 
 # Must have a V prefix to trigger github
-git tag -f "v${VERSION}"
-git push root -f "v${VERSION}" # push the tag
+git tag "v${VERSION}"
+git push root "v${VERSION}" # push the tag
 
 echo "Tag ${VERSION} pushed to github, github actions should now be building the draft release.  If it seems good, click to publish it"
