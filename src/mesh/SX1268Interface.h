16d2c565e878fefd9bc426efe9782a2199af2252
Vladislav Osmanov <info@auxmic.com>
2021-09-18 21:39:29 +0300

    Use EU433 frequency value as default for SX1268

--- src/mesh/SX1268Interface.h
+++ src/mesh/SX1268Interface.h
@@ -8,8 +8,8 @@
 class SX1268Interface : public SX126xInterface<SX1268>
 {
   public:
-    /// override frequency of the SX1268 module regardless of the region
-    virtual float getFreq() { return 433.0; }
+    /// override frequency of the SX1268 module regardless of the region (use EU433 value)
+    virtual float getFreq() { return 433.175f; }
 
     SX1268Interface(RADIOLIB_PIN_TYPE cs, RADIOLIB_PIN_TYPE irq, RADIOLIB_PIN_TYPE rst, RADIOLIB_PIN_TYPE busy, SPIClass &spi);
 };
