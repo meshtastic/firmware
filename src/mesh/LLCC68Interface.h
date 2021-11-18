f8681a9082b9ac978d54e3751b3704ff5bf1054f
Vladislav Osmanov <7123463+osmanovv@users.noreply.github.com>
2021-11-18 14:46:04 +0300

    LLCC68 doesn't support SF12 neither the "Long Slow" channel

--- src/mesh/LLCC68Interface.h
+++ src/mesh/LLCC68Interface.h
@@ -5,6 +5,10 @@
 /**
  * Our adapter for LLCC68 radios
  * https://www.semtech.com/products/wireless-rf/lora-core/llcc68
+ * ⚠️⚠️⚠️
+ * Be aware that LLCC68 does not support Spreading Factor 12 (SF12) and will not work on the default "Long Slow" channel.
+ * You must change the channel if you get `Critical Error #3` with this module.
+ * ⚠️⚠️⚠️
  */
 class LLCC68Interface : public SX126xInterface<LLCC68>
 {
