1175c981c02ecb4e9cbd4522707f063af4df0338
Jm Casler <jm@casler.org>
2021-12-02 17:16:39 -0800

    Update generated protos for S&F

--- src/mesh/generated/storeforward.pb.c
+++ src/mesh/generated/storeforward.pb.c
@@ -6,13 +6,13 @@
 #error Regenerate this file with the current version of nanopb generator.
 #endif
 
-PB_BIND(StoreAndForwardMessage, StoreAndForwardMessage, AUTO)
+PB_BIND(StoreAndForward, StoreAndForward, AUTO)
 
 
-PB_BIND(StoreAndForwardMessage_Statistics, StoreAndForwardMessage_Statistics, AUTO)
+PB_BIND(StoreAndForward_Statistics, StoreAndForward_Statistics, AUTO)
 
 
-PB_BIND(StoreAndForwardMessage_History, StoreAndForwardMessage_History, AUTO)
+PB_BIND(StoreAndForward_History, StoreAndForward_History, AUTO)
 
 
 
