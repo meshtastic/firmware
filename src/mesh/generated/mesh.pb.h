56213dc40a7b7c2e1103fd56771deab28018e8ae
Jm Casler <jm@casler.org>
2021-12-29 09:25:01 -0800

    Support for replies and tap backs
    https://github.com/meshtastic/Meshtastic-device/issues/1035

--- src/mesh/generated/mesh.pb.h
+++ src/mesh/generated/mesh.pb.h
@@ -234,6 +234,8 @@ typedef struct _MeshPacket {
     MeshPacket_Priority priority;
     int32_t rx_rssi;
     MeshPacket_Delayed delayed;
+    uint32_t reply_id;
+    bool is_tapback;
 } MeshPacket;
 
 typedef struct _NodeInfo {
@@ -331,7 +333,7 @@ extern "C" {
 #define RouteDiscovery_init_default              {0, {0, 0, 0, 0, 0, 0, 0, 0}}
 #define Routing_init_default                     {0, {RouteDiscovery_init_default}}
 #define Data_init_default                        {_PortNum_MIN, {0, {0}}, 0, 0, 0, 0}
-#define MeshPacket_init_default                  {0, 0, 0, 0, {Data_init_default}, 0, 0, 0, 0, 0, _MeshPacket_Priority_MIN, 0, _MeshPacket_Delayed_MIN}
+#define MeshPacket_init_default                  {0, 0, 0, 0, {Data_init_default}, 0, 0, 0, 0, 0, _MeshPacket_Priority_MIN, 0, _MeshPacket_Delayed_MIN, 0, 0}
 #define NodeInfo_init_default                    {0, false, User_init_default, false, Position_init_default, 0, 0}
 #define MyNodeInfo_init_default                  {0, 0, 0, "", "", "", _CriticalErrorCode_MIN, 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0, 0}
 #define LogRecord_init_default                   {"", 0, "", _LogRecord_Level_MIN}
@@ -343,7 +345,7 @@ extern "C" {
 #define RouteDiscovery_init_zero                 {0, {0, 0, 0, 0, 0, 0, 0, 0}}
 #define Routing_init_zero                        {0, {RouteDiscovery_init_zero}}
 #define Data_init_zero                           {_PortNum_MIN, {0, {0}}, 0, 0, 0, 0}
-#define MeshPacket_init_zero                     {0, 0, 0, 0, {Data_init_zero}, 0, 0, 0, 0, 0, _MeshPacket_Priority_MIN, 0, _MeshPacket_Delayed_MIN}
+#define MeshPacket_init_zero                     {0, 0, 0, 0, {Data_init_zero}, 0, 0, 0, 0, 0, _MeshPacket_Priority_MIN, 0, _MeshPacket_Delayed_MIN, 0, 0}
 #define NodeInfo_init_zero                       {0, false, User_init_zero, false, Position_init_zero, 0, 0}
 #define MyNodeInfo_init_zero                     {0, 0, 0, "", "", "", _CriticalErrorCode_MIN, 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0, 0}
 #define LogRecord_init_zero                      {"", 0, "", _LogRecord_Level_MIN}
@@ -429,6 +431,8 @@ extern "C" {
 #define MeshPacket_priority_tag                  12
 #define MeshPacket_rx_rssi_tag                   13
 #define MeshPacket_delayed_tag                   15
+#define MeshPacket_reply_id_tag                  16
+#define MeshPacket_is_tapback_tag                17
 #define NodeInfo_num_tag                         1
 #define NodeInfo_user_tag                        2
 #define NodeInfo_position_tag                    3
@@ -528,7 +532,9 @@ X(a, STATIC,   SINGULAR, UINT32,   hop_limit,        10) \
 X(a, STATIC,   SINGULAR, BOOL,     want_ack,         11) \
 X(a, STATIC,   SINGULAR, UENUM,    priority,         12) \
 X(a, STATIC,   SINGULAR, INT32,    rx_rssi,          13) \
-X(a, STATIC,   SINGULAR, UENUM,    delayed,          15)
+X(a, STATIC,   SINGULAR, UENUM,    delayed,          15) \
+X(a, STATIC,   SINGULAR, FIXED32,  reply_id,         16) \
+X(a, STATIC,   SINGULAR, BOOL,     is_tapback,       17)
 #define MeshPacket_CALLBACK NULL
 #define MeshPacket_DEFAULT NULL
 #define MeshPacket_payloadVariant_decoded_MSGTYPE Data
@@ -638,12 +644,12 @@ extern const pb_msgdesc_t ToRadio_PeerInfo_msg;
 #define RouteDiscovery_size                      40
 #define Routing_size                             42
 #define Data_size                                260
-#define MeshPacket_size                          311
+#define MeshPacket_size                          320
 #define NodeInfo_size                            270
 #define MyNodeInfo_size                          451
 #define LogRecord_size                           81
 #define FromRadio_size                           460
-#define ToRadio_size                             314
+#define ToRadio_size                             323
 #define ToRadio_PeerInfo_size                    8
 
 #ifdef __cplusplus
