706d6e2671d854c89fa8578401a02954c6958b9c
Jordan Mulcahey <snhjordy@gmail.com>
2022-01-07 02:29:58 -0800

    Use /usr/bin/env in bash and python script shebangs

--- bin/program-1.1-tbeam.sh
+++ bin/program-1.1-tbeam.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 esptool.py --baud 921600 write_flash 0x10000 release/archive/old/firmware-tbeam-1.1.50.bin
--- bin/run-1-monitor.sh
+++ bin/run-1-monitor.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 pio run --upload-port /dev/ttyUSB1 -t upload -t monitor
