221c8e63e2619e269711ce2db47818c719a3e672
Ben Meadors <benmmeadors@gmail.com>
2022-01-21 13:30:53 -0600

    Fixed comment

--- src/main.cpp
+++ src/main.cpp
@@ -699,7 +699,7 @@ axpDebugOutput.setup();
 #endif
 
 uint32_t rebootAtMsec; // If not zero we will reboot at this time (used to reboot shortly after the update completes)
-uint32_t shutdownAtMsec; // If not zero we will shutdown at this time (used to reboot shortly after the update completes)
+uint32_t shutdownAtMsec; // If not zero we will shutdown at this time (used to shutdown from python or mobile client)
 
 void powerCommandsCheck()
 {
