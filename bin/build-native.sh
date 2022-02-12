39caf6a1e2cd4e81b934504a6abfc32cbec43305
Ben Meadors <benmmeadors@gmail.com>
2022-02-12 14:44:56 -0600

    Removed elfs... elves?

--- bin/build-native.sh
+++ bin/build-native.sh
@@ -28,7 +28,4 @@ cp .pio/build/native/program $OUTDIR/bins/universal/meshtasticd_linux_amd64
 echo Generating $ARCHIVEDIR/firmware-$VERSION.zip
 rm -f $ARCHIVEDIR/firmware-$VERSION.zip
 zip --junk-paths $ARCHIVEDIR/firmware-$VERSION.zip $ARCHIVEDIR/spiffs-$VERSION.bin $OUTDIR/bins/universal/firmware-*-$VERSION.* $OUTDIR/bins/universal/meshtasticd* images/system-info.bin bin/device-install.* bin/device-update.*
-echo Generating $ARCHIVEDIR/elfs-$VERSION.zip
-rm -f $ARCHIVEDIR/elfs-$VERSION.zip
-zip --junk-paths $ARCHIVEDIR/elfs-$VERSION.zip $OUTDIR/elfs/universal/firmware-*-$VERSION.* 
 
