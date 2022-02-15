96b55372170a4ac5e02d8b9f5011a27784a331d3
Jm Casler <jm@casler.org>
2022-02-14 19:32:31 -0800

    New bands, new band plans and new channel configurations

--- src/mesh/MeshRadio.h
+++ src/mesh/MeshRadio.h
@@ -8,10 +8,13 @@
 // Map from old region names to new region enums
 struct RegionInfo {
     RegionCode code;
-    uint8_t numChannels;
-    uint8_t powerLimit; // Or zero for not set
-    float freq;
+    float freqStart;
+    float freqEnd;
+    float dutyCycle;
     float spacing;
+    uint8_t powerLimit; // Or zero for not set
+    bool audioPermitted;
+    bool freqSwitching;
     const char *name; // EU433 etc
 };
 
