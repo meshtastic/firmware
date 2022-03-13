58485fd3c9855cc9d8034e2a55be5fbf37f1a67c
Jm Casler <jm@casler.org>
2022-03-13 14:50:27 -0700

    Remove more old router role work.

--- src/mesh/FloodingRouter.cpp
+++ src/mesh/FloodingRouter.cpp
@@ -27,35 +27,6 @@ bool FloodingRouter::shouldFilterReceived(MeshPacket *p)
     return Router::shouldFilterReceived(p);
 }
 
-bool FloodingRouter::inRangeOfRouter()
-{
-
-    uint32_t maximum_router_sec = 300;
-
-    // FIXME : Scale minimum_snr to accomodate different modem configurations.
-    float minimum_snr = 2;
-
-    for (int i = 0; i < myNodeInfo.router_count; i++) {
-        // A router has been seen and the heartbeat was heard within the last 300 seconds
-        if (
-            ((myNodeInfo.router_sec[i] > 0) && (myNodeInfo.router_sec[i] < maximum_router_sec)) &&
-            (myNodeInfo.router_snr[i] > minimum_snr)
-            ) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-bool FloodingRouter::isPacketLocal(const MeshPacket *p)
-{
-
-    // TODO: Figure out if a packet is from a local node
-
-    return false;
-}
-
 void FloodingRouter::sniffReceived(const MeshPacket *p, const Routing *c)
 {
 
