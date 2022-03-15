15f4857cef0ef8b97611e66b66a52b3b2915fd6a
Sacha Weatherstone <sachaw100@hotmail.com>
2022-03-15 17:30:55 +1100

    Remove unused scripts

--- bin/mqtt-listen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mosquitto_sub -h mqtt.meshtastic.org -v -t \$SYS/\# -t msh/+/stat/\# -t msh/+/json/\#
-# mosquitto_sub -h test.mosquitto.org -v -t mesh/\# -F "%j"
--- bin/upload-to-rak4631.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Converting to uf2 for NRF52 Adafruit bootloader"
-bin/uf2conv.py .pio/build/rak4631/firmware.hex -f 0xADA52840
-cp flash.uf2 /media/kevinh/FTH*BOOT/
