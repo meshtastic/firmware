4bd9a38ce09d2e49f5b4467ed6a35a3ab34cf918
Thomas Göttgens <tgoettgens@gmail.com>
2022-03-28 16:55:58 +0200

    Add CardKB, FACES and RAK14004 support to inputbroker/cannedmessages

---
+++ src/input/facesKbI2cImpl.cpp
@@ -0,0 +1,25 @@
+#include "facesKbI2cImpl.h"
+#include "InputBroker.h"
+
+FacesKbI2cImpl *facesKbI2cImpl;
+
+FacesKbI2cImpl::FacesKbI2cImpl() :
+    KbI2cBase("facesKB")
+{
+}
+
+void FacesKbI2cImpl::init()
+{
+    if (faceskb_found != FACESKB_ADDR)
+    {
+        // Input device is not detected.
+        return;
+    }
+
+    inputBroker->registerSource(this);
+}
+
+void FacesKbI2cImpl::handlePressed()
+{
+    facesKbI2cImpl->PressHandler();
+}
