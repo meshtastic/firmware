c1ec842454a74c1edae02216b8a968b15551b3c6
Jm Casler <jm@casler.org>
2022-03-29 20:06:32 -0700

    bump node_db max_count to 80

--- src/mesh/generated/portnums.pb.h
+++ src/mesh/generated/portnums.pb.h
@@ -12,19 +12,14 @@
 /* Enum definitions */
 /* For any new 'apps' that run on the device or via sister apps on phones/PCs they should pick and use a
  unique 'portnum' for their application.
-
  If you are making a new app using meshtastic, please send in a pull request to add your 'portnum' to this
  master table.
  PortNums should be assigned in the following range:
-
  0-63   Core Meshtastic use, do not use for third party apps
  64-127 Registered 3rd party apps, send in a pull request that adds a new entry to portnums.proto to  register your application
  256-511 Use one of these portnums for your private applications that you don't want to register publically
-
  All other values are reserved.
-
  Note: This was formerly a Type enum named 'typ' with the same id #
-
  We have change to this 'portnum' based scheme for specifying app handlers for particular payloads.
  This change is backwards compatible by treating the legacy OPAQUE/CLEAR_TEXT values identically. */
 typedef enum _PortNum { 
