357c9fcd76c5ef2406b505378920284a99e2eab3
ssysm <contact@theeditorstudio.com>
2022-04-01 23:38:23 -0400

    revert

--- src/mesh/generated/deviceonly.pb.h
+++ src/mesh/generated/deviceonly.pb.h
@@ -108,7 +108,7 @@ extern const pb_msgdesc_t ChannelFile_msg;
 
 /* Maximum encoded size of messages (where known) */
 #define ChannelFile_size                         832
-#define DeviceState_size                         25183
+#define DeviceState_size                         23903
 
 #ifdef __cplusplus
 } /* extern "C" */
