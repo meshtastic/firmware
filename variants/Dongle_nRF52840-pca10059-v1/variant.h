5acde8f01b3705f866c5afd221cb7f445676e24c
Mark Trevor Birss <markbirss@gmail.com>
2022-04-05 15:15:36 +0200

    Update variant.h

--- variants/Dongle_nRF52840-pca10059-v1/variant.h
+++ variants/Dongle_nRF52840-pca10059-v1/variant.h
@@ -43,11 +43,11 @@ extern "C" {
 #define NUM_ANALOG_OUTPUTS (0)
 
 // LEDs
-#define PIN_LED1 (0 + 12)           //Blue LED        P1.12
-#define PIN_LED2 (0 + 6)            //Built in Green  P0.06
+#define PIN_LED1 (0 + 12)           // Blue LED        P1.12
+#define PIN_LED2 (0 + 6)            // Built in Green  P0.06
 
 //Green Built in LED1
-//#define PIN_LED1 (0 + 6)          //LED1            P1.15
+//#define PIN_LED1 (0 + 6)          // LED1            P1.15
   
 //RGB NeoPixel LED2
 //#define PIN_LED1 (0 + 8) Red
@@ -67,7 +67,7 @@ extern "C" {
  */
 
 #ifdef PCA10059
-#define PIN_BUTTON1 (32 + 6)        //BTN_DN           P1.06 Built in button
+#define PIN_BUTTON1 (32 + 6)        // BTN_DN           P1.06 Built in button
 #endif
 
 /*
@@ -79,7 +79,7 @@ static const uint8_t A0 = PIN_A0;
 #define ADC_RESOLUTION 14
 
 // Other pins
-#define PIN_AREF (-1)               //AREF            Not yet used
+#define PIN_AREF (-1)               // AREF            Not yet used
 
 
 static const uint8_t AREF = PIN_AREF;
@@ -99,15 +99,15 @@ static const uint8_t AREF = PIN_AREF;
  */
 #define SPI_INTERFACES_COUNT 2
 
-#define PIN_SPI_MISO  (0 + 17)        //MISO      P0.17
-#define PIN_SPI_MOSI  (0 + 15)        //MOSI      P0.15
-#define PIN_SPI_SCK   (0 + 13)        //SCK       P0.13
+#define PIN_SPI_MISO  (0 + 17)        // MISO      P0.17
+#define PIN_SPI_MOSI  (0 + 15)        // MOSI      P0.15
+#define PIN_SPI_SCK   (0 + 13)        // SCK       P0.13
 
 #define PIN_SPI1_MISO (-1)            //
-#define PIN_SPI1_MOSI (10)            //EPD_MOSI  P0.10
-#define PIN_SPI1_SCK  (9)             //EPD_SCLK  P0.09
+#define PIN_SPI1_MOSI (10)            // EPD_MOSI  P0.10
+#define PIN_SPI1_SCK  (9)             // EPD_SCLK  P0.09
 
-static const uint8_t SS = (0 + 31);   //LORA_CS   P0.31
+static const uint8_t SS = (0 + 31);   // LORA_CS   P0.31
 static const uint8_t MOSI = PIN_SPI_MOSI;
 static const uint8_t MISO = PIN_SPI_MISO;
 static const uint8_t SCK = PIN_SPI_SCK;
@@ -117,13 +117,13 @@ static const uint8_t SCK = PIN_SPI_SCK;
  */
 
 //#define PIN_EINK_EN  (-1)
-#define PIN_EINK_EN    (0 + 6)       //Turn on the Green built in LED
-#define PIN_EINK_CS    (32)          //EPD_CS		
-#define PIN_EINK_BUSY  (20)          //EPD_BUSY  	
-#define PIN_EINK_DC    (24)          //EPD_D/C		
-#define PIN_EINK_RES   (-1)          //Not Connected P0.22 available
-#define PIN_EINK_SCLK  (9)           //EPD_SCLK	
-#define PIN_EINK_MOSI  (10)          //EPD_MOSI	
+#define PIN_EINK_EN    (0 + 6)       // Turn on the Green built in LED
+#define PIN_EINK_CS    (32)          // EPD_CS		
+#define PIN_EINK_BUSY  (20)          // EPD_BUSY  	
+#define PIN_EINK_DC    (24)          // EPD_D/C		
+#define PIN_EINK_RES   (-1)          // Not Connected P0.22 available
+#define PIN_EINK_SCLK  (9)           // EPD_SCLK	
+#define PIN_EINK_MOSI  (10)          // EPD_MOSI	
 
 #define HAS_EINK
 
@@ -132,17 +132,17 @@ static const uint8_t SCK = PIN_SPI_SCK;
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA (32 + 4)         //SDA
-#define PIN_WIRE_SCL (32 + 7)         //SCL
+#define PIN_WIRE_SDA (32 + 4)         // SDA
+#define PIN_WIRE_SCL (32 + 7)         // SCL
 
 // NiceRF 868 LoRa module
 #define USE_SX1262
-#define SX126X_CS      (0 + 31)       //LORA_CS     P0.31
-#define SX126X_DIO1    (0 + 29)       //DIO1        P0.29
-#define SX126X_BUSY    (0 + 2)        //LORA_BUSY	  P0.02
-#define SX126X_RESET   (32 + 15)      //LORA_RESET  P1.15
-#define SX126X_TXEN    (-1)           //TXEN        P1.13 NiceRF 868 dont use
-#define SX126X_RXEN    (-1)           //RXEN        P1.10 NiceRF 868 dont use
+#define SX126X_CS      (0 + 31)       // LORA_CS     P0.31
+#define SX126X_DIO1    (0 + 29)       // DIO1        P0.29
+#define SX126X_BUSY    (0 + 2)        // LORA_BUSY	  P0.02
+#define SX126X_RESET   (32 + 15)      // LORA_RESET  P1.15
+#define SX126X_TXEN    (-1)           // TXEN        P1.13 NiceRF 868 dont use
+#define SX126X_RXEN    (-1)           // RXEN        P1.10 NiceRF 868 dont use
 #define SX126X_E22
 
 #define PIN_GPS_EN     (-1)
