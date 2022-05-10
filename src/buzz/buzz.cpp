a3dde272f7e07fda9768529be346c4b676793498
Thomas Göttgens <tgoettgens@gmail.com>
2022-05-10 11:07:49 +0200

    Buzzer Support for M5Stack

--- src/buzz/buzz.cpp
+++ src/buzz/buzz.cpp
@@ -9,7 +9,12 @@ void playStartMelody(){};
 void playShutdownMelody(){};
 
 #else
+#ifdef M5STACK
+#include "Speaker.h"
+TONE Tone;
+#else
 #include "Tone.h"
+#endif
 
 extern "C" void delay(uint32_t dwMs);
 
@@ -38,13 +43,26 @@ const int DURATION_1_4 = 250;  // 1/4 note
 void playTones(const ToneDuration *tone_durations, int size) {
   for (int i = 0; i < size; i++) {
     const auto &tone_duration = tone_durations[i];
+#ifdef M5STACK
+    Tone.tone(tone_duration.frequency_khz);
+    delay(tone_duration.duration_ms);
+    Tone.mute();
+#else
     tone(PIN_BUZZER, tone_duration.frequency_khz, tone_duration.duration_ms);
+#endif
     // to distinguish the notes, set a minimum time between them.
     delay(1.3 * tone_duration.duration_ms);
   }
 }
 
+#ifdef M5STACK
+void playBeep() {
+  ToneDuration melody[] = {{NOTE_B3, DURATION_1_4}};
+  playTones(melody, sizeof(melody) / sizeof(ToneDuration));
+}
+#else
 void playBeep() { tone(PIN_BUZZER, NOTE_B3, DURATION_1_4); }
+#endif
 
 void playStartMelody() {
   ToneDuration melody[] = {{NOTE_B3, DURATION_1_4},
