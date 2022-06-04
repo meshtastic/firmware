df9e9bc223c97eea13bc5c0cc3d1d8049817a357
Thomas Göttgens <tgoettgens@gmail.com>
2022-06-04 10:37:24 +0200

    Bugfix for nRF Crypto 90% done, includes heavy debug code, do not merge for now.

--- src/nrf52/NRF52CryptoEngine.cpp
+++ src/nrf52/NRF52CryptoEngine.cpp
@@ -16,48 +16,54 @@ class NRF52CryptoEngine : public CryptoEngine
      */
     virtual void encrypt(uint32_t fromNode, uint64_t packetId, size_t numBytes, uint8_t *bytes) override
     {
-//        DEBUG_MSG("NRF52 encrypt!\n");
-
+        hexDump("before", bytes, numBytes, 16);
         if (key.length > 16) {
+            DEBUG_MSG("Software encrypt fr=%x, num=%x, numBytes=%d!\n", fromNode, (uint32_t) packetId, numBytes);
             AES_ctx ctx;
             initNonce(fromNode, packetId);
             AES_init_ctx_iv(&ctx, key.bytes, nonce);
             AES_CTR_xcrypt_buffer(&ctx, bytes, numBytes);
         } else if (key.length > 0) {
+            DEBUG_MSG("nRF52 encrypt fr=%x, num=%x, numBytes=%d!\n", fromNode, (uint32_t) packetId, numBytes);
             nRFCrypto.begin();
             nRFCrypto_AES ctx;
             uint8_t myLen = ctx.blockLen(numBytes);
+            DEBUG_MSG("nRF52 encBuf myLen=%d!\n", myLen);
             char encBuf[myLen] = {0};
-            memcpy(encBuf, bytes, numBytes);
             initNonce(fromNode, packetId);
             ctx.begin();
-            ctx.Process(encBuf, numBytes, nonce, key.bytes, key.length, (char*)bytes, ctx.encryptFlag, ctx.ctrMode);
+            ctx.Process((char*)bytes, numBytes, nonce, key.bytes, key.length, encBuf, ctx.encryptFlag, ctx.ctrMode);
             ctx.end();
             nRFCrypto.end();
+            memcpy(bytes, encBuf, numBytes);
         }
+        hexDump("after", bytes, numBytes, 16);
     }
 
     virtual void decrypt(uint32_t fromNode, uint64_t packetId, size_t numBytes, uint8_t *bytes) override
     {
-//        DEBUG_MSG("NRF52 decrypt!\n");
-
+        hexDump("before", bytes, numBytes, 16);
         if (key.length > 16) {
+            DEBUG_MSG("Software decrypt fr=%x, num=%x, numBytes=%d!\n", fromNode, (uint32_t) packetId, numBytes);
             AES_ctx ctx;
             initNonce(fromNode, packetId);
             AES_init_ctx_iv(&ctx, key.bytes, nonce);
             AES_CTR_xcrypt_buffer(&ctx, bytes, numBytes);
         } else if (key.length > 0) {
+            DEBUG_MSG("nRF52 decrypt fr=%x, num=%x, numBytes=%d!\n", fromNode, (uint32_t) packetId, numBytes);
             nRFCrypto.begin();
             nRFCrypto_AES ctx;
             uint8_t myLen = ctx.blockLen(numBytes);
+            DEBUG_MSG("nRF52 decBuf myLen=%d!\n", myLen);
             char decBuf[myLen] = {0};
-            memcpy(decBuf, bytes, numBytes);
             initNonce(fromNode, packetId);
             ctx.begin();
-            ctx.Process(decBuf, numBytes, nonce, key.bytes, key.length, (char*)bytes, ctx.decryptFlag, ctx.ctrMode);
+            ctx.Process((char*)bytes, numBytes, nonce, key.bytes, key.length, decBuf, ctx.decryptFlag, ctx.ctrMode);
             ctx.end();
             nRFCrypto.end();
+            memcpy(bytes, decBuf, numBytes);
         }
+        hexDump("after", bytes, numBytes, 16);
     }
 
   private:
