1843c0ab0e1cee85ccb17dcd2036240d5128bc49
Ben Meadors <benmmeadors@gmail.com>
2022-06-11 15:51:45 -0500

    Added getRegisterValue for i2cscan

--- src/debug/i2cScan.h
+++ src/debug/i2cScan.h
@@ -4,6 +4,24 @@
 #include "mesh/generated/telemetry.pb.h"
 
 #ifndef NO_WIRE
+uint16_t getRegisterValue(uint8_t address, uint8_t reg, uint8_t length) {
+    uint16_t value = 0x00;
+    Wire.beginTransmission(address);
+    Wire.write(reg);
+    Wire.endTransmission();
+    delay(20);
+    Wire.requestFrom((int)address, length);
+    DEBUG_MSG("Wire.available() = %d\n", Wire.available());
+    if (Wire.available() == 2) {
+        // Read MSB, then LSB
+        value = (uint16_t)Wire.read() << 8;  
+        value |= Wire.read();
+    } else if (Wire.available()) {
+        value = Wire.read();
+    }
+    return value;
+}
+
 uint8_t oled_probe(byte addr)
 {
     uint8_t r = 0;
@@ -35,7 +53,7 @@ uint8_t oled_probe(byte addr)
 void scanI2Cdevice(void)
 {
     byte err, addr;
-    uint8_t r = 0x00;
+    uint16_t registerValue = 0x00;
     int nDevices = 0;
     for (addr = 1; addr < 127; addr++) {
         Wire.beginTransmission(addr);
@@ -75,15 +93,8 @@ void scanI2Cdevice(void)
             if (addr == CARDKB_ADDR) {
                 cardkb_found = addr;
                 // Do we have the RAK14006 instead?
-                Wire.beginTransmission(addr);
-                Wire.write(0x04); // SENSOR_GET_VERSION
-                Wire.endTransmission();
-                delay(20);
-                Wire.requestFrom((int)addr, 1);
-                if (Wire.available()) {
-                    r = Wire.read();
-                }
-                if (r == 0x02) { // KEYPAD_VERSION
+                registerValue = getRegisterValue(addr, 0x04, 1);
+                if (registerValue == 0x02) { // KEYPAD_VERSION
                     DEBUG_MSG("RAK14004 found\n");
                     kb_model = 0x02;
                 } else {
@@ -106,36 +117,22 @@ void scanI2Cdevice(void)
             }
 #endif
         if (addr == BME_ADDR || addr == BME_ADDR_ALTERNATE) {
-            Wire.beginTransmission(addr);
-            Wire.write(0xD0); // GET_ID
-            Wire.endTransmission();
-            delay(20);
-            Wire.requestFrom((int)addr, 1);
-            if (Wire.available()) {
-                r = Wire.read();
-            }
-            if (r == 0x61) {
+            registerValue = getRegisterValue(addr, 0xD0, 1); // GET_ID
+            if (registerValue == 0x61) {
                 DEBUG_MSG("BME-680 sensor found at address 0x%x\n", (uint8_t)addr);
                 nodeTelemetrySensorsMap[TelemetrySensorType_BME680] = addr;
-            } else if (r == 0x60) {
+            } else if (registerValue == 0x60) {
                 DEBUG_MSG("BME-280 sensor found at address 0x%x\n", (uint8_t)addr);
                 nodeTelemetrySensorsMap[TelemetrySensorType_BME280] = addr;
             }
         }
         if (addr == INA_ADDR || addr == INA_ADDR_ALTERNATE) {
-            Wire.beginTransmission(addr);
-            Wire.write(0xFE); // INA260_REG_MFG_UID
-            Wire.endTransmission();
-            delay(20);
-            Wire.requestFrom((int)addr, 2);
-            if (Wire.available()) {
-                r = Wire.read();
-            }
-            DEBUG_MSG("INA260_REG_MFG_UID: 0x%x\n", (uint8_t)r);
-            if (r == 0x5449) {
+            registerValue = getRegisterValue(addr, 0xFE, 2);
+            DEBUG_MSG("Register MFG_UID: 0x%x\n", registerValue);
+            if (registerValue == 0x5449) {
                 DEBUG_MSG("INA260 sensor found at address 0x%x\n", (uint8_t)addr);
                 nodeTelemetrySensorsMap[TelemetrySensorType_INA260] = addr;
-            } else { // Assume INA219 if INA260 mfr ID is not found
+            } else { // Assume INA219 if INA260 ID is not found
                 DEBUG_MSG("INA219 sensor found at address 0x%x\n", (uint8_t)addr);
                 nodeTelemetrySensorsMap[TelemetrySensorType_INA219] = addr;
             }
