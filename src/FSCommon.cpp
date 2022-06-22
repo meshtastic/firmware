e7dfd14917f2d4cc2c42b6a00a715035c0c0c740
Thomas Göttgens <tgoettgens@gmail.com>
2022-06-22 15:33:53 +0200

    Change recursive delete to be recursive

--- src/FSCommon.cpp
+++ src/FSCommon.cpp
@@ -2,8 +2,8 @@
 #include "FSCommon.h"
 
 void listDir(const char * dirname, uint8_t levels)
-#ifdef FSCom
 {
+#ifdef FSCom
     File root = FSCom.open(dirname);
     if(!root){
         return;
@@ -29,29 +29,41 @@ void listDir(const char * dirname, uint8_t levels)
 }
 
 void rmDir(const char * dirname)
-#ifdef FSCom
 {
-    File root = FSCom.open(dirname);
-    if(!root){
+#ifdef FSCom
+    File file = FSCom.open(dirname);
+    if(!file){
         return;
     }
-    if(!root.isDirectory()){
+    if(!file.isDirectory()){
+        file.close();
+        FSCom.remove(file.name());
+      //  DEBUG_MSG("Remove FILE  %s\n", file.name());
         return;
     }
 
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory() && !String(file.name()).endsWith(".")) {
-            file.close();
-            rmDir(file.name());
-            FSCom.rmdir(file.name());
-        } else {
-            file.close();
-            FSCom.remove(file.name());
+    file.rewindDirectory();
+    while (true) {
+        File entry = file.openNextFile();
+        if (!entry) {
+          break;
         }
-        file.close();
-        file = root.openNextFile();
+        char dirpath[100];   // array to hold the result.
+        strcpy(dirpath, dirname); // copy string one into the result.
+        strcat(dirpath,"/"); // append string two to the result.
+        strcat(dirpath,entry.name()); // append string two to the result.
+        if(entry.isDirectory() && !String(entry.name()).endsWith(".")) {
+            entry.close();
+         //   DEBUG_MSG("Descend DIR  %s\n", dirpath);
+            rmDir(dirpath);
+        } else {
+            entry.close();
+         //   DEBUG_MSG("Remove FILE  %s\n", entry.name());
+            FSCom.remove(entry.name());
+        }
     }
+    FSCom.rmdir(dirname);
+   // DEBUG_MSG("Remove DIR  %s\n", dirname);
     file.close();
 #endif
 }
