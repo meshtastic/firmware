8a81ebbd5575af2d377ecdd93050e866665c9b59
Ben Meadors <benmmeadors@gmail.com>
2022-08-12 12:15:03 -0500

    Unifying bluetooth underpinnings

--- src/platform/esp32/ESP32Bluetooth.h
+++ src/platform/esp32/ESP32Bluetooth.h
@@ -2,22 +2,6 @@
 
 #pragma once
 
-extern uint16_t fromNumValHandle;
-
-class BluetoothPhoneAPI : public PhoneAPI
-{
-protected: 
-    /**
-     * Subclasses can use this as a hook to provide custom notifications for their transport (i.e. bluetooth notifies)
-     */
-    virtual void onNowHasData(uint32_t fromRadioNum) override;
-
-    /// Check the current underlying physical link to see if the client is currently connected
-    virtual bool checkIsConnected() override;
-};
-
-extern PhoneAPI *bluetoothPhoneAPI;
-
 class ESP32Bluetooth
 {
   public:
