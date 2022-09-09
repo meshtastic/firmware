914ddbad7ec26bda2dd177ac79e5c14894cbda3b
caveman99 <caveman99@users.noreply.github.com>
2022-09-09 09:19:18 +0000

    [create-pull-request] automated change

--- src/mesh/generated/localonly.pb.c
+++ src/mesh/generated/localonly.pb.c
@@ -6,7 +6,7 @@
 #error Regenerate this file with the current version of nanopb generator.
 #endif
 
-PB_BIND(LocalConfig, LocalConfig, 2)
+PB_BIND(LocalConfig, LocalConfig, AUTO)
 
 
 PB_BIND(LocalModuleConfig, LocalModuleConfig, 2)
