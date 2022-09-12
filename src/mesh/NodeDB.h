9df7a966d9e961f07769d517396692ab2a6a39ab
Ben Meadors <benmmeadors@gmail.com>
2022-09-12 08:13:34 -0500

    Backwards logic

--- src/mesh/NodeDB.h
+++ src/mesh/NodeDB.h
@@ -177,9 +177,9 @@ extern NodeDB nodeDB;
 
 #define ONE_DAY 24 * 60 * 60
 
-#define default_gps_attempt_time IF_ROUTER(5 * 60, 15 * 60)
-#define default_gps_update_interval IF_ROUTER(2 * 60, ONE_DAY)
-#define default_broadcast_interval_secs IF_ROUTER(12 * 60 * 60, 15 * 60)
+#define default_gps_attempt_time IF_ROUTER(15 * 60, 5 * 60)
+#define default_gps_update_interval IF_ROUTER(ONE_DAY, 2 * 60)
+#define default_broadcast_interval_secs IF_ROUTER(ONE_DAY / 2, 15 * 60)
 #define default_wait_bluetooth_secs IF_ROUTER(1, 60)
 #define default_mesh_sds_timeout_secs IF_ROUTER(NODE_DELAY_FOREVER, 2 * 60 * 60)
 #define default_sds_secs 365 * ONE_DAY
