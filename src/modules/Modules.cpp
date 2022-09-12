17db87e042ed1e1fb5d8af9ea1b75d970ed4cd9a
Thomas Göttgens <tgoettgens@gmail.com>
2022-09-12 10:08:32 +0200

    implement #1542

--- src/modules/Modules.cpp
+++ src/modules/Modules.cpp
@@ -19,11 +19,11 @@
 #endif
 #ifdef ARCH_ESP32
 #include "modules/esp32/RangeTestModule.h"
-#include "modules/esp32/SerialModule.h"
 #include "modules/esp32/StoreForwardModule.h"
 #endif
 #if defined(ARCH_ESP32) || defined(ARCH_NRF52)
 #include "modules/ExternalNotificationModule.h"
+#include "modules/SerialModule.h"
 #endif
 /**
  * Create module instances here.  If you are adding a new module, you must 'new' it here (or somewhere else)
