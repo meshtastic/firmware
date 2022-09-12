f0133baaf2353bb6b4da1904e46e197a5d729609
Ben Meadors <benmmeadors@gmail.com>
2022-09-12 07:39:41 -0500

    Add tbeam-s3-core

--- src/mesh/generated/localonly.pb.h
+++ src/mesh/generated/localonly.pb.h
@@ -144,7 +144,7 @@ extern const pb_msgdesc_t LocalModuleConfig_msg;
 #define LocalModuleConfig_fields &LocalModuleConfig_msg
 
 /* Maximum encoded size of messages (where known) */
-#define LocalConfig_size                         332
+#define LocalConfig_size                         336
 #define LocalModuleConfig_size                   270
 
 #ifdef __cplusplus
