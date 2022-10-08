6e40102f268cae43ace5e09db77a95674616adec
Thomas Göttgens <tgoettgens@gmail.com>
2022-10-08 17:07:16 +0200

    - use bigger screen fonts on Wiphone - erase oldest entry from NodeDB if it is full.

--- src/modules/CannedMessageModule.cpp
+++ src/modules/CannedMessageModule.cpp
@@ -11,7 +11,7 @@
 #include "graphics/fonts/OLEDDisplayFontsRU.h"
 #endif
 
-#if defined(USE_EINK) || defined(ILI9341_DRIVER)
+#if defined(USE_EINK) || defined(ILI9341_DRIVER) || defined(ST7735_CS)
 // The screen is bigger so use bigger fonts
 #define FONT_SMALL ArialMT_Plain_16
 #define FONT_MEDIUM ArialMT_Plain_24
