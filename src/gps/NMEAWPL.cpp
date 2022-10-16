b1f789dddd97439fd7833967b86e5f88bdaa7799
Thomas Göttgens <tgoettgens@gmail.com>
2022-10-16 16:45:32 +0200

    fix cppcheck

--- src/gps/NMEAWPL.cpp
+++ src/gps/NMEAWPL.cpp
@@ -15,7 +15,7 @@
  * -------------------------------------------
  */
 
-uint printWPL(char *buf, Position &pos, const char *name)
+uint printWPL(char *buf, const Position &pos, const char *name)
 {
     uint len = sprintf(buf, "$GNWPL,%07.2f,%c,%08.2f,%c,%s", pos.latitude_i * 1e-5, pos.latitude_i < 0 ? 'S' : 'N', pos.longitude_i * 1e-5, pos.longitude_i < 0 ? 'W' : 'E', name);
     uint chk = 0;
@@ -50,9 +50,9 @@ uint printWPL(char *buf, Position &pos, const char *name)
  * -------------------------------------------
  */
 
-uint printGGA(char *buf, Position &pos)
+uint printGGA(char *buf, const Position &pos)
 {
-    uint len = sprintf(buf, "$GNGGA,%06d.%03d,%07.2f,%c,%08.2f,%c,%d,%02d,%04d,%04d,%c,%04d,%c,%d,%04d",
+    uint len = sprintf(buf, "$GNGGA,%06u.%03u,%07.2f,%c,%08.2f,%c,%u,%02u,%04u,%04d,%c,%04d,%c,%d,%04d",
         pos.time / 1000,
         pos.time % 1000,
         pos.latitude_i * 1e-5, pos.latitude_i < 0 ? 'S' : 'N',
