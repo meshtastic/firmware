45f9dee89ae407493c32e7b79a02c84d33a933cc
Ben Meadors <benmmeadors@gmail.com>
2022-10-16 09:58:58 -0500

    Telemetry phone api guard (#1805)
    * Delay start of telemetry modules
    
    * Don't send unless our toPhoneQueue is empty
    
    * Get wrecked, checks!

--- src/gps/NMEAWPL.cpp
+++ src/gps/NMEAWPL.cpp
@@ -14,7 +14,7 @@
  * 6 Checksum
  */
 
-uint printWPL(char *buf, Position &pos, const char *name)
+uint printWPL(char *buf, const Position &pos, const char *name)
 {
     uint len = sprintf(buf, "$GNWPL,%07.2f,%c,%08.2f,%c,%s", pos.latitude_i * 1e-5, pos.latitude_i < 0 ? 'S' : 'N', pos.longitude_i * 1e-5, pos.longitude_i < 0 ? 'W' : 'E', name);
     uint chk = 0;
