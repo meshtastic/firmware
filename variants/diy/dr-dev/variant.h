0ce018cf97bd153e19bdbe45442106b1852d4259
code8buster <communismisgreat@national.shitposting.agency>
2022-10-31 21:39:11 -0400

    Put more of the module specific pins in their blocks

--- variants/diy/dr-dev/variant.h
+++ variants/diy/dr-dev/variant.h
@@ -1,4 +1,4 @@
-// For OLED LCD
+// Initialize i2c bus on sd_dat and esp_led pins, respectively. We need a bus to not hang on boot
 #define HAS_SCREEN 0
 #define I2C_SDA 4
 #define I2C_SCL 5
@@ -27,7 +27,7 @@
 #undef RF95_MOSI
 #define RF95_MOSI 23
 
-//  --PINS FOR THE 900M22S
+// PINS FOR THE 900M22S
 
 #define LORA_DIO1 26  // IRQ for SX1262/SX1268
 #define LORA_DIO2 22 // BUSY for SX1262/SX1268
@@ -36,17 +36,19 @@
 #undef RF95_NSS
 #define RF95_NSS 16
 #define SX126X_BUSY 22
+#define SX126X_CS 16
 
 
 // PINS FOR THE 900M30S
 /*
 #define LORA_DIO1 27  // IRQ for SX1262/SX1268
-#define LORA_RXEN 21  // Input - RF switch RX control, connecting external MCU IO, valid in high level
 #define LORA_DIO2 35 // BUSY for SX1262/SX1268
 #define LORA_TXEN NOT_A_PIN // Input - RF switch TX control, connecting external MCU IO or DIO2, valid in high level
+#define LORA_RXEN 21  // Input - RF switch RX control, connecting external MCU IO, valid in high level
 #undef RF95_NSS
 #define RF95_NSS 33
 #define SX126X_BUSY 35
+#define SX126X_CS 33
 */
 
 // RX/TX for RFM95/SX127x
@@ -55,15 +57,15 @@
 // #define RF95_TCXO <GPIO#>
 
 // common pinouts for SX126X modules
-#define SX126X_CS 33
+
 #define SX126X_DIO1 LORA_DIO1
 #define SX126X_RESET LORA_RESET
 #define SX126X_RXEN LORA_RXEN
 #define SX126X_TXEN LORA_TXEN
 
 // supported modules list
-#define USE_RF95 // RFM95/SX127x
+//#define USE_RF95 // RFM95/SX127x
 #define USE_SX1262
-#define USE_SX1268
-#define USE_LLCC68
+//#define USE_SX1268
+//#define USE_LLCC68
 #define SX126X_E22
