f1a9cf351e367c1f40c598dda1cabdd933008f85
Ben Meadors <benmmeadors@gmail.com>
2022-11-05 09:28:41 -0500

    Master to develop (resolves conflicts) (#1903)
    * Rearranging deck chairs, 900M22S working, 30S not
    
    * We do have wire, just not where it's expected
    
    * Put more of the module specific pins in their blocks
    
    * tryfix compile with NO_SCREEN
    
    * Portduino only: don't continue to try rebooting (#1887)
    
    Co-authored-by: Thomas Göttgens <tgoettgens@gmail.com>
    
    * Support for TLORA 2.1-1.8
    
    * Don't allow arbitrary channel name for admin (#1886)
    
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>
    
    * Experimental DIY nrf52840 feather support (#1884)
    
    * Experimental DIY nrf52840 feather support
    
    * Fix target
    
    * sx1262 wiring
    
    * Remove lib
    
    * Don't consider Response as ACK for FloodingRouter (#1885)
    
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>
    
    * Fix feather diy (#1892)
    
    * Fix variant
    
    * Fix feather diy target
    
    * [modules][external notifications] allow select channel to listen to
    
    * Fix feather diy pin mapping (#1894)
    
    Thanks to @markbirss
    
    * [create-pull-request] automated change
    
    * Override Screen Autodtect
    
    * Maybe fix crash of this board.
    
    * Fixes reset loop with new espressif32 platform on tlora-v2
    
     Sending a pulse to the OLED_RESET Pin 16 results in a reset loop using recent version of the espressif32 platform.
    
    * Experiment with self hosted action runner
    
    * Fix MQTT on ETH
    
    * revert the tryfix
    
    * Cheating
    
    * Cleanup artifacts older than 1 month
    
    * Update cleanup_artifacts.yml
    
    * Update cleanup_artifacts.yml
    
    * Update cleanup_artifacts.yml
    
    * Delete cleanup_artifacts.yml
    
    * fix: use RF95_IRQ for wakeup source in doLightSleep (#1899)
    
    Co-authored-by: Thomas Göttgens <tgoettgens@gmail.com>
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>
    
    * Update protos
    
    * Update protos
    
    Co-authored-by: code8buster <communismisgreat@national.shitposting.agency>
    Co-authored-by: Thomas Göttgens <tgoettgens@gmail.com>
    Co-authored-by: GUVWAF <78759985+GUVWAF@users.noreply.github.com>
    Co-authored-by: Mykhailo Lesyk <m@lesyk.org>
    Co-authored-by: Mykhailo Lesyk <lesykm@users.noreply.github.com>
    Co-authored-by: caveman99 <caveman99@users.noreply.github.com>
    Co-authored-by: he-leon <he-leon@users.noreply.github.com>
    Co-authored-by: Lars Weber <weber@weber-software.com>

--- variants/tlora_v2/variant.h
+++ variants/tlora_v2/variant.h
@@ -8,8 +8,6 @@
 #define I2C_SDA 21 // I2C pins for this board
 #define I2C_SCL 22
 
-#define RESET_OLED 16 // If defined, this pin will be used to reset the display controller
-
 #define VEXT_ENABLE 21 // active low, powers the oled display and the lora antenna boost
 #define LED_PIN 25     // If defined we will blink this LED
 #define BUTTON_PIN                                                                                                               \
