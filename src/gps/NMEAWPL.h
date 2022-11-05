87bb30d0b2023b7fed3106ab0c87181551e8faec
Ben Meadors <benmmeadors@gmail.com>
2022-11-05 15:10:43 -0500

    Fix for alpine linux builds

--- src/gps/NMEAWPL.h
+++ src/gps/NMEAWPL.h
@@ -3,5 +3,5 @@
 #include <Arduino.h>
 #include "main.h"
 
-uint printWPL(char *buf, const Position &pos, const char *name);
-uint printGGA(char *buf, const Position &pos);
+uin32_t printWPL(char *buf, const Position &pos, const char *name);
+uin32_t printGGA(char *buf, const Position &pos);
