d641adc0fc677548753af2d1571441c27997913a
Ben Meadors <benmmeadors@gmail.com>
2022-11-05 15:20:09 -0500

    Update NMEAWPL.h

--- src/gps/NMEAWPL.h
+++ src/gps/NMEAWPL.h
@@ -3,5 +3,5 @@
 #include <Arduino.h>
 #include "main.h"
 
-uin32_t printWPL(char *buf, const Position &pos, const char *name);
-uin32_t printGGA(char *buf, const Position &pos);
+uint32_t printWPL(char *buf, const Position &pos, const char *name);
+uint32_t printGGA(char *buf, const Position &pos);
