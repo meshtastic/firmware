a43d27147e39d035ae2acfd481f288df4b58e70a
Ahmed Kaddoura <akadd8@gmail.com>
2022-11-11 08:33:44 -0800

    Add Black Lager to module messages

--- src/modules/BlackLagerModule.cpp
+++ src/modules/BlackLagerModule.cpp
@@ -13,11 +13,11 @@ MeshPacket *BlackLagerModule::allocReply()
     auto req = *currentRequest;
     auto &p = req.decoded;
     // The incoming message is in p.payload
-    DEBUG_MSG("Received message from=0x%0x, id=%d, msg=%.*s\n", req.from, req.id, p.payload.size, p.payload.bytes);
+    DEBUG_MSG("Received Black Lager message from=0x%0x, id=%d, msg=%.*s\n", req.from, req.id, p.payload.size, p.payload.bytes);
 
-    screen->print("Sending reply\n");
+    screen->print("Sending Black Lager reply\n");
 
-    const char *replyStr = "Message Received";
+    const char *replyStr = "Black Lager message Received";
     auto reply = allocDataPacket();                 // Allocate a packet for sending
     reply->decoded.payload.size = strlen(replyStr); // You must specify how many bytes are in the reply
     memcpy(reply->decoded.payload.bytes, replyStr, reply->decoded.payload.size);
