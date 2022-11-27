0919993cabca25ff745ed76fd9dd2d63b2227197
Thomas Göttgens <tgoettgens@gmail.com>
2022-11-27 14:03:50 +0100

    update board definition, update copy/paste errors, fix SX1280.

--- variants/tlora_v2_1_18/variant.h
+++ variants/tlora_v2_1_18/variant.h
@@ -1,9 +1,5 @@
 #undef GPS_RX_PIN
 #undef GPS_TX_PIN
-#define GPS_RX_PIN 15 // per @der_bear on the forum, 36 is incorrect for this board type and 15 is a better pick
-#define GPS_TX_PIN 13
-
-#define EXT_NOTIFY_OUT 2 // Default pin to use for Ext Notify Module.
 
 #define BATTERY_PIN 35 // A battery voltage measurement pin, voltage divider connected here to measure battery voltage
 // ratio of voltage divider = 2.0 (R42=100k, R43=100k)
@@ -20,11 +16,7 @@
 #define USE_SX1280
 #define LORA_RESET 23
 
-#define SX128X_CS 18 // FIXME - we really should define LORA_CS instead
+#define SX128X_CS 18
 #define SX128X_DIO1 26
-#define SX128X_DIO2 33
 #define SX128X_BUSY 32
 #define SX128X_RESET LORA_RESET
-#define SX128X_E22 // Not really an E22 but TTGO seems to be trying to clone that
-// Internally the TTGO module hooks the SX1280-DIO2 in to control the TX/RX switch (which is the default for the sx1280interface
-// code)
