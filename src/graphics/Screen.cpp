e461258fa12a63489f5525bc83d288d3a253bdec
joshx1 <jxli2@dons.usfca.edu>
2022-11-30 11:58:27 -0800

    change title to Black Lager

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -132,7 +132,7 @@ static void drawIconScreen(const char *upperMsg, OLEDDisplay *display, OLEDDispl
 
     display->setFont(FONT_MEDIUM);
     display->setTextAlignment(TEXT_ALIGN_LEFT);
-    const char *title = "meshtastic.org";
+    const char *title = "black_lager.org";
     display->drawString(x + getStringCenteredX(title), y + SCREEN_HEIGHT - FONT_HEIGHT_MEDIUM, title);
     display->setFont(FONT_SMALL);
 
