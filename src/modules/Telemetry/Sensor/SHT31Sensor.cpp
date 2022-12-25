de7025c9797b64e2ada92d1ca560f19d084e140f
miaoski <miaoski@gmail.com>
2022-12-25 19:41:52 +0800

    support SHT31 temperature sensor

---
+++ src/modules/Telemetry/Sensor/SHT31Sensor.cpp
@@ -0,0 +1,31 @@
+#include "../mesh/generated/telemetry.pb.h"
+#include "configuration.h"
+#include "TelemetrySensor.h"
+#include "SHT31Sensor.h"
+#include <Adafruit_SHT31.h>
+
+SHT31Sensor::SHT31Sensor() : 
+    TelemetrySensor(TelemetrySensorType_SHT31, "SHT31") 
+{
+}
+
+int32_t SHT31Sensor::runOnce() {
+    DEBUG_MSG("Init sensor: %s\n", sensorName);
+    if (!hasSensor()) {
+        return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
+    }
+    status = sht31.begin(); 
+    return initI2CSensor();
+}
+
+void SHT31Sensor::setup() 
+{
+    // Set up oversampling and filter initialization
+}
+
+bool SHT31Sensor::getMetrics(Telemetry *measurement) {
+    measurement->variant.environment_metrics.temperature = sht31.readTemperature();
+    measurement->variant.environment_metrics.relative_humidity = sht31.readHumidity();
+
+    return true;
+}    
---
+++ src/modules/Telemetry/Sensor/SHT31Sensor.h
@@ -0,0 +1,16 @@
+#include "../mesh/generated/telemetry.pb.h"
+#include "TelemetrySensor.h"
+#include <Adafruit_SHT31.h>
+
+class SHT31Sensor : virtual public TelemetrySensor {
+private:
+    Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+protected:
+    virtual void setup() override;
+    
+public:
+    SHT31Sensor();
+    virtual int32_t runOnce() override;
+    virtual bool getMetrics(Telemetry *measurement) override;
+};    
