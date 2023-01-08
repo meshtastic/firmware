68301deff283a221022b34aa386a7aa9d39ba165
Thomas Göttgens <tgoettgens@gmail.com>
2023-01-08 18:15:51 +0100

    Adding to generated for now, protobuf PR will come when it works ... :-)

--- src/mesh/PhoneAPI.h
+++ src/mesh/PhoneAPI.h
@@ -42,6 +42,9 @@ class PhoneAPI : public Observer<uint32_t> // FIXME, we shouldn't be inheriting 
     /// downloads it
     MeshPacket *packetForPhone = NULL;
 
+    // file transfer packets destined for phone. Push it to the queue then free it.
+    XModem *xmodemPacketForPhone = NULL;
+
     // Keep QueueStatus packet just as packetForPhone
     QueueStatus *queueStatusPacketForPhone = NULL;
 
