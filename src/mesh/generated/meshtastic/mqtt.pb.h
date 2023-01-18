b2bd7577259bad31d9abaa2f626349cb4151eca7
thebentern <thebentern@users.noreply.github.com>
2023-01-18 14:57:44 +0000

    [create-pull-request] automated change

--- src/mesh/generated/meshtastic/mqtt.pb.h
+++ src/mesh/generated/meshtastic/mqtt.pb.h
@@ -4,7 +4,7 @@
 #ifndef PB_MESHTASTIC_MESHTASTIC_MQTT_PB_H_INCLUDED
 #define PB_MESHTASTIC_MESHTASTIC_MQTT_PB_H_INCLUDED
 #include <pb.h>
-#include "meshtastic/mesh.pb.h"
+#include "./mesh.pb.h"
 
 #if PB_PROTO_HEADER_VERSION != 40
 #error Regenerate this file with the current version of nanopb generator.
