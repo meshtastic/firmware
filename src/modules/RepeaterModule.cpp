99c4e10b63e28a8706ad69c37f00f8cf1bcc59a0
Ben Meadors <benmmeadors@gmail.com>
2023-01-28 08:18:47 -0600

    Remove comments

--- src/modules/RepeaterModule.cpp
+++ src/modules/RepeaterModule.cpp
@@ -12,9 +12,8 @@ bool RepeaterModule::handleReceivedProtobuf(const meshtastic_MeshPacket &mp, mes
     printPacket("Repeater observed message", &mp);
     router->sniffReceived(&mp, r);
 
-    // FIXME - move this to a non promsicious PhoneAPI module?
-    // Note: we are careful not to send back packets that started with the phone back to the phone
-    if ((mp.to == NODENUM_BROADCAST || mp.to == nodeDB.getNodeNum()) && (mp.from != 0)) {
+    if ((mp.to == NODENUM_BROADCAST || mp.to == nodeDB.getNodeNum()) && (mp.from != 0))
+    {
         printPacket("Delivering rx packet", &mp);
         service.handleFromRadio(&mp);
     }
