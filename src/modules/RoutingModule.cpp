afbe9c9d0ce1e131642056c8c60e9e7f162dab34
Ben Meadors <benmmeadors@gmail.com>
2023-01-30 14:50:57 -0600

    Fmt

--- src/modules/RoutingModule.cpp
+++ src/modules/RoutingModule.cpp
@@ -14,8 +14,7 @@ bool RoutingModule::handleReceivedProtobuf(const meshtastic_MeshPacket &mp, mesh
 
     // FIXME - move this to a non promsicious PhoneAPI module?
     // Note: we are careful not to send back packets that started with the phone back to the phone
-    if ((mp.to == NODENUM_BROADCAST || mp.to == nodeDB.getNodeNum()) && (mp.from != 0))
-    {
+    if ((mp.to == NODENUM_BROADCAST || mp.to == nodeDB.getNodeNum()) && (mp.from != 0)) {
         printPacket("Delivering rx packet", &mp);
         service.handleFromRadio(&mp);
     }
@@ -30,8 +29,7 @@ meshtastic_MeshPacket *RoutingModule::allocReply()
     assert(currentRequest);
 
     // We only consider making replies if the request was a legit routing packet (not just something we were sniffing)
-    if (currentRequest->decoded.portnum == meshtastic_PortNum_ROUTING_APP)
-    {
+    if (currentRequest->decoded.portnum == meshtastic_PortNum_ROUTING_APP) {
         assert(0); // 1.2 refactoring fixme, Not sure if anything needs this yet?
         // return allocDataProtobuf(u);
     }
