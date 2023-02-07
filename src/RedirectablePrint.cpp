0e09ee6ddecd98e1ee29c06bd6fce078ead0323b
Thomas Göttgens <tgoettgens@gmail.com>
2023-02-07 01:02:51 +0100

    eliminate main source of multiline logging

--- src/RedirectablePrint.cpp
+++ src/RedirectablePrint.cpp
@@ -5,6 +5,7 @@
 #include "configuration.h"
 #include <assert.h>
 #include <cstring>
+#include <stdexcept>
 #include <sys/time.h>
 #include <time.h>
 
@@ -172,4 +173,23 @@ void RedirectablePrint::hexDump(const char *logLevel, unsigned char *buf, uint16
         log(logLevel, s);
     }
     log(logLevel, "   +------------------------------------------------+ +----------------+\n");
+}
+
+std::string RedirectablePrint::mt_sprintf(const std::string fmt_str, ...)
+{
+    int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+    std::unique_ptr<char[]> formatted;
+    va_list ap;
+    while (1) {
+        formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
+        strcpy(&formatted[0], fmt_str.c_str());
+        va_start(ap, fmt_str);
+        final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+        va_end(ap);
+        if (final_n < 0 || final_n >= n)
+            n += abs(final_n - n + 1);
+        else
+            break;
+    }
+    return std::string(formatted.get());
 }
