MisterRager/meshtastic-firmware

Add Hardware: BetaFPV 900 Nano TX

src/platform/esp32/architecture.h
@@ -95,6 +95,8 @@
 #define HW_VENDOR meshtastic_HardwareModel_TLORA_T3_S3
 #elif defined(BETAFPV_2400_TX)
 #define HW_VENDOR meshtastic_HardwareModel_BETAFPV_2400_TX
+#elif defined(BETAFPV_900_TX_NANO)
+#define HW_VENDOR meshtastic_HardwareModel_PRIVATE_HW
 #endif
 
 //

meshtastic/firmware

src/platform/esp32/architecture.h
@@ -97,6 +97,8 @@
 #define HW_VENDOR meshtastic_HardwareModel_BETAFPV_2400_TX
 #elif defined(NANO_G1_EXPLORER)
 #define HW_VENDOR meshtastic_HardwareModel_NANO_G1_EXPLORER
+#elif defined(BETAFPV_900_TX_NANO)
+#define HW_VENDOR meshtastic_HardwareModel_BETAFPV_900_NANO_TX
 #endif
 
 //