f1c457f0c34180a4e134b91557307f10cfd8157c
Thomas Göttgens <tgoettgens@gmail.com>
2023-02-21 14:53:27 +0100

    tryfix #2228 as suggested by @mverch67

--- src/mesh/ReliableRouter.cpp
+++ src/mesh/ReliableRouter.cpp
@@ -148,7 +148,10 @@ bool ReliableRouter::stopRetransmission(GlobalPacketId key)
     if (old) {
         auto numErased = pending.erase(key);
         assert(numErased == 1);
+        // remove the 'original' (identified by originator and packet->id) from the txqueue and free it
         cancelSending(getFrom(old->packet), old->packet->id);
+        // now free the pooled copy for retransmission too. tryfix for #2228
+        packetPool.release(old->packet);
         return true;
     } else
         return false;
