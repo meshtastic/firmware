f738e2f7d840a39076c4c6d12e36f0185bec964d
Ben Meadors <benmmeadors@gmail.com>
2023-03-15 10:29:01 -0500

    Add built in LED pin

--- variants/rpipico/variant.h
+++ variants/rpipico/variant.h
@@ -21,6 +21,8 @@
 #define BUTTON_PIN 17
 #define EXT_NOTIFY_OUT 4
 
+#define LED_PIN PIN_LED
+
 #define BATTERY_PIN 26
 // ratio of voltage divider = 3.0 (R17=200k, R18=100k)
 #define ADC_MULTIPLIER 3.1 // 3.0 + a bit for being optimistic
