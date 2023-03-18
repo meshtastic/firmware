8c68d888c80f0199021c6753b088d670d4a8f7bd
GUVWAF <78759985+GUVWAF@users.noreply.github.com>
2023-03-18 13:23:37 +0100

    SX126x: Try next Interface when chip not found (#2363)
    * If chip was not found, return false for init()
    
    * SX1268: Only overwrite frequency when out of bounds
    Happens when region is still UNSET

--- src/mesh/SX1268Interface.h
+++ src/mesh/SX1268Interface.h
@@ -8,8 +8,7 @@
 class SX1268Interface : public SX126xInterface<SX1268>
 {
   public:
-    /// override frequency of the SX1268 module regardless of the region (use EU433 value)
-    virtual float getFreq() override { return 433.175f; }
+    virtual float getFreq() override;
 
     SX1268Interface(RADIOLIB_PIN_TYPE cs, RADIOLIB_PIN_TYPE irq, RADIOLIB_PIN_TYPE rst, RADIOLIB_PIN_TYPE busy, SPIClass &spi);
 };
