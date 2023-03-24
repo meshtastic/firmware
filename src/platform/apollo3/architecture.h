a7f9e5ddb4f178fefc369509c578654649bc9ca4
Ben Meadors <benmmeadors@gmail.com>
2023-03-23 19:35:51 -0500

    WIP scaffolding

---
+++ src/platform/apollo3/architecture.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define ARCH_APOLLO3
+
+//
+// defaults for ARCH_APOLLO3 architecture
+//
+
+//
+// set HW_VENDOR
+//
+
+#ifndef HW_VENDOR
+#define HW_VENDOR HardwareModel_PRIVATE_HW
+#endif
---
+++ src/platform/apollo3/main-apollo3.cpp
@@ -0,0 +1,24 @@
+#include "RTC.h"
+#include "configuration.h"
+
+void setBluetoothEnable(bool on) {}
+
+void playStartMelody() {}
+
+void updateBatteryLevel(uint8_t level) {}
+
+void getMacAddr(uint8_t *dmac)
+{
+    for (int i = 0; i < 6; i++)
+        dmac[i] = i;
+}
+
+void cpuDeepSleep(uint64_t msecToWake) {}
+
+/* pacify libc_nano */
+extern "C" {
+int _gettimeofday(struct timeval *tv, void *tzvp)
+{
+    return -1;
+}
+}
