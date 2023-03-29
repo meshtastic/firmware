dad9cb53fac6b04c96b314929148e0d8aece6cbb
Thomas Göttgens <tgoettgens@gmail.com>
2023-03-29 13:07:27 +0200

    trunk it baby

--- variants/MakePython_nRF52840_oled/variant.h
+++ variants/MakePython_nRF52840_oled/variant.h
@@ -28,8 +28,8 @@ extern "C" {
 #define RADIOLIB_GODMODE
 
 // LEDs
-#define PIN_LED1 (32 + 10)           // LED        P1.15
-#define PIN_LED2 (-1)                // 
+#define PIN_LED1 (32 + 10) // LED        P1.15
+#define PIN_LED2 (-1)      //
 
 #define LED_BUILTIN PIN_LED1
 #define LED_CONN PIN_LED2
@@ -37,25 +37,24 @@ extern "C" {
 #define LED_GREEN PIN_LED1
 #define LED_BLUE PIN_LED2
 
-#define LED_STATE_ON 0              // State when LED is litted
+#define LED_STATE_ON 0 // State when LED is litted
 
 /*
  * Buttons
  */
 
-#define PIN_BUTTON1 (32 + 15)        // P1.15 Built in button
+#define PIN_BUTTON1 (32 + 15) // P1.15 Built in button
 
 /*
  * Analog pins
  */
-#define PIN_A0 (-1)  
+#define PIN_A0 (-1)
 
 static const uint8_t A0 = PIN_A0;
 #define ADC_RESOLUTION 14
 
 // Other pins
-#define PIN_AREF (-1)               // AREF            Not yet used
-
+#define PIN_AREF (-1) // AREF            Not yet used
 
 static const uint8_t AREF = PIN_AREF;
 
@@ -74,11 +73,11 @@ static const uint8_t AREF = PIN_AREF;
  */
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO  (0 + 31)            // MISO      P0.31
-#define PIN_SPI_MOSI  (0 + 30)            // MOSI      P0.30
-#define PIN_SPI_SCK   (0 + 29)            // SCK       P0.29
+#define PIN_SPI_MISO (0 + 31) // MISO      P0.31
+#define PIN_SPI_MOSI (0 + 30) // MOSI      P0.30
+#define PIN_SPI_SCK (0 + 29)  // SCK       P0.29
 
-static const uint8_t SS = (32 + 15);       // LORA_CS   P1.15
+static const uint8_t SS = (32 + 15); // LORA_CS   P1.15
 static const uint8_t MOSI = PIN_SPI_MOSI;
 static const uint8_t MISO = PIN_SPI_MISO;
 static const uint8_t SCK = PIN_SPI_SCK;
@@ -88,22 +87,21 @@ static const uint8_t SCK = PIN_SPI_SCK;
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA   (0 + 26)          // SDA
-#define PIN_WIRE_SCL   (0 + 27)          // SCL
+#define PIN_WIRE_SDA (0 + 26) // SDA
+#define PIN_WIRE_SCL (0 + 27) // SCL
 
 // E-Byte E28 2.4 Ghz LoRa module
 #define USE_SX1280
-#define LORA_RESET   (0 + 5)
-#define SX128X_CS    (0 + 23)
-#define SX128X_DIO1  (0 + 4)
-#define SX128X_BUSY  (0 + 7) 
-//#define SX128X_TXEN  (32 + 9) 
-//#define SX128X_RXEN  (0 + 12) 
+#define LORA_RESET (0 + 5)
+#define SX128X_CS (0 + 23)
+#define SX128X_DIO1 (0 + 4)
+#define SX128X_BUSY (0 + 7)
+//#define SX128X_TXEN  (32 + 9)
+//#define SX128X_RXEN  (0 + 12)
 #define SX128X_RESET LORA_RESET
 
-
-#define PIN_GPS_EN     (-1)
-#define PIN_GPS_PPS    (-1)           // Pulse per second input from the GPS
+#define PIN_GPS_EN (-1)
+#define PIN_GPS_PPS (-1) // Pulse per second input from the GPS
 
 #define GPS_RX_PIN PIN_SERIAL1_RX
 #define GPS_TX_PIN PIN_SERIAL1_TX
@@ -125,7 +123,7 @@ static const uint8_t SCK = PIN_SPI_SCK;
 #undef AREF_VOLTAGE
 #define AREF_VOLTAGE 3.0
 #define VBAT_AR_INTERNAL AR_INTERNAL_3_0
-#define ADC_MULTIPLIER VBAT_DIVIDER_COMP //REAL_VBAT_MV_PER_LSB
+#define ADC_MULTIPLIER VBAT_DIVIDER_COMP // REAL_VBAT_MV_PER_LSB
 #define VBAT_RAW_TO_SCALED(x) (REAL_VBAT_MV_PER_LSB * x)
 
 #ifdef __cplusplus
