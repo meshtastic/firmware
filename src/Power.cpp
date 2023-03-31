921ecebf3dfe0d1d9c0d5ad13993a8147e3e4016
Vladislav Osmanov <7123463+osmanovv@users.noreply.github.com>
2023-03-31 13:43:42 +0300

    `EXT_PWR_DETECT` pin to detect external power source for boards without the power management chip

--- src/Power.cpp
+++ src/Power.cpp
@@ -160,8 +160,18 @@ class AnalogBatteryLevel : public HasBatteryLevel
 
     /// If we see a battery voltage higher than physics allows - assume charger is pumping
     /// in power
+    /// On some boards we don't have the power management chip (like AXPxxxx)
+    /// so we use EXT_PWR_DETECT GPIO pin to detect external power source
     virtual bool isVbusIn() override
     {
+#ifdef EXT_PWR_DETECT
+    // if external powered that pin will be pulled up
+    if (digitalRead(EXT_PWR_DETECT) == HIGH) {
+        return true;
+    }
+    // if it's not HIGH - check the battery
+#endif
+
         return getBattVoltage() > chargingVolt;
     }
 
