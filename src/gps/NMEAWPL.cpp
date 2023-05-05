e1c4968c58e60cdf20d4023972b0f0bb16e3c68a
Thomas Göttgens <tgoettgens@gmail.com>
2023-05-05 09:44:18 +0200

    wrong datapoint

--- src/gps/NMEAWPL.cpp
+++ src/gps/NMEAWPL.cpp
@@ -63,7 +63,7 @@ uint32_t printGGA(char *buf, size_t bufsz, const meshtastic_Position &pos)
         buf, bufsz, "$GNGGA,%02d%02d%02d.%02d,%02d%07.4f,%c,%03d%07.4f,%c,%u,%02u,%04u,%04d,%c,%04d,%c,%d,%04d", t->tm_hour,
         t->tm_min, t->tm_sec, pos.timestamp_millis_adjust, geoCoord.getDMSLatDeg(),
         (abs(geoCoord.getLatitude()) - geoCoord.getDMSLatDeg() * 1e+7) * 6e-6, geoCoord.getDMSLatCP(), geoCoord.getDMSLonDeg(),
-        (abs(geoCoord.getLongitude()) - geoCoord.getDMSLonDeg() * 1e+7) * 6e-6, geoCoord.getDMSLonCP(), pos.fix_type,
+        (abs(geoCoord.getLongitude()) - geoCoord.getDMSLonDeg() * 1e+7) * 6e-6, geoCoord.getDMSLonCP(), pos.fix_quality,
         pos.sats_in_view, pos.HDOP, geoCoord.getAltitude(), 'M', pos.altitude_geoidal_separation, 'M', 0, 0);
 
     uint32_t chk = 0;
