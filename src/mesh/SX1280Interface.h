16e46906683cf3356a0c4ef5c8e7a1c41aaaa598
Thomas Göttgens <tgoettgens@gmail.com>
2023-05-08 13:18:28 +0200

    RadioLib6 support

--- src/mesh/SX1280Interface.h
+++ src/mesh/SX1280Interface.h
@@ -9,5 +9,6 @@
 class SX1280Interface : public SX128xInterface<SX1280>
 {
   public:
-    SX1280Interface(RADIOLIB_PIN_TYPE cs, RADIOLIB_PIN_TYPE irq, RADIOLIB_PIN_TYPE rst, RADIOLIB_PIN_TYPE busy, SPIClass &spi);
+    SX1280Interface(LockingArduinoHal *hal, RADIOLIB_PIN_TYPE cs, RADIOLIB_PIN_TYPE irq, RADIOLIB_PIN_TYPE rst,
+                    RADIOLIB_PIN_TYPE busy);
 };
