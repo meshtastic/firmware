70dc13a998df48fc3d82eaced7dd0df0beeaa409
Thomas Göttgens <tgoettgens@gmail.com>
2023-05-10 14:14:48 +0200

    use BSEC2 only

--- src/modules/Telemetry/Sensor/BME680Sensor.h
+++ src/modules/Telemetry/Sensor/BME680Sensor.h
@@ -1,29 +1,15 @@
 #include "../mesh/generated/meshtastic/telemetry.pb.h"
 #include "TelemetrySensor.h"
-#ifdef USE_BSEC2
 #include <bsec2.h>
-#else
-#include <bsec.h>
-#endif // USE_BSEC2
 
 #define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000) // That's 6 hours worth of millis()
 
-#ifdef USE_BSEC2
 #include "config/Default_H2S_NonH2S/Default_H2S_NonH2S.h"
-#else
-const uint8_t Default_H2S_NonH2S_config[] = {
-#include <config/generic_33v_3s_4d/bsec_iaq.txt>
-};
-#endif // USE_BSEC2
 
 class BME680Sensor : virtual public TelemetrySensor
 {
   private:
-#ifdef USE_BSEC2
     Bsec2 bme680;
-#else
-    Bsec bme680;
-#endif // USE_BSEC2
 
   protected:
     virtual void setup() override;
