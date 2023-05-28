81a2fe3140cf4a7907af39cc83e6ba7d6bae77f6
GUVWAF <thijs@havinga.eu>
2023-05-28 15:04:50 +0200

    Enable Telemetry on RP2040

--- src/platform/rp2040/architecture.h
+++ src/platform/rp2040/architecture.h
@@ -2,6 +2,16 @@
 
 #define ARCH_RP2040
 
+#ifndef HAS_TELEMETRY
+#define HAS_TELEMETRY 1
+#endif
+#ifndef HAS_SENSOR
+#define HAS_SENSOR 1
+#endif
+#ifndef HAS_RADIO
+#define HAS_RADIO 1
+#endif
+
 #if defined(PRIVATE_HW)
 #define HW_VENDOR meshtastic_HardwareModel_PRIVATE_HW
 #endif
