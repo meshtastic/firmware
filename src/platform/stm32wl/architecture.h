a491ceefcda593a6c900f2b5c79a74c0162a551a
Ben Meadors <benmmeadors@gmail.com>
2023-06-01 07:14:55 -0500

    Wio-e5 wip (#2265)
    * Wio-e5 / STM32WL wip
    
    * Stubbing some FS stuff out
    
    * Wio-e5 / STM32WL wip
    
    * Stubbing some FS stuff out
    
    * Wio-e5 / STM32WL wip
    
    * Stubbing some FS stuff out
    
    * Wio-e5 / STM32WL wip
    
    * Stubbing some FS stuff out
    
    * LittleFS compiles. Can't check with actual device.
    
    * make cppcheck happy again
    
    * Guard against accelerometer thread
    
    * Missed a spot
    
    * Upload via ST-LINK
    
    * Derive MAC address from UID
    
    * upload port
    
    * Trunk it
    
    * Guard it
    
    * Maybe fix the cache error on startup.
    
    * Latest RadioLib ref to fix SubGHZ
    
    * revert nasty Sub-GHz Hack
    
    * Boots and radio inits with RadioLib 6.0, LittleFS doesn't seem to work
    
    ---------
    
    Co-authored-by: Thomas Göttgens <tgoettgens@gmail.com>
    Co-authored-by: GUVWAF <thijs@havinga.eu>

--- src/platform/stm32wl/architecture.h
+++ src/platform/stm32wl/architecture.h
@@ -6,24 +6,19 @@
 // defaults for STM32WL architecture
 //
 
+#ifndef HAS_RADIO
+#define HAS_RADIO 1
+#endif
+
 //
 // set HW_VENDOR
 //
 
 #ifndef HW_VENDOR
-#define HW_VENDOR HardwareModel_PRIVATE_HW
+#define HW_VENDOR meshtastic_HardwareModel_PRIVATE_HW
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-void stm32wl_emulate_digitalWrite(long unsigned int pin, long unsigned int value);
-int stm32wl_emulate_digitalRead(long unsigned int pin);
-#ifdef __cplusplus
-}
-#endif
-
-/* virtual pins for stm32wl_emulate_digitalWrite() / stm32wl_emulate_digitalRead() to recognize */
+/* virtual pins */
 #define SX126X_CS 1000
 #define SX126X_DIO1 1001
 #define SX126X_RESET 1003
