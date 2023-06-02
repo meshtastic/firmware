cd787232caadc35794e3a406e22645bc2ed1176f
Ben Meadors <benmmeadors@gmail.com>
2023-06-02 06:32:34 -0500

    Use INA for device battery level (#2536)
    * WIP
    
    * Continued wip
    
    * We got em
    
    * Voltage sensor base class
    
    * INA voltage
    
    * Log it
    
    * Stacie's mom has got it going on
    
    * Move declaration up
    
    * Last one
    
    * Sneaky little bugger
    
    * Macro guard to avoid calling methods

--- src/modules/Telemetry/Sensor/INA260Sensor.h
+++ src/modules/Telemetry/Sensor/INA260Sensor.h
@@ -1,8 +1,9 @@
 #include "../mesh/generated/meshtastic/telemetry.pb.h"
 #include "TelemetrySensor.h"
+#include "VoltageSensor.h"
 #include <Adafruit_INA260.h>
 
-class INA260Sensor : virtual public TelemetrySensor
+class INA260Sensor : virtual public TelemetrySensor, VoltageSensor
 {
   private:
     Adafruit_INA260 ina260 = Adafruit_INA260();
@@ -14,4 +15,5 @@ class INA260Sensor : virtual public TelemetrySensor
     INA260Sensor();
     virtual int32_t runOnce() override;
     virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
+    virtual uint16_t getBusVoltageMv() override;
 };
