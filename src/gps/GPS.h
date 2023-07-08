a273177d50e8f9b8f6b8a9c60c4e9c0c76b8c119
Max-Plastix <66535617+Max-Plastix@users.noreply.github.com>
2023-07-08 16:30:52 -0700

    Correct unused variable warning and typo around GNSS_MODEL_UNKNOWN (#2596)
    * Small warning and typo cleanup.
    
    * Update GPS.cpp (missed one instance of GNSS_MODEL_UNKONW)

--- src/gps/GPS.h
+++ src/gps/GPS.h
@@ -14,7 +14,7 @@ struct uBloxGnssModelInfo {
 typedef enum {
     GNSS_MODEL_MTK,
     GNSS_MODEL_UBLOX,
-    GNSS_MODEL_UNKONW,
+    GNSS_MODEL_UNKNOWN,
 } GnssModel_t;
 
 // Generate a string representation of DOP
@@ -175,7 +175,7 @@ class GPS : private concurrency::OSThread
     uint8_t fixeddelayCtr = 0;
 
   protected:
-    GnssModel_t gnssModel = GNSS_MODEL_UNKONW;
+    GnssModel_t gnssModel = GNSS_MODEL_UNKNOWN;
 };
 
 // Creates an instance of the GPS class.
