32bb60dfa4d4ed68af027e47fbde4cccd6d65b89
Jon <jcw122@gmail.com>
2023-07-26 18:38:58 -0400

    Not correct but can't figure it out yet

--- src/modules/AmbientLightingModule.cpp
+++ src/modules/AmbientLightingModule.cpp
@@ -1,25 +1,22 @@
 #include "AmbientLightingModule.h"
+#include "mesh/generated/meshtastic/module_config.pb.h"
 
-void handleAmbientLightingConfig(const AmbientLightingConfig &config)
+void handleModuleConfig(const meshtastic::ModuleConfig &config)
 {
-#ifdef RAK4630
-    if (config.has_led_state()) {
-        if (config.led_state()) {
-            rgb.begin();
-        } else {
-            rgb.end(); // or any other method to turn off the LED
+#ifdef HAS_NCP5623
+    if (config.has_ambient_lighting()) {
+        const auto &ambient_lighting_config = config.ambient_lighting();
+        if (ambient_lighting_config.has_current()) {
+            rgb.setCurrent(ambient_lighting_config.current() * 31 / 100); // Scale the percentage to 1-31 range
         }
-    }
 
-    if (config.has_current()) {
-        rgb.setCurrent(config.current() * 31 / 100); // Scale the percentage to 1-31 range
-    }
-
-    if (config.has_red() || config.has_green() || config.has_blue()) {
-        uint8_t red = config.has_red() ? config.red() * 255 / 100 : 0; // Scale the percentage to 0-255 range
-        uint8_t green = config.has_green() ? config.green() * 255 / 100 : 0;
-        uint8_t blue = config.has_blue() ? config.blue() * 255 / 100 : 0;
-        rgb.setColor(red, green, blue);
+        if (ambient_lighting_config.has_red() || ambient_lighting_config.has_green() || ambient_lighting_config.has_blue()) {
+            uint8_t red = ambient_lighting_config.has_red() ? ambient_lighting_config.red() * 255 / 100
+                                                            : 0; // Scale the percentage to 0-255 range
+            uint8_t green = ambient_lighting_config.has_green() ? ambient_lighting_config.green() * 255 / 100 : 0;
+            uint8_t blue = ambient_lighting_config.has_blue() ? ambient_lighting_config.blue() * 255 / 100 : 0;
+            rgb.setColor(red, green, blue);
+        }
     }
 #endif
 }
