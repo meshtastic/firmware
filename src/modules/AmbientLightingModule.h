8dc7d84400f59cf3781b2a09ee1579393f2ce17d
Jon <jcw122@gmail.com>
2023-07-26 21:13:18 -0400

    Back to basics

--- src/modules/AmbientLightingModule.h
+++ src/modules/AmbientLightingModule.h
@@ -1,18 +1,4 @@
-#ifndef AMBIENT_LIGHTING_MODULE_H
-#define AMBIENT_LIGHTING_MODULE_H
-
-#include "main.h"
-#include "mesh/generated/meshtastic/module_config.pb.h"
-
 #ifdef HAS_NCP5623
 #include <NCP5623.h>
 extern NCP5623 rgb;
-#endif
-
-class AmbientLightingModule
-{
-  public:
-    void handleConfig(const meshtastic::ModuleConfig &config);
-};
-
 #endif
