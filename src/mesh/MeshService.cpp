b050a4d2c7a6ebd027ae8741ec50e7b3bf7df0ca
Ben Meadors <benmmeadors@gmail.com>
2023-08-01 17:01:40 -0500

    Check decoded

--- src/mesh/MeshService.cpp
+++ src/mesh/MeshService.cpp
@@ -82,9 +82,10 @@ int MeshService::handleFromRadio(const meshtastic_MeshPacket *mp)
     powerFSM.trigger(EVENT_PACKET_FOR_PHONE); // Possibly keep the node from sleeping
 
     nodeDB.updateFrom(*mp); // update our DB state based off sniffing every RX packet from the radio
-    if (mp->decoded.portnum == meshtastic_PortNum_TELEMETRY_APP && mp->decoded.request_id > 0) {
+    if (mp->which_payload_variant == meshtastic_MeshPacket_decoded_tag &&
+        mp->decoded.portnum == meshtastic_PortNum_TELEMETRY_APP && mp->decoded.request_id > 0) {
         LOG_DEBUG(
-            "Received telemetry response. Don't send our NodeInfo because this potentially a Repeater which will ignore our "
+            "Received telemetry response. Skip sending our NodeInfo because this potentially a Repeater which will ignore our "
             "request for its NodeInfo.\n");
     } else if (mp->which_payload_variant == meshtastic_MeshPacket_decoded_tag && !nodeDB.getMeshNode(mp->from)->has_user &&
                nodeInfoModule) {
