32e02e535675830e5ec5f40ac218dc39245d5702
Ben Meadors <benmmeadors@gmail.com>
2023-08-14 19:00:51 -0500

    Initial Detection sensor module feature (#2722)
    * WIP
    
    * Updates
    
    * Doh!
    
    * Move it out of the macro guard so portduino can build
    
    * Changes from feedback

--- src/modules/AdminModule.cpp
+++ src/modules/AdminModule.cpp
@@ -373,6 +373,11 @@ void AdminModule::handleSetModuleConfig(const meshtastic_ModuleConfig &c)
         moduleConfig.has_neighbor_info = true;
         moduleConfig.neighbor_info = c.payload_variant.neighbor_info;
         break;
+    case meshtastic_ModuleConfig_detection_sensor_tag:
+        LOG_INFO("Setting module config: Detection Sensor\n");
+        moduleConfig.has_detection_sensor = true;
+        moduleConfig.detection_sensor = c.payload_variant.detection_sensor;
+        break;
     }
 
     saveChanges(SEGMENT_MODULECONFIG);
@@ -512,6 +517,11 @@ void AdminModule::handleGetModuleConfig(const meshtastic_MeshPacket &req, const 
             LOG_INFO("Getting module config: Neighbor Info\n");
             res.get_module_config_response.which_payload_variant = meshtastic_ModuleConfig_neighbor_info_tag;
             res.get_module_config_response.payload_variant.neighbor_info = moduleConfig.neighbor_info;
+            break;
+        case meshtastic_AdminMessage_ModuleConfigType_DETECTIONSENSOR_CONFIG:
+            LOG_INFO("Getting module config: Detection Sensor\n");
+            res.get_module_config_response.which_payload_variant = meshtastic_ModuleConfig_detection_sensor_tag;
+            res.get_module_config_response.payload_variant.detection_sensor = moduleConfig.detection_sensor;
             break;
         }
 
