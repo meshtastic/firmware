2ab310bc648c0024a23aa15c243fa362d9bb6a10
Thomas Göttgens <tgoettgens@gmail.com>
2023-08-23 09:44:20 +0200

    reset sym after second keypress also remove debug print and non-working scancodes.

--- src/modules/CannedMessageModule.cpp
+++ src/modules/CannedMessageModule.cpp
@@ -171,7 +171,7 @@ int CannedMessageModule::handleInputEvent(const InputEvent *event)
         (event->inputEvent == static_cast<char>(meshtastic_ModuleConfig_CannedMessageConfig_InputEventChar_LEFT)) ||
         (event->inputEvent == static_cast<char>(meshtastic_ModuleConfig_CannedMessageConfig_InputEventChar_RIGHT))) {
         // LOG_DEBUG("Canned message event (%x)\n", event->kbchar);
-        //  tweak for left/right events generated via trackball/touch with empty kbchar
+        // tweak for left/right events generated via trackball/touch with empty kbchar
         if (!event->kbchar) {
             if (event->inputEvent == static_cast<char>(meshtastic_ModuleConfig_CannedMessageConfig_InputEventChar_LEFT)) {
                 this->payload = 0xb4;
@@ -195,7 +195,7 @@ int CannedMessageModule::handleInputEvent(const InputEvent *event)
             this->runState = CANNED_MESSAGE_RUN_STATE_FREETEXT;
         }
         // pass the pressed key
-        LOG_DEBUG("Canned message ANYKEY (%x)\n", event->kbchar);
+        // LOG_DEBUG("Canned message ANYKEY (%x)\n", event->kbchar);
         this->payload = event->kbchar;
         this->lastTouchMillis = millis();
         validEvent = true;
