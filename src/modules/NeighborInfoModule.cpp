cfb6a1394c77d99addd6b6d7466e76347b0b97d1
GUVWAF <thijs@havinga.eu>
2023-09-05 19:56:42 +0200

    Only update neighbors when module is enabled

--- src/modules/NeighborInfoModule.cpp
+++ src/modules/NeighborInfoModule.cpp
@@ -203,8 +203,10 @@ Pass it to an upper client; do not persist this data on the mesh
 */
 bool NeighborInfoModule::handleReceivedProtobuf(const meshtastic_MeshPacket &mp, meshtastic_NeighborInfo *np)
 {
-    printNeighborInfo("RECEIVED", np);
-    updateNeighbors(mp, np);
+    if (enabled) {
+        printNeighborInfo("RECEIVED", np);
+        updateNeighbors(mp, np);
+    }
     // Allow others to handle this packet
     return false;
 }
