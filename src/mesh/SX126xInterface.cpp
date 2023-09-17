c1817f9fa21fadaf2204f7e49a9216d305486ac7
S5NC <145265251+S5NC@users.noreply.github.com>
2023-09-17 17:35:31 +0100

    Update SX126xInterface.cpp

--- src/mesh/SX126xInterface.cpp
+++ src/mesh/SX126xInterface.cpp
@@ -88,10 +88,10 @@ template <typename T> bool SX126xInterface<T>::init()
 #endif
 
 #ifndef SX126X_RXEN
-    SX126X_RXEN = RADIOLIB_NC;
+    #define SX126X_RXEN RADIOLIB_NC;
 #endif
 #ifndef SX126X_TXEN
-    SX126X_TXEN = RADIOLIB_NC;
+    #define SX126X_TXEN RADIOLIB_NC;
 #endif
     // If the pins for RX or for TX isn't defined, then it won't control that function as it is set to NC
     if (res == RADIOLIB_ERR_NONE) {
