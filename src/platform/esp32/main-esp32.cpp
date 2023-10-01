5edb5a2bc85368a418f67b4478ebf3d1c7891ce5
Ben Meadors <benmmeadors@gmail.com>
2023-09-30 21:04:54 -0500

    Heltec wireless tracker doesn't like the button interrupt (maybe all s3 because user button press doubles as bootloader mode trigger?)

--- src/platform/esp32/main-esp32.cpp
+++ src/platform/esp32/main-esp32.cpp
@@ -201,7 +201,7 @@ void cpuDeepSleep(uint32_t msecToWake)
 
 #ifdef BUTTON_PIN
 
-#if SOC_PM_SUPPORT_EXT_WAKEUP
+#if SOC_PM_SUPPORT_EXT_WAKEUP && !defined(HELTEC_WIRELESS_TRACKER)
     esp_sleep_enable_ext0_wakeup((gpio_num_t)(1ULL << (config.device.button_gpio ? config.device.button_gpio : BUTTON_PIN)), LOW);
 #endif
 
