fc8da13bfa6dc89cf41da913789b67995841e78d
S5NC <145265251+S5NC@users.noreply.github.com>
2023-11-08 18:55:45 +0000

    Add comment explaining necessity for second 0 check
    Thank you @GUVWAF

--- src/mesh/RadioInterface.cpp
+++ src/mesh/RadioInterface.cpp
@@ -446,9 +446,11 @@ void RadioInterface::applyModemConfig()
 
     if ((power == 0) || ((power > myRegion->powerLimit) && !devicestate.owner.is_licensed))
         power = myRegion->powerLimit;
-
+    
     if (power == 0)
-        power = 17; // Default to default power if we don't have a valid power
+        power = 17; // Default to this power level if we don't have a valid regional power limit (powerLimit of myRegion defaults
+                    // to 0, currently no region has an actual power limit of 0 [dBm] so we can assume regions which have this
+                    // variable set to 0 don't have a valid power limit)
 
     // Set final tx_power back onto config
     loraConfig.tx_power = (int8_t)power; // cppcheck-suppress assignmentAddressToInteger
