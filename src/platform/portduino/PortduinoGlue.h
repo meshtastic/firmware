4af90eeb3934f505f466e865b9eadbc9ab857c78
Jonathan Bennett <jbennett@incomsystems.biz>
2023-11-19 14:00:03 -0600

    Revamp yaml config for Raspbian

--- src/platform/portduino/PortduinoGlue.h
+++ src/platform/portduino/PortduinoGlue.h
@@ -4,18 +4,6 @@
 
 extern std::map<int, int> settingsMap;
 
-enum {
-    use_sx1262,
-    sx126x_cs,
-    sx126x_dio1,
-    sx126x_busy,
-    sx126x_reset,
-    sx126x_dio2_as_rf_switch,
-    use_rf95,
-    rf95_nss,
-    rf95_irq,
-    rf95_reset,
-    rf95_dio1
-};
+enum { use_sx1262, cs, irq, busy, reset, dio2_as_rf_switch, use_rf95 };
 
 #endif
