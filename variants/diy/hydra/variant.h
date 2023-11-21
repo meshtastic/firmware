170db79aa7c5342180f4c17d99ebb0f61e3fe869
Fexiven <48439988+Fexiven@users.noreply.github.com>
2023-11-21 14:43:18 +0100

    Update variant.h

--- variants/diy/hydra/variant.h
+++ variants/diy/hydra/variant.h
@@ -17,8 +17,8 @@
 #define LED_PIN 2           // add status LED (compatible with core-pcb and DIY targets)
 
 // Radio
-//#define USE_SX1262 // E22-900M30S uses SX1262 - 868Mhz
-//#define USE_SX1262 // E22-300M30S uses SX1268 - 433Mhz
+#define USE_SX1262 // E22-900M30S uses SX1262 - 868Mhz
+#define USE_SX1262 // E22-300M30S uses SX1268 - 433Mhz
 
 #define SX126X_MAX_POWER                                                                                                         \
     22 // Outputting 22dBm from SX1262 results in ~30dBm E22-900M30S output (module only uses last stage of the YP2233W PA)
