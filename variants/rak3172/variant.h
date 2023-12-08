2c6e2bb08938b36c478a28fea26b05f8d682c32e
Thomas Göttgens <tgoettgens@gmail.com>
2023-12-08 15:38:13 +0100

    Making progress with OSFS, still WIP

---
+++ variants/rak3172/variant.h
@@ -0,0 +1,17 @@
+/*
+Wio-E5 mini (formerly LoRa-E5 mini)
+https://www.seeedstudio.com/LoRa-E5-mini-STM32WLE5JC-p-4869.html
+https://www.seeedstudio.com/LoRa-E5-Wireless-Module-p-4745.html
+*/
+
+/*
+This variant is a work in progress.
+Do not expect a working Meshtastic device with this target.
+*/
+
+#ifndef _VARIANT_WIOE5_
+#define _VARIANT_WIOE5_
+
+#define USE_STM32WLx
+
+#endif
