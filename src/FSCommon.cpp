fe90751698ff5af07790e83cedcce8480c33d792
Thomas Göttgens <tgoettgens@gmail.com>
2023-12-13 16:11:35 +0100

    more progress, still too big. Adding RAK3172 to the equasion

--- src/FSCommon.cpp
+++ src/FSCommon.cpp
@@ -30,19 +30,21 @@ uint16_t OSFS::startOfEEPROM = 1;
 uint16_t OSFS::endOfEEPROM = 2048;
 
 // 3) How do I read from the medium?
-void OSFS::readNBytes(uint16_t address, unsigned int num, byte* output) {
-	for (uint16_t i = address; i < address + num; i++) {
-		*output = EEPROM.read(i);
-		output++;
-	}
+void OSFS::readNBytes(uint16_t address, unsigned int num, byte *output)
+{
+    for (uint16_t i = address; i < address + num; i++) {
+        *output = EEPROM.read(i);
+        output++;
+    }
 }
 
 // 4) How to I write to the medium?
-void OSFS::writeNBytes(uint16_t address, unsigned int num, const byte* input) {
-	for (uint16_t i = address; i < address + num; i++) {
-		EEPROM.update(i, *input);
-		input++;
-	}
+void OSFS::writeNBytes(uint16_t address, unsigned int num, const byte *input)
+{
+    for (uint16_t i = address; i < address + num; i++) {
+        EEPROM.update(i, *input);
+        input++;
+    }
 }
 #endif
 
@@ -56,30 +58,30 @@ void OSFS::writeNBytes(uint16_t address, unsigned int num, const byte* input) {
 bool copyFile(const char *from, const char *to)
 {
 #ifdef ARCH_STM32WL
-  unsigned char cbuffer[2048];
+    unsigned char cbuffer[2048];
 
-  // Var to hold the result of actions
-  OSFS::result r;
+    // Var to hold the result of actions
+    OSFS::result r;
 
-  r = OSFS::getFile(from, cbuffer);
+    r = OSFS::getFile(from, cbuffer);
 
-  if (r == notfound) {
-  	LOG_ERROR("Failed to open source file %s\n", from);
-    return false;
-  } else if (r == noerr) {
-    r = OSFS::newFile(to, cbuffer, true);
-    if (r == noerr) {
-      return true;
+    if (r == notfound) {
+        LOG_ERROR("Failed to open source file %s\n", from);
+        return false;
+    } else if (r == noerr) {
+        r = OSFS::newFile(to, cbuffer, true);
+        if (r == noerr) {
+            return true;
+        } else {
+            LOG_ERROR("OSFS Error %d\n", r);
+            return false;
+        }
+
     } else {
-    	LOG_ERROR("OSFS Error %d\n", r);
-      return false;
+        LOG_ERROR("OSFS Error %d\n", r);
+        return false;
     }
-
-  } else {
-  	LOG_ERROR("OSFS Error %d\n", r);
-    return false;
-  }
-return true;
+    return true;
 
 #elif defined(FSCom)
     unsigned char cbuffer[16];
--- variants/rak3172/variant.h
+++ variants/rak3172/variant.h
@@ -1,16 +1,10 @@
 /*
-Wio-E5 mini (formerly LoRa-E5 mini)
-https://www.seeedstudio.com/LoRa-E5-mini-STM32WLE5JC-p-4869.html
-https://www.seeedstudio.com/LoRa-E5-Wireless-Module-p-4745.html
-*/
-
-/*
 This variant is a work in progress.
 Do not expect a working Meshtastic device with this target.
 */
 
-#ifndef _VARIANT_WIOE5_
-#define _VARIANT_WIOE5_
+#ifndef _VARIANT_RAK3172_
+#define _VARIANT_RAK3172_
 
 #define USE_STM32WLx
 
