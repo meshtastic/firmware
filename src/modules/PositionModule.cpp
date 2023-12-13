1e824a309ea9665904f44c559aa260cde7864a67
Ben Meadors <benmmeadors@gmail.com>
2023-12-13 13:54:58 -0600

    5 minutes

--- src/modules/PositionModule.cpp
+++ src/modules/PositionModule.cpp
@@ -270,15 +270,12 @@ void PositionModule::sendLostAndFoundText()
     p->to = NODENUM_BROADCAST;
     char *message = new char[60];
     sprintf(message, "🚨I'm lost! Lat / Lon: %f, %f\0", (lastGpsLatitude * 1e-7), (lastGpsLongitude * 1e-7));
-    meshtastic_MeshPacket *p = allocDataPacket();
     p->decoded.portnum = meshtastic_PortNum_TEXT_MESSAGE_APP;
     p->want_ack = false;
     p->decoded.payload.size = strlen(message);
     memcpy(p->decoded.payload.bytes, message, p->decoded.payload.size);
 
-    service.sendToMesh(
-        p, RX_SRC_LOCAL,
-        true); 
+    service.sendToMesh(p, RX_SRC_LOCAL, true);
 }
 
 struct SmartPosition PositionModule::getDistanceTraveledSinceLastSend(meshtastic_PositionLite currentPosition)
