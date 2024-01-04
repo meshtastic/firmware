fc16c9316c33642ae605e6f8ad646e3e6302b22f
Amin Husni <aminhusni@gmail.com>
2024-01-05 00:10:39 +0800

    Update RadioInterface.cpp
    Formatting issues

--- src/mesh/RadioInterface.cpp
+++ src/mesh/RadioInterface.cpp
@@ -108,20 +108,24 @@ const RegionInfo regions[] = {
     RDEF(UA_868, 868.0f, 868.6f, 1, 0, 14, true, false, false),
 
     /*
+        Malaysia
         433 - 435 MHz at 100mW, no restrictions.
-        919 - 923 Mhz at 500mW, no restrictions.
-        923 - 924 MHz at 500mW with 1% duty cycle OR frequency hopping. 
-        Frequency hopping is used for 919 - 923 MHz. 
         https://www.mcmc.gov.my/skmmgovmy/media/General/pdf/Short-Range-Devices-Specification.pdf
     */
     RDEF(MY_433, 433.0f, 435.0f, 100, 0, 20, true, false, false),
 
+    /*
+        Malaysia
+        919 - 923 Mhz at 500mW, no restrictions.
+        923 - 924 MHz at 500mW with 1% duty cycle OR frequency hopping.
+        Frequency hopping is used for 919 - 923 MHz.
+        https://www.mcmc.gov.my/skmmgovmy/media/General/pdf/Short-Range-Devices-Specification.pdf
+    */
     RDEF(MY_919, 919.0f, 924.0f, 100, 0, 27, true, true, false),
 
     /*
        2.4 GHZ WLAN Band equivalent. Only for SX128x chips.
     */
-
     RDEF(LORA_24, 2400.0f, 2483.5f, 100, 0, 10, true, false, true),
 
     /*
