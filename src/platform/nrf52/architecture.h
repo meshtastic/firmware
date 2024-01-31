aaf55a239f20b02a4a04e2733d3abe206ab6f484
hdngr <1875033+hdngr@users.noreply.github.com>
2024-01-30 20:01:15 -0800

    update build flags to use CANARYONE enum

--- src/platform/nrf52/architecture.h
+++ src/platform/nrf52/architecture.h
@@ -48,8 +48,8 @@
 #define HW_VENDOR meshtastic_HardwareModel_T_ECHO
 #elif defined(NANO_G2_ULTRA)
 #define HW_VENDOR meshtastic_HardwareModel_NANO_G2_ULTRA
-#elif defined(CANARY_V1_0)
-#define HW_VENDOR meshtastic_HardwareModel_CANARY_V1_0
+#elif defined(CANARYONE)
+#define HW_VENDOR meshtastic_HardwareModel_CANARYONE
 #elif defined(NORDIC_PCA10059)
 #define HW_VENDOR meshtastic_HardwareModel_NRF52840_PCA10059
 #elif defined(PRIVATE_HW) || defined(FEATHER_DIY)
