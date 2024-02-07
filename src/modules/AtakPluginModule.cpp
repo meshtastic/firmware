00e5a8443f43cbe941e34bd2c9d86521742a349d
Ben Meadors <benmmeadors@gmail.com>
2024-02-07 16:43:14 -0600

    Protos and formatting

--- src/modules/AtakPluginModule.cpp
+++ src/modules/AtakPluginModule.cpp
@@ -6,16 +6,14 @@
 #include "main.h"
 #include "meshtastic/atak.pb.h"
 
-extern "C"
-{
+extern "C" {
 #include "mesh/compression/unishox2.h"
 }
 
 AtakPluginModule *atakPluginModule;
 
 AtakPluginModule::AtakPluginModule()
-    : ProtobufModule("atak", meshtastic_PortNum_ATAK_PLUGIN, &meshtastic_TAKPacket_msg),
-      concurrency::OSThread("AtakPluginModule")
+    : ProtobufModule("atak", meshtastic_PortNum_ATAK_PLUGIN, &meshtastic_TAKPacket_msg), concurrency::OSThread("AtakPluginModule")
 {
     ourPortNum = meshtastic_PortNum_ATAK_PLUGIN;
 }
@@ -41,25 +39,23 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
         return;
 
     // From Phone (EUD)
-    if (mp.from == 0)
-    {
+    if (mp.from == 0) {
         // LOG_DEBUG("Received uncompressed TAK payload from phone with %d bytes\n", mp.decoded.payload.size);
         // // Compress for LoRA transport
         // meshtastic_TAKPacket compressed = meshtastic_TAKPacket_init_default;
         // memccpy(&compressed, t, sizeof(meshtastic_TAKPacket), sizeof(meshtastic_TAKPacket));
         // compressed.contact.which_callsign_variant = meshtastic_Contact_callsign_compressed_tag;
         // auto length =
-        //     unishox2_compress_simple(t->contact.callsign_variant.callsign_uncompressed, sizeof(t->contact.callsign_variant.callsign_uncompressed),
+        //     unishox2_compress_simple(t->contact.callsign_variant.callsign_uncompressed,
+        //     sizeof(t->contact.callsign_variant.callsign_uncompressed),
         //                              compressed.contact.callsign_variant.callsign_compressed);
-        // memset(compressed.contact.callsign_variant.callsign_uncompressed, '\0', strlen(compressed.contact.callsign_variant.callsign_uncompressed));
-        // LOG_DEBUG("Uncompressed callsign %d bytes\n", strlen(t->contact.callsign_variant.callsign_uncompressed));
-        // LOG_DEBUG("Compressed callsign %d bytes\n", length);
+        // memset(compressed.contact.callsign_variant.callsign_uncompressed, '\0',
+        // strlen(compressed.contact.callsign_variant.callsign_uncompressed)); LOG_DEBUG("Uncompressed callsign %d bytes\n",
+        // strlen(t->contact.callsign_variant.callsign_uncompressed)); LOG_DEBUG("Compressed callsign %d bytes\n", length);
         // mp.decoded.payload.size = pb_encode_to_bytes(mp.decoded.payload.bytes, sizeof(mp.decoded.payload.bytes),
         //                                              meshtastic_TAKPacket_fields, &compressed);
         // LOG_DEBUG("Final payload size of %d bytes\n", mp.decoded.payload.size);
-    }
-    else
-    {
+    } else {
         // if (!t->has_contact || t->contact.which_callsign_variant != meshtastic_Contact_callsign_compressed_tag)
         // {
         //     // Not compressed. Something
@@ -71,15 +67,16 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
         // auto decompressedCopy = packetPool.allocCopy(mp);
         // meshtastic_TAKPacket uncompressed = meshtastic_TAKPacket_init_default;
         // memccpy(&uncompressed, t, sizeof(meshtastic_TAKPacket), sizeof(meshtastic_TAKPacket));
-        // auto length = unishox2_decompress_simple(t->contact.callsign_variant.callsign_compressed, decompressedCopy->decoded.payload.size,
+        // auto length = unishox2_decompress_simple(t->contact.callsign_variant.callsign_compressed,
+        // decompressedCopy->decoded.payload.size,
         //                                          uncompressed.contact.callsign_variant.callsign_uncompressed);
         // decompressedCopy->decoded.payload.size =
         //     pb_encode_to_bytes(decompressedCopy->decoded.payload.bytes, sizeof(decompressedCopy->decoded.payload),
         //                        meshtastic_TAKPacket_fields, &uncompressed);
 
         // LOG_DEBUG("Compressed callsign: %d bytes\n", strlen(t->contact.callsign_variant.callsign_compressed));
-        // LOG_DEBUG("Decompressed callsign: '%s' @ %d bytes\n", uncompressed.contact.callsign_variant.callsign_uncompressed, length);
-        // service.sendToPhone(decompressedCopy);
+        // LOG_DEBUG("Decompressed callsign: '%s' @ %d bytes\n", uncompressed.contact.callsign_variant.callsign_uncompressed,
+        // length); service.sendToPhone(decompressedCopy);
     }
     return;
 }
--- src/modules/AtakPluginModule.h
+++ src/modules/AtakPluginModule.h
@@ -7,17 +7,17 @@
  */
 class AtakPluginModule : public ProtobufModule<meshtastic_TAKPacket>, private concurrency::OSThread
 {
-public:
-  /** Constructor
-   * name is for debugging output
-   */
-  AtakPluginModule();
+  public:
+    /** Constructor
+     * name is for debugging output
+     */
+    AtakPluginModule();
 
-protected:
-  virtual bool handleReceivedProtobuf(const meshtastic_MeshPacket &mp, meshtastic_TAKPacket *t) override;
-  virtual void alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtastic_TAKPacket *t) override;
-  /* Does our periodic broadcast */
-  int32_t runOnce() override;
+  protected:
+    virtual bool handleReceivedProtobuf(const meshtastic_MeshPacket &mp, meshtastic_TAKPacket *t) override;
+    virtual void alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtastic_TAKPacket *t) override;
+    /* Does our periodic broadcast */
+    int32_t runOnce() override;
 };
 
 extern AtakPluginModule *atakPluginModule;
