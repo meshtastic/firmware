2a1d42b473787cf2e74a6ee74fe11174ded9f5bc
Ben Meadors <benmmeadors@gmail.com>
2024-02-08 21:28:13 -0600

    S

--- src/modules/AtakPluginModule.cpp
+++ src/modules/AtakPluginModule.cpp
@@ -6,7 +6,8 @@
 #include "main.h"
 #include "meshtastic/atak.pb.h"
 
-extern "C" {
+extern "C"
+{
 #include "mesh/compression/unishox2.h"
 }
 
@@ -35,33 +36,39 @@ bool AtakPluginModule::handleReceivedProtobuf(const meshtastic_MeshPacket &mp, m
 void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtastic_TAKPacket *t)
 {
     // From Phone (EUD)
-    if (mp.from == 0) {
+    if (mp.from == 0)
+    {
         LOG_DEBUG("Received uncompressed TAK payload from phone with %d bytes\n", mp.decoded.payload.size);
         // Compress for LoRA transport
         meshtastic_TAKPacket compressed = *t;
         compressed.is_compressed = true;
-        if (t->has_contact) {
+        if (t->has_contact)
+        {
             auto length = unishox2_compress_simple(t->contact.callsign, sizeof(t->contact.callsign), compressed.contact.callsign);
-            LOG_DEBUG("Uncompressed callsign %d bytes\n", strlen(t->contact.callsign));
-            LOG_DEBUG("Compressed callsign %d bytes\n", length);
+            LOG_DEBUG("Uncompressed callsign '%s' - %d bytes\n", t->contact.callsign, strlen(t->contact.callsign));
+            LOG_DEBUG("Compressed callsign '%s' - %d bytes\n", t->contact.callsign, length);
 
             length = unishox2_compress_simple(t->contact.device_callsign, sizeof(t->contact.device_callsign),
                                               compressed.contact.device_callsign);
-            LOG_DEBUG("Uncompressed device_callsign %d bytes\n", strlen(t->contact.device_callsign));
-            LOG_DEBUG("Compressed device_callsign %d bytes\n", length);
+            LOG_DEBUG("Uncompressed device_callsign '%s' - %d bytes\n", t->contact.device_callsign, strlen(t->contact.device_callsign));
+            LOG_DEBUG("Compressed device_callsign '%s' - %d bytes\n", compressed.contact.device_callsign, length);
         }
 
-        if (compressed.which_payload_variant == meshtastic_TAKPacket_chat_tag) {
+        if (compressed.which_payload_variant == meshtastic_TAKPacket_chat_tag)
+        {
             auto length = unishox2_compress_simple(t->payload_variant.chat.message, sizeof(t->payload_variant.chat.message),
                                                    compressed.payload_variant.chat.message);
-            LOG_DEBUG("Uncompressed chat message %d bytes\n", strlen(compressed.payload_variant.chat.message));
-            LOG_DEBUG("Compressed chat message %d bytes\n", length);
+            LOG_DEBUG("Uncompressed chat message '%s' - %d bytes\n", t->payload_variant.chat.message, strlen(t->payload_variant.chat.message));
+            LOG_DEBUG("Compressed chat message '%s' - %d bytes\n", compressed.payload_variant.chat.message, length);
         }
         mp.decoded.payload.size = pb_encode_to_bytes(mp.decoded.payload.bytes, sizeof(mp.decoded.payload.bytes),
                                                      meshtastic_TAKPacket_fields, &compressed);
         LOG_DEBUG("Final payload size of %d bytes\n", mp.decoded.payload.size);
-    } else {
-        if (!t->is_compressed) {
+    }
+    else
+    {
+        if (!t->is_compressed)
+        {
             // Not compressed. Something is wrong
             LOG_ERROR("Received uncompressed TAKPacket over radio!\n");
             return;
@@ -70,7 +77,8 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
         // Decompress for Phone (EUD)
         auto decompressedCopy = packetPool.allocCopy(mp);
         meshtastic_TAKPacket uncompressed = *t;
-        if (t->has_contact) {
+        if (t->has_contact)
+        {
             auto length =
                 unishox2_decompress_simple(t->contact.callsign, sizeof(t->contact.callsign), uncompressed.contact.callsign);
 
@@ -83,7 +91,8 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
             LOG_DEBUG("Compressed device_callsign: %d bytes\n", strlen(t->contact.device_callsign));
             LOG_DEBUG("Decompressed device_callsign: '%s' @ %d bytes\n", uncompressed.contact.device_callsign, length);
         }
-        if (uncompressed.which_payload_variant == meshtastic_TAKPacket_chat_tag) {
+        if (uncompressed.which_payload_variant == meshtastic_TAKPacket_chat_tag)
+        {
             auto length = unishox2_decompress_simple(t->payload_variant.chat.message, sizeof(t->payload_variant.chat.message),
                                                      uncompressed.payload_variant.chat.message);
             LOG_DEBUG("Compressed chat message: %d bytes\n", strlen(t->payload_variant.chat.message));
