1ff979235322ad6fed5badd01f4775b4adff5d05
Ben Meadors <benmmeadors@gmail.com>
2024-02-10 11:20:44 -0600

    Fix missing (to) from ATAK geochat

--- src/modules/AtakPluginModule.cpp
+++ src/modules/AtakPluginModule.cpp
@@ -6,7 +6,8 @@
 #include "main.h"
 #include "meshtastic/atak.pb.h"
 
-extern "C" {
+extern "C"
+{
 #include "mesh/compression/unishox2.h"
 }
 
@@ -35,23 +36,29 @@ bool AtakPluginModule::handleReceivedProtobuf(const meshtastic_MeshPacket &mp, m
 meshtastic_TAKPacket AtakPluginModule::cloneTAKPacketData(meshtastic_TAKPacket *t)
 {
     meshtastic_TAKPacket clone = meshtastic_TAKPacket_init_zero;
-    if (t->has_group) {
+    if (t->has_group)
+    {
         clone.has_group = true;
         clone.group = t->group;
     }
-    if (t->has_status) {
+    if (t->has_status)
+    {
         clone.has_status = true;
         clone.status = t->status;
     }
-    if (t->has_contact) {
+    if (t->has_contact)
+    {
         clone.has_contact = true;
         clone.contact = {0};
     }
 
-    if (t->which_payload_variant == meshtastic_TAKPacket_pli_tag) {
+    if (t->which_payload_variant == meshtastic_TAKPacket_pli_tag)
+    {
         clone.which_payload_variant = meshtastic_TAKPacket_pli_tag;
         clone.payload_variant.pli = t->payload_variant.pli;
-    } else if (t->which_payload_variant == meshtastic_TAKPacket_chat_tag) {
+    }
+    else if (t->which_payload_variant == meshtastic_TAKPacket_chat_tag)
+    {
         clone.which_payload_variant = meshtastic_TAKPacket_chat_tag;
         clone.payload_variant.chat = {0};
     }
@@ -62,12 +69,14 @@ meshtastic_TAKPacket AtakPluginModule::cloneTAKPacketData(meshtastic_TAKPacket *
 void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtastic_TAKPacket *t)
 {
     // From Phone (EUD)
-    if (mp.from == 0) {
+    if (mp.from == 0)
+    {
         LOG_DEBUG("Received uncompressed TAK payload from phone with %d bytes\n", mp.decoded.payload.size);
         // Compress for LoRA transport
         auto compressed = cloneTAKPacketData(t);
         compressed.is_compressed = true;
-        if (t->has_contact) {
+        if (t->has_contact)
+        {
             auto length = unishox2_compress_simple(t->contact.callsign, strlen(t->contact.callsign), compressed.contact.callsign);
             LOG_DEBUG("Uncompressed callsign '%s' - %d bytes\n", t->contact.callsign, strlen(t->contact.callsign));
             LOG_DEBUG("Compressed callsign '%s' - %d bytes\n", t->contact.callsign, length);
@@ -78,18 +87,30 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
                       strlen(t->contact.device_callsign));
             LOG_DEBUG("Compressed device_callsign '%s' - %d bytes\n", compressed.contact.device_callsign, length);
         }
-        if (t->which_payload_variant == meshtastic_TAKPacket_chat_tag) {
+        if (t->which_payload_variant == meshtastic_TAKPacket_chat_tag)
+        {
             auto length = unishox2_compress_simple(t->payload_variant.chat.message, strlen(t->payload_variant.chat.message),
                                                    compressed.payload_variant.chat.message);
             LOG_DEBUG("Uncompressed chat message '%s' - %d bytes\n", t->payload_variant.chat.message,
                       strlen(t->payload_variant.chat.message));
             LOG_DEBUG("Compressed chat message '%s' - %d bytes\n", compressed.payload_variant.chat.message, length);
+
+            if (t->payload_variant.chat.has_to)
+            {
+                length = unishox2_compress_simple(t->payload_variant.chat.to, strlen(t->payload_variant.chat.to),
+                                                  compressed.payload_variant.chat.to);
+                LOG_DEBUG("Uncompressed chat to '%s' - %d bytes\n", t->payload_variant.chat.to, strlen(t->payload_variant.chat.to));
+                LOG_DEBUG("Compressed chat to '%s' - %d bytes\n", compressed.payload_variant.chat.to, length);
+            }
         }
         mp.decoded.payload.size = pb_encode_to_bytes(mp.decoded.payload.bytes, sizeof(mp.decoded.payload.bytes),
                                                      meshtastic_TAKPacket_fields, &compressed);
         LOG_DEBUG("Final payload size of %d bytes\n", mp.decoded.payload.size);
-    } else {
-        if (!t->is_compressed) {
+    }
+    else
+    {
+        if (!t->is_compressed)
+        {
             // Not compressed. Something is wrong
             LOG_ERROR("Received uncompressed TAKPacket over radio!\n");
             return;
@@ -99,7 +120,8 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
         auto decompressedCopy = packetPool.allocCopy(mp);
         auto uncompressed = cloneTAKPacketData(t);
         uncompressed.is_compressed = false;
-        if (t->has_contact) {
+        if (t->has_contact)
+        {
             auto length =
                 unishox2_decompress_simple(t->contact.callsign, strlen(t->contact.callsign), uncompressed.contact.callsign);
 
@@ -112,11 +134,20 @@ void AtakPluginModule::alterReceivedProtobuf(meshtastic_MeshPacket &mp, meshtast
             LOG_DEBUG("Compressed device_callsign: %d bytes\n", strlen(t->contact.device_callsign));
             LOG_DEBUG("Decompressed device_callsign: '%s' @ %d bytes\n", uncompressed.contact.device_callsign, length);
         }
-        if (uncompressed.which_payload_variant == meshtastic_TAKPacket_chat_tag) {
+        if (uncompressed.which_payload_variant == meshtastic_TAKPacket_chat_tag)
+        {
             auto length = unishox2_decompress_simple(t->payload_variant.chat.message, strlen(t->payload_variant.chat.message),
                                                      uncompressed.payload_variant.chat.message);
             LOG_DEBUG("Compressed chat message: %d bytes\n", strlen(t->payload_variant.chat.message));
             LOG_DEBUG("Decompressed chat message: '%s' @ %d bytes\n", uncompressed.payload_variant.chat.message, length);
+
+            if (t->payload_variant.chat.has_to)
+            {
+                length = unishox2_decompress_simple(t->payload_variant.chat.to, strlen(t->payload_variant.chat.to),
+                                                    uncompressed.payload_variant.chat.to);
+                LOG_DEBUG("Compressed chat to: %d bytes\n", strlen(t->payload_variant.chat.to));
+                LOG_DEBUG("Decompressed chat to: '%s' @ %d bytes\n", uncompressed.payload_variant.chat.to, length);
+            }
         }
         decompressedCopy->decoded.payload.size =
             pb_encode_to_bytes(decompressedCopy->decoded.payload.bytes, sizeof(decompressedCopy->decoded.payload),
