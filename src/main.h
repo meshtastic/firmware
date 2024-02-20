b888f5cac08163516c08ca990fe26a2de8d938df
Gabriele Russo <gabryr96@gmail.com>
2024-02-20 14:27:48 +0100

    Fixes [3074] Heltec Tracker Screen issues + minor fixes (#3213)
    * Fix Heltec Tracker Screen issues
    
    Fix Heltec Tracker Screen issues like wrong offsets, display size and screen not shutting down.
    
    Divides board into two different envs for 1.0 and 1.1 version PCB
    
    * Helteck wireless tracker default version V1_1
    
    * rename heltec tracker 1.1 - trunk fmt
    
    rename varian of heltec tracker 1.1 to "heltec tracker" to be retro-compatible.
    
    Trunk formatting.
    
    * Heltec Tracker increase Screen update to 3Hz
    
    Heltec Tracker increase Screen update to 3Hz from 1Hz

--- src/main.h
+++ src/main.h
@@ -70,8 +70,6 @@ extern uint32_t shutdownAtMsec;
 
 extern uint32_t serialSinceMsec;
 
-extern int heltec_version;
-
 // If a thread does something that might need for it to be rescheduled ASAP it can set this flag
 // This will suppress the current delay and instead try to run ASAP.
 extern bool runASAP;
