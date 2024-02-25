d434117ffdded1e1dbb118264c546fafab097375
Thomas Herrmann <mail@thoherr.de>
2024-02-25 20:03:54 +0100

    add UI frame for PaxCounter module (#3285)
    * add UI frame to display PaxCounter module data
    
    * only acquire screen when paxcounter is active, i.e. enabled and wifi and ble are both off
    
    * sync font #define with other occurrences in code
    
    * protect screen specific code with #if HAS_SCREEN
    
    * limit upload_speed to 115200 for TLORA_V2_1_16
    
    * fix failing trunk checks; sorry
    
    * Revert "limit upload_speed to 115200 for TLORA_V2_1_16"
    
    This reverts commit 4eb549c5e886508a4d39a7bfe689bc1976cbaa4b.
    
    ---------
    
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>

--- src/modules/esp32/PaxcounterModule.h
+++ src/modules/esp32/PaxcounterModule.h
@@ -23,6 +23,11 @@ class PaxcounterModule : private concurrency::OSThread, public ProtobufModule<me
     bool sendInfo(NodeNum dest = NODENUM_BROADCAST);
     virtual bool handleReceivedProtobuf(const meshtastic_MeshPacket &mp, meshtastic_Paxcount *p) override;
     virtual meshtastic_MeshPacket *allocReply() override;
+    bool isActive() { return moduleConfig.paxcounter.enabled && !config.bluetooth.enabled && !config.network.wifi_enabled; }
+#if HAS_SCREEN
+    virtual bool wantUIFrame() override { return isActive(); }
+    virtual void drawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) override;
+#endif
 };
 
 extern PaxcounterModule *paxcounterModule;
