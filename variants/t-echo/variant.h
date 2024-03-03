c7b92824be5331c530d8aebff5a50553070a7cd3
Ben Meadors <benmmeadors@gmail.com>
2024-03-03 08:46:20 -0600

    Remove / comment out unused LED pins

--- variants/t-echo/variant.h
+++ variants/t-echo/variant.h
@@ -44,15 +44,11 @@ extern "C" {
 
 // LEDs
 #define PIN_LED1 (0 + 14) // 13 red (confirmed on 1.0 board)
-#define PIN_LED2 (0 + 15) // 14 blue
-#define PIN_LED3 (0 + 13) // 15 green
+// Unused(by firmware) LEDs:
+// #define PIN_LED2 (0 + 15) // 14 blue
+// #define PIN_LED3 (0 + 13) // 15 green
 
-#define LED_RED PIN_LED3
-#define LED_BLUE PIN_LED1
-#define LED_GREEN PIN_LED2
-
-#define LED_BUILTIN LED_BLUE
-#define LED_CONN PIN_GREEN
+#define LED_BUILTIN PIN_LED1
 
 #define LED_STATE_ON 0 // State when LED is lit
 #define LED_INVERTED 1
