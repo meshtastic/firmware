beeb24df68f24f36d22540bcb70454a345679b67
Thomas Herrmann <mail@thoherr.de>
2024-03-06 19:15:04 +0100

    add some documentation, cleanup

--- src/modules/esp32/PaxcounterModule.h
+++ src/modules/esp32/PaxcounterModule.h
@@ -8,17 +8,18 @@
 #include <libpax_api.h>
 
 /**
- * A simple example module that just replies with "Message received" to any message it receives.
+ * Wrapper module for the estimate passenger (PAX) count library (https://github.com/dbinfrago/libpax) which
+ * implements the core functionality of the ESP32 Paxcounter project (https://github.com/cyberman54/ESP32-Paxcounter)
  */
 class PaxcounterModule : private concurrency::OSThread, public ProtobufModule<meshtastic_Paxcount>
 {
     bool firstTime = true;
     bool reportedDataSent = true;
 
+    static void handlePaxCounterReportRequest();
+
   public:
     PaxcounterModule();
-
-    static void handlePaxCounterReportRequest();
 
   protected:
     struct count_payload_t count_from_libpax = {0, 0, 0};
