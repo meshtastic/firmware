b3ec3c20fbd80081328e44e7d4ce084afd42fa58
David Ellefsen <david@ellefsen.co.za>
2024-03-07 16:31:20 +0200

    Update device-install.sh files to account for bleota-c3.bin file

--- bin/device-install.bat
+++ bin/device-install.bat
@@ -31,9 +31,13 @@ IF EXIST %FILENAME% IF x%FILENAME:update=%==x%FILENAME% (
 	%PYTHON% -m esptool --baud 115200 erase_flash
 	%PYTHON% -m esptool --baud 115200 write_flash 0x00 %FILENAME%
     
-    @REM Account for S3 board's different OTA partition
+    @REM Account for S3 and C3 board's different OTA partition
     IF x%FILENAME:s3=%==x%FILENAME% IF x%FILENAME:v3=%==x%FILENAME% IF x%FILENAME:t-deck=%==x%FILENAME% IF x%FILENAME:wireless-paper=%==x%FILENAME% IF x%FILENAME:wireless-tracker=%==x%FILENAME% (
-        %PYTHON% -m esptool --baud 115200 write_flash 0x260000 bleota.bin
+        IF x%FILENAME:esp32c3=%==x%FILENAME% (
+            %PYTHON% -m esptool --baud 115200 write_flash 0x260000 bleota.bin
+        ) else (
+            %PYTHON% -m esptool --baud 115200 write_flash 0x260000 bleota-c3.bin
+        )
 	) else (
         %PYTHON% -m esptool --baud 115200 write_flash 0x260000 bleota-s3.bin
     )
