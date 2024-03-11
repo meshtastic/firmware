86303066108a447defb0b72d3294bd8650548b43
Thomas Göttgens <tgoettgens@gmail.com>
2024-03-11 13:49:04 +0100

    trunk fmt

--- variants/heltec_hri485_2/variant.h
+++ variants/heltec_hri485_2/variant.h
@@ -3,17 +3,17 @@
 
 #define HAS_ETHERNET 1
 
-#define ETH_TYPE                        ETH_PHY_RTL8201
-#define ETH_ADDR                        0
-#define ETH_CLK_MODE                    ETH_CLOCK_GPIO16_OUT
-#define ETH_RESET_PIN                   -1
-#define ETH_MDC_PIN                     23
-#define ETH_POWER_PIN                   12
-#define ETH_MDIO_PIN                    18
-#define SD_MISO_PIN                     34
-#define SD_MOSI_PIN                     13
-#define SD_SCLK_PIN                     14
-#define SD_CS_PIN                       5
+#define ETH_TYPE ETH_PHY_RTL8201
+#define ETH_ADDR 0
+#define ETH_CLK_MODE ETH_CLOCK_GPIO16_OUT
+#define ETH_RESET_PIN -1
+#define ETH_MDC_PIN 23
+#define ETH_POWER_PIN 12
+#define ETH_MDIO_PIN 18
+#define SD_MISO_PIN 34
+#define SD_MOSI_PIN 13
+#define SD_SCLK_PIN 14
+#define SD_CS_PIN 5
 
 #define USE_SX1262
 
