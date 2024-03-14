684a3775999e185a1b4a42daab13eb0a323ee447
Ben Meadors <benmmeadors@gmail.com>
2024-03-14 17:00:57 -0500

    Try-fix router missed messages (#3405)

--- src/PowerFSM.cpp
+++ src/PowerFSM.cpp
@@ -358,10 +358,10 @@ void PowerFSM_setup()
     // Don't add power saving transitions if we are a power saving tracker or sensor. Sleep will be initiatiated through the
     // modules
     if ((isRouter || config.power.is_power_saving) && !isTrackerOrSensor) {
-        powerFSM.add_timed_transition(&stateNB, isInfrastructureRole ? &stateSDS : &stateLS,
+        powerFSM.add_timed_transition(&stateNB, &stateLS,
                                       getConfiguredOrDefaultMs(config.power.min_wake_secs, default_min_wake_secs), NULL,
                                       "Min wake timeout");
-        powerFSM.add_timed_transition(&stateDARK, isInfrastructureRole ? &stateSDS : &stateLS,
+        powerFSM.add_timed_transition(&stateDARK, &stateLS,
                                       getConfiguredOrDefaultMs(config.power.wait_bluetooth_secs, default_wait_bluetooth_secs),
                                       NULL, "Bluetooth timeout");
     }
