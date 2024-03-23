2e210c25381e4e9c641f6ee44b86aff02a7bf91f
GUVWAF <78759985+GUVWAF@users.noreply.github.com>
2024-03-23 18:38:29 +0100

    Set unused header bytes to zero for future use (#3479)

--- src/mesh/RadioInterface.h
+++ src/mesh/RadioInterface.h
@@ -34,6 +34,12 @@ typedef struct {
 
     /** The channel hash - used as a hint for the decoder to limit which channels we consider */
     uint8_t channel;
+
+    // ***For future use*** Last byte of the NodeNum of the next-hop for this packet
+    uint8_t next_hop;
+
+    // ***For future use*** Last byte of the NodeNum of the node that will relay/relayed this packet
+    uint8_t relay_node;
 } PacketHeader;
 
 /**
