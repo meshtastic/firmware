7b4ad3c6eddb4ff2799029d97c36299334639bee
todd-herbert <herbert.todd@gmail.com>
2024-04-07 02:04:26 +1300

    Remap backlight toggle and touch button (#3560)
    * Update E-Ink display after sending adhoc ping or disable/enable GPS
    
    * Resume display updates when touch button pressed
    
    * Use touch hold as modifier; change double-click behavior for user button
    
    * Fix preprocessor exclusions
    
    * Purge backlight behavior
    
    * Distinguish between 3x and 4x multi-presses
    
    * Touch button considers "Wake screen on tap or motion" user-setting
    
    * Don't assume device has BUTTON_PIN
    
    * Rename misleading method

--- src/ButtonThread.cpp
+++ src/ButtonThread.cpp
@@ -48,7 +48,7 @@ ButtonThread::ButtonThread() : OSThread("Button")
     userButton.setPressMs(c_longPressTime);
     userButton.setDebounceMs(1);
     userButton.attachDoubleClick(userButtonDoublePressed);
-    userButton.attachMultiClick(userButtonMultiPressed);
+    userButton.attachMultiClick(userButtonMultiPressed, this); // Reference to instance: get click count from non-static OneButton
 #ifndef T_DECK // T-Deck immediately wakes up after shutdown, so disable this function
     userButton.attachLongPressStart(userButtonPressedLongStart);
     userButton.attachLongPressStop(userButtonPressedLongStop);
@@ -86,7 +86,8 @@ ButtonThread::ButtonThread() : OSThread("Button")
 
 #ifdef BUTTON_PIN_TOUCH
     userButtonTouch = OneButton(BUTTON_PIN_TOUCH, true, true);
-    userButtonTouch.attachClick(touchPressed);
+    userButtonTouch.setPressMs(400);
+    userButtonTouch.attachLongPressStart(touchPressedLongStart); // Better handling with longpress than click?
     wakeOnIrq(BUTTON_PIN_TOUCH, FALLING);
 #endif
 }
@@ -138,26 +139,42 @@ int32_t ButtonThread::runOnce()
 
         case BUTTON_EVENT_DOUBLE_PRESSED: {
             LOG_BUTTON("Double press!\n");
-#if defined(USE_EINK) && defined(PIN_EINK_EN)
-            digitalWrite(PIN_EINK_EN, digitalRead(PIN_EINK_EN) == LOW);
-#endif
             service.refreshLocalMeshNode();
             service.sendNetworkPing(NODENUM_BROADCAST, true);
-            if (screen)
+            if (screen) {
                 screen->print("Sent ad-hoc ping\n");
-            break;
-        }
-#if HAS_GPS
-        case BUTTON_EVENT_MULTI_PRESSED: {
-            LOG_BUTTON("Multi press!\n");
-            if (!config.device.disable_triple_click && (gps != nullptr)) {
-                gps->toggleGpsMode();
-                if (screen)
-                    screen->forceDisplay();
+                screen->forceDisplay(true); // Force a new UI frame, then force an EInk update
             }
             break;
         }
+
+        case BUTTON_EVENT_MULTI_PRESSED: {
+            LOG_BUTTON("Mulitipress! %hux\n", multipressClickCount);
+            switch (multipressClickCount) {
+#if HAS_GPS
+            // 3 clicks: toggle GPS
+            case 3:
+                if (!config.device.disable_triple_click && (gps != nullptr)) {
+                    gps->toggleGpsMode();
+                    if (screen)
+                        screen->forceDisplay(true); // Force a new UI frame, then force an EInk update
+                }
+                break;
 #endif
+#if defined(USE_EINK) && defined(PIN_EINK_EN) // i.e. T-Echo
+            // 4 clicks: toggle backlight
+            case 4:
+                digitalWrite(PIN_EINK_EN, digitalRead(PIN_EINK_EN) == LOW);
+                break;
+#endif
+            // No valid multipress action
+            default:
+                break;
+            } // end switch: click count
+
+            break;
+        } // end multipress event
+
         case BUTTON_EVENT_LONG_PRESSED: {
             LOG_BUTTON("Long press!\n");
             powerFSM.trigger(EVENT_PRESS);
@@ -176,12 +193,24 @@ int32_t ButtonThread::runOnce()
             power->shutdown();
             break;
         }
-        case BUTTON_EVENT_TOUCH_PRESSED: {
+
+#ifdef BUTTON_PIN_TOUCH
+        case BUTTON_EVENT_TOUCH_LONG_PRESSED: {
             LOG_BUTTON("Touch press!\n");
-            if (screen)
-                screen->forceDisplay();
+            if (config.display.wake_on_tap_or_motion) {
+                if (screen) {
+                    // Wake if asleep
+                    if (powerFSM.getState() == &stateDARK)
+                        powerFSM.trigger(EVENT_PRESS);
+
+                    // Update display (legacy behaviour)
+                    screen->forceDisplay();
+                }
+            }
             break;
         }
+#endif // BUTTON_PIN_TOUCH
+
         default:
             break;
         }
@@ -204,6 +233,25 @@ void ButtonThread::wakeOnIrq(int irq, int mode)
             mainDelay.interruptFromISR(&higherWake);
         },
         FALLING);
+}
+
+// Static callback
+void ButtonThread::userButtonMultiPressed(void *callerThread)
+{
+    // Grab click count from non-static button, while the info is still valid
+    ButtonThread *thread = (ButtonThread *)callerThread;
+    thread->storeClickCount();
+
+    // Then handle later, in the usual way
+    btnEvent = BUTTON_EVENT_MULTI_PRESSED;
+}
+
+// Non-static method, runs during callback. Grabs info while still valid
+void ButtonThread::storeClickCount()
+{
+#ifdef BUTTON_PIN
+    multipressClickCount = userButton.getNumberClicks();
+#endif
 }
 
 void ButtonThread::userButtonPressedLongStart()
