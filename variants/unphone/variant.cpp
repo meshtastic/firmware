54defe005f5b0a4a3fed1c8a394e8df11ac3787e
Hamish Cunningham <hamish@gate.ac.uk>
2024-04-07 22:45:47 +0100

    cleaned up initVariant a little

--- variants/unphone/variant.cpp
+++ variants/unphone/variant.cpp
@@ -1,17 +1,17 @@
 // meshtastic/firmware/variants/unphone/variant.cpp
 
 #include "unPhone.h"
-
 unPhone u = unPhone("meshtastic_unphone");
 
 void initVariant() {
-  u.begin();
+  u.begin();                    // initialise hardware etc.
   u.store(u.buildTime);
-  u.printWakeupReason();        // what woke us up? stored, not printed :|
+  u.printWakeupReason();        // what woke us up? (stored, not printed :|)
   u.checkPowerSwitch();         // if power switch is off, shutdown
 
-  // buzz a bit
-  for(int i = 0; i < 3; i++) {
+  for(int i = 0; i < 3; i++) {  // buzz a bit
     u.vibe(true);  delay(150); u.vibe(false); delay(150);
   }
+  u.printStore();               // print stored messages to Serial
+  Serial.flush();
 }
