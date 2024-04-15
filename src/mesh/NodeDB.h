4593cfa36dccd859dfc6bbb3ca20b93fd051ca5f
Ben Meadors <benmmeadors@gmail.com>
2024-04-15 07:22:05 -0500

    Add LoadFileState to differentiate types of success / failures (#3625)

--- src/mesh/NodeDB.h
+++ src/mesh/NodeDB.h
@@ -38,6 +38,19 @@ uint32_t sinceLastSeen(const meshtastic_NodeInfoLite *n);
 /// Given a packet, return how many seconds in the past (vs now) it was received
 uint32_t sinceReceived(const meshtastic_MeshPacket *p);
 
+enum LoadFileResult {
+    // Successfully opened the file
+    SUCCESS = 1,
+    // File does not exist
+    NOT_FOUND = 2,
+    // Device does not have a filesystem
+    NO_FILESYSTEM = 3,
+    // File exists, but could not decode protobufs
+    DECODE_FAILED = 4,
+    // File exists, but open failed for some reason
+    OTHER_FAILURE = 5
+};
+
 class NodeDB
 {
     // NodeNum provisionalNodeNum; // if we are trying to find a node num this is our current attempt
@@ -115,7 +128,8 @@ class NodeDB
 
     bool factoryReset();
 
-    bool loadProto(const char *filename, size_t protoSize, size_t objSize, const pb_msgdesc_t *fields, void *dest_struct);
+    LoadFileResult loadProto(const char *filename, size_t protoSize, size_t objSize, const pb_msgdesc_t *fields,
+                             void *dest_struct);
     bool saveProto(const char *filename, size_t protoSize, const pb_msgdesc_t *fields, const void *dest_struct);
 
     void installRoleDefaults(meshtastic_Config_DeviceConfig_Role role);
