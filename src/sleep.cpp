4c406fca97b74e3a8071407107ec7bdefb238e1e
Todd Herbert <herbert.todd@gmail.com>
2024-04-20 16:10:17 +1200

    Apply behavior to blink during light sleep

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -85,6 +85,12 @@ void setCPUFast(bool on)
 
 void setLed(bool ledOn)
 {
+    // If LED has been repurposed for use with External Notification module, no blink
+    if (moduleConfig.external_notification.enabled &&
+        (moduleConfig.external_notification.alert_message || moduleConfig.external_notification.alert_bell) &&
+        (moduleConfig.external_notification.output == LED_PIN || moduleConfig.external_notification.output == 0))
+        return;
+
 #ifdef LED_PIN
     // toggle the led so we can get some rough sense of how often loop is pausing
     digitalWrite(LED_PIN, ledOn ^ LED_INVERTED);
