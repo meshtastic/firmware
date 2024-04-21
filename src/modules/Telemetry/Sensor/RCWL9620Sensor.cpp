402b0d7e0bb41c2437f94e346ab1f35b2d597345
Thomas Göttgens <tgoettgens@gmail.com>
2024-04-21 14:39:55 +0200

    ditch that no-good m5 dependancy and do it ourself

--- src/modules/Telemetry/Sensor/RCWL9620Sensor.cpp
+++ src/modules/Telemetry/Sensor/RCWL9620Sensor.cpp
@@ -11,8 +11,7 @@ int32_t RCWL9620Sensor::runOnce()
     if (!hasSensor()) {
         return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
     }
-    status = 1;
-    rcwl9620.begin(nodeTelemetrySensorsMap[sensorType].second, nodeTelemetrySensorsMap[sensorType].first, -1, -1);
+    status = begin(nodeTelemetrySensorsMap[sensorType].first, nodeTelemetrySensorsMap[sensorType].second);
     return initI2CSensor();
 }
 
@@ -21,6 +20,42 @@ void RCWL9620Sensor::setup() {}
 bool RCWL9620Sensor::getMetrics(meshtastic_Telemetry *measurement)
 {
     LOG_DEBUG("RCWL9620Sensor::getMetrics\n");
-    measurement->variant.environment_metrics.distance = rcwl9620.getDistance();
+    measurement->variant.environment_metrics.distance = getDistance();
     return true;
+}
+
+bool RCWL9620Sensor::begin(uint8_t addr, TwoWire *wire)
+{
+    _wire = wire;
+    _addr = addr;
+    if (i2c_dev)
+        delete i2c_dev;
+    i2c_dev = new Adafruit_I2CDevice(_addr, _wire);
+    if (!i2c_dev->begin())
+        return false;
+    return true;
+}
+
+float RCWL9620Sensor::getDistance()
+{
+    uint32_t data;
+    _wire->beginTransmission(_addr); // Transfer data to addr.
+    _wire->write(0x01);
+    _wire->endTransmission(); // Stop data transmission with the Ultrasonic
+                              // Unit.
+
+    _wire->requestFrom(_addr,
+                       (uint8_t)3); // Request 3 bytes from Ultrasonic Unit.
+
+    data = _wire->read();
+    data <<= 8;
+    data |= _wire->read();
+    data <<= 8;
+    data |= _wire->read();
+    float Distance = float(data) / 1000;
+    if (Distance > 4500.00) {
+        return 4500.00;
+    } else {
+        return Distance;
+    }
 }
