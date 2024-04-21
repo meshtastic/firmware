82a296452f66a41fa31f1b1b056e2cd0a95cd859
Thomas Göttgens <tgoettgens@gmail.com>
2024-04-21 14:39:55 +0200

    ditch that no-good m5 dependancy and do it ourself

--- src/modules/Telemetry/Sensor/RCWL9620Sensor.h
+++ src/modules/Telemetry/Sensor/RCWL9620Sensor.h
@@ -1,14 +1,18 @@
 #include "../mesh/generated/meshtastic/telemetry.pb.h"
 #include "TelemetrySensor.h"
-#include <Unit_Sonic.h>
+#include <Adafruit_I2CDevice.h>
 
 class RCWL9620Sensor : public TelemetrySensor
 {
   private:
-    SONIC_I2C rcwl9620;
+    uint8_t _addr;
+    TwoWire *_wire;
 
   protected:
     virtual void setup() override;
+    bool begin(uint8_t addr = 0x57, TwoWire *wire = &Wire);
+    Adafruit_I2CDevice *i2c_dev = NULL;
+    float getDistance();
 
   public:
     RCWL9620Sensor();
