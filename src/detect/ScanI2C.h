8a67079e60a224bf44cb8aa59095fbc273ad9d9b
Gareth Coleman <gareth@sublime.org>
2024-04-21 09:16:50 +0100

    lets just define it without guards!

--- src/detect/ScanI2C.h
+++ src/detect/ScanI2C.h
@@ -41,9 +41,7 @@ class ScanI2C
         BQ24295,
         LSM6DS3,
         TCA9555,
-#if defined(HAS_NCP5623) || defined(UNPHONE) || defined(RGBLED_RED)
         NCP5623,
-#endif
     } DeviceType;
 
     // typedef uint8_t DeviceAddress;
