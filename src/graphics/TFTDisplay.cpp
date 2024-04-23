bd85e4da14d09ab5a70998e315eb9383162a8c6d
Jonathan Bennett <jbennett@incomsystems.biz>
2024-04-22 22:48:27 -0500

    native: Make touch i2c address configurable

--- src/graphics/TFTDisplay.cpp
+++ src/graphics/TFTDisplay.cpp
@@ -396,8 +396,8 @@ class LGFX : public lgfx::LGFX_Device
             touch_cfg.pin_int = settingsMap[touchscreenIRQ];
             touch_cfg.bus_shared = true;
             touch_cfg.offset_rotation = 1;
-            if (settingsMap[touchscreenModule] == ft5x06) {
-                touch_cfg.i2c_addr = 0x38;
+            if (settingsMap[touchscreenI2CAddr] != -1) {
+                touch_cfg.i2c_addr = settingsMap[touchscreenI2CAddr];
             }
 
             _touch_instance->config(touch_cfg);
