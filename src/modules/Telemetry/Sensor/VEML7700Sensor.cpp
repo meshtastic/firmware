f19aa49eb230fbac41192201210c79597861be4d
Jorge Castillo <jorge@flamesense.tech>
2024-05-07 16:11:41 -0400

    add veml7700

---
+++ src/modules/Telemetry/Sensor/VEML7700Sensor.cpp
@@ -0,0 +1,34 @@
+#include "VEML7700Sensor.h"
+#include "../mesh/generated/meshtastic/telemetry.pb.h"
+#include "TelemetrySensor.h"
+#include "configuration.h"
+#include <Adafruit_VEML7700.h>
+#include <typeinfo>
+
+VEML7700Sensor::VEML7700Sensor() : TelemetrySensor(meshtastic_TelemetrySensorType_VEML7700, "VEML7700") {}
+
+int32_t VEML7700Sensor::runOnce()
+{
+    LOG_INFO("Init sensor: %s\n", sensorName);
+    if (!hasSensor()) {
+        return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
+    }
+    status = veml7700.begin(nodeTelemetrySensorsMap[sensorType].second);
+
+    veml7700.setLowThreshold(10000);
+    veml7700.setHighThreshold(20000);
+    veml7700.interruptEnable(true);
+
+    return initI2CSensor();
+}
+
+void VEML7700Sensor::setup() {}
+
+bool VEML7700Sensor::getMetrics(meshtastic_Telemetry *measurement)
+{
+    LOG_DEBUG("VEML7700Sensor::getMetrics\n");
+
+    measurement->variant.environment_metrics.lux = veml7700.readLux(VEML_LUX_AUTO);
+
+    return true;
+}
