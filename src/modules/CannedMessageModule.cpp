995feb2e1902fd702c7a1af2214b431e16bac95a
Nicholas <nbaddorf@gmail.com>
2024-05-07 07:45:16 -0400

    Added back the function I moved causing issue with versions

--- src/modules/CannedMessageModule.cpp
+++ src/modules/CannedMessageModule.cpp
@@ -483,6 +483,37 @@ int32_t CannedMessageModule::runOnce()
                 rebootAtMsec = millis() + DEFAULT_REBOOT_SECONDS * 1000;
                 runState = CANNED_MESSAGE_RUN_STATE_INACTIVE;
                 break;
+            case 0x9e: // toggle GPS like triple press does
+#if !MESHTASTIC_EXCLUDE_GPS
+                if (gps != nullptr) {
+                    gps->toggleGpsMode();
+                }
+                if (screen)
+                    screen->forceDisplay();
+                showTemporaryMessage("GPS Toggled");
+#endif
+                break;
+            // mute (switch off/toggle) external notifications on fn+m
+            case 0xac:
+                if (moduleConfig.external_notification.enabled == true) {
+                    if (externalNotificationModule->getMute()) {
+                        externalNotificationModule->setMute(false);
+                        showTemporaryMessage("Notifications \nEnabled");
+                    } else {
+                        externalNotificationModule->stopNow(); // this will turn off all GPIO and sounds and idle the loop
+                        externalNotificationModule->setMute(true);
+                        showTemporaryMessage("Notifications \nDisabled");
+                    }
+                }
+                break;
+            case 0xaf: // fn+space send network ping like double press does
+                service.refreshLocalMeshNode();
+                if (service.trySendPosition(NODENUM_BROADCAST, true)) {
+                    showTemporaryMessage("Position \nUpdate Sent");
+                } else {
+                    showTemporaryMessage("Node Info \nUpdate Sent");
+                }
+                break;
             default:
                 if (this->cursor == this->freetext.length()) {
                     this->freetext += this->payload;
