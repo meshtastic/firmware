96b5bd2fd0d432c0c2412533c82cd47e10f3a4a9
Thomas Göttgens <tgoettgens@gmail.com>
2024-05-11 15:20:11 +0200

    unphone has a display, don't default BLE PIN to 123456 (#3865)
    fixes #3822

--- src/mesh/NodeDB.cpp
+++ src/mesh/NodeDB.cpp
@@ -273,7 +273,7 @@ void NodeDB::installDefaultConfig()
     // FIXME: Default to bluetooth capability of platform as default
     config.bluetooth.enabled = true;
     config.bluetooth.fixed_pin = defaultBLEPin;
-#if defined(ST7735_CS) || defined(USE_EINK) || defined(ILI9341_DRIVER) || defined(ST7789_CS)
+#if defined(ST7735_CS) || defined(USE_EINK) || defined(ILI9341_DRIVER) || defined(ST7789_CS) || defined(HX8357_CS)
     bool hasScreen = true;
 #elif ARCH_PORTDUINO
     bool hasScreen = false;
