7771fce36ad6c0d7f8baf4cb937ce6d1eb908f76
Ben Meadors <benmmeadors@gmail.com>
2024-06-02 09:06:05 -0500

    Init

--- src/modules/Telemetry/Sensor/MLX90632Sensor.h
+++ src/modules/Telemetry/Sensor/MLX90632Sensor.h
@@ -8,16 +8,16 @@
 
 class MLX90632Sensor : public TelemetrySensor
 {
-  private:
-    MLX90632 mlx;
+private:
+  MLX90632 mlx = MLX90632();
 
-  protected:
-    virtual void setup() override;
+protected:
+  virtual void setup() override;
 
-  public:
-    MLX90632Sensor();
-    virtual int32_t runOnce() override;
-    virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
+public:
+  MLX90632Sensor();
+  virtual int32_t runOnce() override;
+  virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
 };
 
 #endif
