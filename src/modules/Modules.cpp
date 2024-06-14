a63eced168d8c917bed82ab2e7db19baf8cd902a
Ben Meadors <benmmeadors@gmail.com>
2024-06-14 08:40:17 -0500

    Exclude from header

--- src/modules/Modules.cpp
+++ src/modules/Modules.cpp
@@ -70,7 +70,10 @@
 #include "modules/SerialModule.h"
 #endif
 #endif
+
+#if !MESHTASTIC_EXCLUDE_DROPZONE
 #include "modules/DropzoneModule.h"
+#endif
 
 /**
  * Create module instances here.  If you are adding a new module, you must 'new' it here (or somewhere else)
