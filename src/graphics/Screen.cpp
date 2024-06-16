c159603ef1733908a4c10fd681a2a5377493a9f3
Alexander <156134901+Dorn8010@users.noreply.github.com>
2024-06-17 00:42:08 +0200

    Update Screen.cpp
    snprintf - Safety related

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -314,12 +314,12 @@ static void drawDeepSleepScreen(OLEDDisplay *display, OLEDDisplayUiState *state,
     LOG_DEBUG("Drawing deep sleep screen\n");
     
     // Check if idText is not assigned OR empty OR has non printable characters
-    if (!idText || strlen(idText) == 0 || !(haveGlyphs(idText)) ) {
+    if (!idText || strlen(idText) == 0 || !haveGlyphs(idText)) {
         drawIconScreen("Sleeping", display, state, x, y);
     } else {
-        // Create a new string that is the concatenation of idText and " sleeping."
+        // Create a new string that is the concatenation of idText and " sleeping"
         char displayStr[30]; // Declare a character array for the display string 5+buffer for changes
-        sprintf(displayStr, "%s sleeping", idText);
+        snprintf(displayStr, sizeof(displayStr), "%s sleeping", idText); // Use snprintf for safety
 
         // Display displayStr on the screen
         drawIconScreen(displayStr, display, state, x, y);
