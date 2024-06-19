a151f608d82c2f2feb42634948ec07a6e338e304
Mikhael Skvortsov <mikhael.skvortsov@gmail.com>
2024-06-19 17:15:53 +0300

    Fix protobuf structs handling

--- src/main.cpp
+++ src/main.cpp
@@ -173,7 +173,7 @@ const char *getDeviceName()
     static char name[20];
     snprintf(name, sizeof(name), "%02x%02x", dmac[4], dmac[5]);
     // if the shortname exists and is NOT the new default of ab3c, use it for BLE name.
-    if ((owner.short_name != NULL) && (strcmp(owner.short_name, name) != 0)) {
+    if (strcmp(owner.short_name, name) != 0) {
         snprintf(name, sizeof(name), "%s_%02x%02x", owner.short_name, dmac[4], dmac[5]);
     } else {
         snprintf(name, sizeof(name), "Meshtastic_%02x%02x", dmac[4], dmac[5]);
