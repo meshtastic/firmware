23ac6b65141914496ba108580cf0377f538db2e4
Warren Guy <5602790+warrenguy@users.noreply.github.com>
2024-06-23 21:40:13 +0100

    fix INA3221 sensor (#4168)
    - pass wire to begin()
    - remove redundant setAddr() (already set in header)

--- src/modules/Telemetry/Sensor/INA3221Sensor.cpp
+++ src/modules/Telemetry/Sensor/INA3221Sensor.cpp
@@ -16,8 +16,7 @@ int32_t INA3221Sensor::runOnce()
         return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
     }
     if (!status) {
-        ina3221.setAddr(INA3221_ADDR42_SDA); // i2c address 0x42
-        ina3221.begin();
+        ina3221.begin(nodeTelemetrySensorsMap[sensorType].second);
         ina3221.setShuntRes(100, 100, 100); // 0.1 Ohm shunt resistors
         status = true;
     } else {
