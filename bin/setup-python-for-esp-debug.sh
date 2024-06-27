232e9d93b6cc5de083602f5704d855409ea11af1
Kevin Hester <kevinh@geeksville.com>
2024-06-27 08:38:16 -0700

    fix bogus shellcheck warning

--- bin/setup-python-for-esp-debug.sh
+++ bin/setup-python-for-esp-debug.sh
@@ -1,3 +1,6 @@
+# shellcheck shell=bash
+# (this minor script is actually shell agnostic, and is intended to be sourced rather than run in a subshell)
+
 # This is a little script you can source if you want to make ESP debugging work on a modern (24.04) ubuntu machine
 # It assumes you have built and installed python 2.7 from source with:
 # ./configure --enable-optimizations --enable-shared --enable-unicode=ucs4
