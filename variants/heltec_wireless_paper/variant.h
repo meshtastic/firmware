5fa5b81e6dcd49666e16d4adc07914df53bafd57
Todd Herbert <herbert.todd@gmail.com>
2024-07-06 19:08:48 +1200

    Change deprecated ADC attenuation parameter From 11dB to 12dB. Resolves compiler warning. Allegly, no impact on function: `This is deprecated, it behaves the same as `ADC_ATTEN_DB_12`

--- variants/heltec_wireless_paper/variant.h
+++ variants/heltec_wireless_paper/variant.h
@@ -27,7 +27,7 @@
 #define ADC_CHANNEL ADC2_GPIO20_CHANNEL
 #define ADC_MULTIPLIER 2                // Voltage divider is roughly 1:1
 #define BAT_MEASURE_ADC_UNIT 2          // Use ADC2
-#define ADC_ATTENUATION ADC_ATTEN_DB_11 // Voltage divider output is quite high
+#define ADC_ATTENUATION ADC_ATTEN_DB_12 // Voltage divider output is quite high
 
 // LoRa
 #define USE_SX1262
