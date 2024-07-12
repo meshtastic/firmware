dc3fe60ae08efa2d902c14be2f0163d262a10b69
Ben Meadors <benmmeadors@gmail.com>
2024-07-12 13:55:28 -0500

    Moved back to bottom

--- src/mesh/Default.cpp
+++ src/mesh/Default.cpp
@@ -1,10 +1,11 @@
 #include "Default.h"
 
-uint32_t Default::getConfiguredOrDefault(uint32_t configured, uint32_t defaultValue)
+
+uint32_t Default::getConfiguredOrDefaultMs(uint32_t configuredInterval, uint32_t defaultInterval)
 {
-    if (configured > 0)
-        return configured;
-    return defaultValue;
+    if (configuredInterval > 0)
+        return configuredInterval * 1000;
+    return defaultInterval * 1000;
 }
 
 uint32_t Default::getConfiguredOrDefaultMs(uint32_t configuredInterval)
@@ -14,13 +15,12 @@ uint32_t Default::getConfiguredOrDefaultMs(uint32_t configuredInterval)
     return default_broadcast_interval_secs * 1000;
 }
 
-uint32_t Default::getConfiguredOrDefaultMs(uint32_t configuredInterval, uint32_t defaultInterval)
+uint32_t Default::getConfiguredOrDefault(uint32_t configured, uint32_t defaultValue)
 {
-    if (configuredInterval > 0)
-        return configuredInterval * 1000;
-    return defaultInterval * 1000;
+    if (configured > 0)
+        return configured;
+    return defaultValue;
 }
-
 /**
  * Calculates the scaled value of the configured or default value in ms based on the number of online nodes.
  *
