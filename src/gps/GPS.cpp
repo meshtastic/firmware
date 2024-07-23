d0509ce69fd58a099aaae2ed8c3e7516c1a77b7b
Tom Fifield <tom@tomfifield.net>
2024-07-23 11:45:52 +0800

    Only run speedSelect skip the first time

--- src/gps/GPS.cpp
+++ src/gps/GPS.cpp
@@ -403,7 +403,7 @@ bool GPS::setup()
         if (tx_gpio && gnssModel == GNSS_MODEL_UNKNOWN) {
 
             // if GPS_BAUDRATE is specified in variant (i.e. not 9600), skip to the specified rate.
-            if (GPS_BAUDRATE > serialSpeeds[speedSelect]) {
+            if (speedSelect == 0 && GPS_BAUDRATE != serialSpeeds[speedSelect]) {
                 speedSelect = std::find(serialSpeeds, std::end(serialSpeeds), GPS_BAUDRATE) - serialSpeeds;
             }
 
