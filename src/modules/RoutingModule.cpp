95aa0054665fdecf9bfc0bb57a85778a40b9fe12
Jonathan Bennett <jbennett@incomsystems.biz>
2024-07-29 14:25:55 -0500

    In EVENT_MODE, don't respond with hop_limit set more then the configured max.

--- src/modules/RoutingModule.cpp
+++ src/modules/RoutingModule.cpp
@@ -51,7 +51,10 @@ uint8_t RoutingModule::getHopLimitForResponse(uint8_t hopStart, uint8_t hopLimit
         // Hops used by the request. If somebody in between running modified firmware modified it, ignore it
         uint8_t hopsUsed = hopStart < hopLimit ? config.lora.hop_limit : hopStart - hopLimit;
         if (hopsUsed > config.lora.hop_limit) {
+// In event mode, we never want to send packets with more than our default 3 hops.
+#if !(EVENTMODE)             // This falls through to the default.
             return hopsUsed; // If the request used more hops than the limit, use the same amount of hops
+#endif
         } else if ((uint8_t)(hopsUsed + 2) < config.lora.hop_limit) {
             return hopsUsed + 2; // Use only the amount of hops needed with some margin as the way back may be different
         }
