d4a9e0de688ed27084a6f7cedb5dca8535787761
mverch67 <manuel.verch@gmx.de>
2024-08-01 21:49:25 +0200

    rotate screen

--- src/graphics/TFTDisplay.cpp
+++ src/graphics/TFTDisplay.cpp
@@ -857,9 +857,9 @@ bool TFTDisplay::connect()
     ft6336u.begin();
     pinMode(SCREEN_TOUCH_INT, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(SCREEN_TOUCH_INT), rak14014_tpIntHandle, FALLING);
-#elif defined(T_DECK) || defined(PICOMPUTER_S3) || defined(SENSECAP_INDICATOR) || defined(CHATTER_2)
+#elif defined(T_DECK) || defined(PICOMPUTER_S3) || defined(CHATTER_2)
     tft->setRotation(1); // T-Deck has the TFT in landscape
-#elif defined(T_WATCH_S3)
+#elif defined(T_WATCH_S3) || defined(SENSECAP_INDICATOR)
     tft->setRotation(2); // T-Watch S3 left-handed orientation
 #else
     tft->setRotation(3); // Orient horizontal and wide underneath the silkscreen name label
