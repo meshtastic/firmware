b4250a900f55dbda23d484a16025610a776120f4
Jonathan Bennett <jbennett@incomsystems.biz>
2024-08-05 12:34:10 -0500

    Turn PKI back off for STM32 :(

--- src/configuration.h
+++ src/configuration.h
@@ -256,7 +256,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MESHTASTIC_EXCLUDE_MQTT 1
 #define MESHTASTIC_EXCLUDE_POWERMON 1
 #define MESHTASTIC_EXCLUDE_I2C 1
-#define MESHTASTIC_EXCLUDE_PKI_KEYGEN 1
+#define MESHTASTIC_EXCLUDE_PKI 1
 #endif
 
 // Turn off all optional modules
