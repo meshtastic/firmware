b726792efd1ea7ba34eeb23af22a48eda1a00ec9
Jonathan Bennett <jbennett@incomsystems.biz>
2024-08-10 13:45:41 -0500

    Re-implement PKI from #1509 (#4379)
    * Re-implement PKI from #1509
    co-authored-by: edinnen <ethanjdinnen@protonmail.com>
    
    * Set the key lengnth to actually make PKI work.
    
    * Remove unused variable and initialize keys to null
    
    * move printBytes() to meshUtils
    
    * Don't reset PKI key son reboot unless needed.
    
    * Remove double encryption for PKI messages
    
    * Cleanup encrypt logic
    
    * Add the MESHTASTIC_EXCLUDE_PKI option, and set it for minimal builds. Required for STM32 targets for now.
    
    * Use SHA-256 for PKI key hashing, and add MESHTASTIC_EXCLUDE_PKI_KEYGEN for STM32
    
    * Fix a crash when node is null
    
    * Don't send PKI encrypted packets while licensed
    
    * use chIndex 8 for PKI
    
    * Don't be so clever, that you corrupt incoming packets
    
    * Pass on channel 8 for now
    
    * Typo
    
    * Lock keys once non-zero
    
    * We in fact need 2 scratch buffers, to store the encrypted bytes, unencrypted bytes, and decoded protobuf.
    
    * Lighter approach to retaining known key
    
    * Attach the public key to PKI decrypted packets in device memory
    
    * Turn PKI back off for STM32 :(
    
    * Don't just memcp over a protobuf
    
    * Don't PKI encrypt nodeinfo packets
    
    * Add a bit more memory logging around nodeDB
    
    * Use the proper macro to refer to NODENUM_BROADCAST
    
    * Typo fix
    
    * Don't PKI encrypt ROUTING (naks and acks)
    
    * Adds SecurityConfig protobuf
    
    * Add admin messages over PKI
    
    * Disable PKI for the WIO-e5
    
    * Add MINIMUM_SAFE_FREE_HEAP macro and set to safe 1.5k
    
    * Add missed "has_security"
    
    * Add the admin_channel_enabled option
    
    * STM32 again
    
    * add missed configuration.h at the top of files
    
    * Add EXCLUDE_TZ and RTC
    
    * Enable PKI build on STM32 once again
    
    * Attempt 1 at moving PKI to aes-ccm
    
    * Fix buffers for encrypt/decrypt
    
    * Eliminate unused aes variable
    
    * Add debugging lines
    
    * Set hash to 0 for PKI
    
    * Fix debug lines so they don't print pointers.
    
    * logic fix and more debug
    
    * Rather important typo
    
    * Check for short packets before attempting decrypt
    
    * Don't forget to give cryptoEngine the keys!
    
    * Use the right scratch buffer
    
    * Cleanup
    
    * moar cleanups
    
    * Minor hardening
    
    * Remove some in-progress stuff
    
    * Turn PKI back off on STM32
    
    * Return false
    
    * 2.5 protos
    
    * Sync up protos
    
    * Add initial cryptography test vector tests
    
    * re-add MINIMUM_SAFE_FREE_HEAP
    
    * Housekeeping and comment fixes
    
    * Add explanatory comment about weak dh25519 keys
    
    ---------
    
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>

--- src/mesh/CryptoEngine.h
+++ src/mesh/CryptoEngine.h
@@ -1,6 +1,8 @@
 #pragma once
-
+#include "AES.h"
 #include "concurrency/LockGuard.h"
+#include "configuration.h"
+#include "mesh-pb-constants.h"
 #include <Arduino.h>
 
 extern concurrency::Lock *cryptLock;
@@ -26,9 +28,34 @@ class CryptoEngine
     uint8_t nonce[16] = {0};
 
     CryptoKey key = {};
+#if !(MESHTASTIC_EXCLUDE_PKI)
+    uint8_t private_key[32] = {0};
+#endif
 
   public:
+#if !(MESHTASTIC_EXCLUDE_PKI)
+    uint8_t public_key[32] = {0};
+#endif
+
     virtual ~CryptoEngine() {}
+#if !(MESHTASTIC_EXCLUDE_PKI)
+#if !(MESHTASTIC_EXCLUDE_PKI_KEYGEN)
+    virtual void generateKeyPair(uint8_t *pubKey, uint8_t *privKey);
+#endif
+    void clearKeys();
+    void setPrivateKey(uint8_t *_private_key);
+    virtual bool encryptCurve25519(uint32_t toNode, uint32_t fromNode, uint64_t packetNum, size_t numBytes, uint8_t *bytes,
+                                   uint8_t *bytesOut);
+    virtual bool decryptCurve25519(uint32_t fromNode, uint64_t packetNum, size_t numBytes, uint8_t *bytes, uint8_t *bytesOut);
+    virtual bool setDHKey(uint32_t nodeNum);
+    virtual void hash(uint8_t *bytes, size_t numBytes);
+
+    virtual void aesSetKey(const uint8_t *key, size_t key_len);
+
+    virtual void aesEncrypt(uint8_t *in, uint8_t *out);
+    AESSmall256 *aes = NULL;
+
+#endif
 
     /**
      * Set the key used for encrypt, decrypt.
