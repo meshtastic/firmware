e6bcf4bbd68cab15f5aec44285679ff22e0e2aa7
Tom Fifield <tom@tomfifield.net>
2024-08-11 20:06:38 +0800

    Address some FIXME comments (#4435)
    * Address some FIXME comments
    
    These comments have since been addressed by more modern code.
    Remove them to reduce the clutter in the codebase.
    
    * Remove 'dumb idea' from SimpleAllocator
    
    4 year old code that was set never to run can probably be safely
    deleted.

--- src/gps/GPS.h
+++ src/gps/GPS.h
@@ -51,7 +51,7 @@ enum GPSPowerState : uint8_t {
 const char *getDOPString(uint32_t dop);
 
 /**
- * A gps class that only reads from the GPS periodically (and FIXME - eventually keeps the gps powered down except when reading)
+ * A gps class that only reads from the GPS periodically and keeps the gps powered down except when reading
  *
  * When new data is available it will notify observers.
  */
