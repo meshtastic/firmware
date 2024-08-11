8f3614d66c2671d5160c3bb95374c0ba712dc2f6
Ben Meadors <benmmeadors@gmail.com>
2024-08-11 17:22:11 -0500

    User to UserLite in NodeDB (#4438)
    * User to UserLite in the nodedb
    
    * Tronkdor the burninator

--- src/mesh/generated/meshtastic/deviceonly.pb.cpp
+++ src/mesh/generated/meshtastic/deviceonly.pb.cpp
@@ -9,6 +9,9 @@
 PB_BIND(meshtastic_PositionLite, meshtastic_PositionLite, AUTO)
 
 
+PB_BIND(meshtastic_UserLite, meshtastic_UserLite, AUTO)
+
+
 PB_BIND(meshtastic_NodeInfoLite, meshtastic_NodeInfoLite, AUTO)
 
 
