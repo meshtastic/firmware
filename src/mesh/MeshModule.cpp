c16f20de21cded1d56ea9992b86ebe9b2ce3c816
Jonathan Bennett <jbennett@incomsystems.biz>
2024-08-12 23:30:34 -0500

    Make "Alloc an error" a LOG_WARN

--- src/mesh/MeshModule.cpp
+++ src/mesh/MeshModule.cpp
@@ -54,8 +54,8 @@ meshtastic_MeshPacket *MeshModule::allocAckNak(meshtastic_Routing_Error err, Nod
     p->to = to;
     p->decoded.request_id = idFrom;
     p->channel = chIndex;
-    if (err != meshtastic_Routing_Error_NONE)
-        LOG_ERROR("Alloc an err=%d,to=0x%x,idFrom=0x%x,id=0x%x\n", err, to, idFrom, p->id);
+    if (err != meshtastic_Routing_Error_NONE && err != meshtastic_Routing_Error_NONE_PKI)
+        LOG_WARN("Alloc an err=%d,to=0x%x,idFrom=0x%x,id=0x%x\n", err, to, idFrom, p->id);
 
     return p;
 }
