d398419aef5303c7121d551589d8dcaa470d2b10
Ben Meadors <benmmeadors@gmail.com>
2024-08-15 08:47:49 -0500

    Router and sensor are impolite (#4468)

--- src/modules/Telemetry/DeviceTelemetry.cpp
+++ src/modules/Telemetry/DeviceTelemetry.cpp
@@ -16,12 +16,14 @@
 int32_t DeviceTelemetryModule::runOnce()
 {
     refreshUptime();
+    bool isImpoliteRole = config.device.role == meshtastic_Config_DeviceConfig_Role_SENSOR ||
+                          config.device.role == meshtastic_Config_DeviceConfig_Role_ROUTER;
     if (((lastSentToMesh == 0) ||
          ((uptimeLastMs - lastSentToMesh) >=
           Default::getConfiguredOrDefaultMsScaled(moduleConfig.telemetry.device_update_interval,
                                                   default_telemetry_broadcast_interval_secs, numOnlineNodes))) &&
-        airTime->isTxAllowedChannelUtil(config.device.role != meshtastic_Config_DeviceConfig_Role_SENSOR) &&
-        airTime->isTxAllowedAirUtil() && config.device.role != meshtastic_Config_DeviceConfig_Role_REPEATER &&
+        airTime->isTxAllowedChannelUtil(!isImpoliteRole) && airTime->isTxAllowedAirUtil() &&
+        config.device.role != meshtastic_Config_DeviceConfig_Role_REPEATER &&
         config.device.role != meshtastic_Config_DeviceConfig_Role_CLIENT_HIDDEN) {
         sendTelemetry();
         lastSentToMesh = uptimeLastMs;
