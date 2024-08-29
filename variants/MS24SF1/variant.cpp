b8cfc7257947dcf2bc1f0fe11653fc3fa36c988c
Mark Trevor Birss <markbirss@gmail.com>
2024-08-29 16:13:17 +0200

    Update variant.cpp

--- variants/MS24SF1/variant.cpp
+++ variants/MS24SF1/variant.cpp
@@ -29,10 +29,4 @@ const uint32_t g_ADigitalPinMap[] = {
 
 void initVariant()
 {
-    // LED1 & LED2
-    //pinMode(PIN_LED1, OUTPUT);
-    //ledOff(PIN_LED1);
-
-    //pinMode(PIN_LED2, OUTPUT);
-    //ledOff(PIN_LED2);
 }
