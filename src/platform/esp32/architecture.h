6a24566efb0789789ea46159ed6825957abfc7a7
Manuel <71137295+mverch67@users.noreply.github.com>
2024-08-30 12:53:06 +0200

    Lilygo T3S3 E-Paper support (#4569)
    * t3s3 e-paper support
    
    * remove GPS autodetect (which leads to crashes during startup when no GPS present)
    
    * update EINK defines
    
    * keep definitions for external GPS connector but disable GPS auto scan by default

--- src/platform/esp32/architecture.h
+++ src/platform/esp32/architecture.h
@@ -122,6 +122,8 @@
 #define HW_VENDOR meshtastic_HardwareModel_HELTEC_WIRELESS_PAPER
 #elif defined(TLORA_T3S3_V1)
 #define HW_VENDOR meshtastic_HardwareModel_TLORA_T3_S3
+#elif defined(TLORA_T3S3_EPAPER)
+#define HW_VENDOR meshtastic_HardwareModel_TLORA_T3_S3
 #elif defined(CDEBYTE_EORA_S3)
 #define HW_VENDOR meshtastic_HardwareModel_CDEBYTE_EORA_S3
 #elif defined(BETAFPV_2400_TX)
