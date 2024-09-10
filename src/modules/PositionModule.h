f74fdd6af483448623631c2fc75e5b4255b5e69f
Ben Meadors <benmmeadors@gmail.com>
2024-09-10 14:31:47 -0500

    Trunk

--- src/modules/PositionModule.h
+++ src/modules/PositionModule.h
@@ -62,7 +62,8 @@ class PositionModule : public ProtobufModule<meshtastic_Position>, private concu
     void sendLostAndFoundText();
     bool hasQualityTimesource();
 
-    const uint32_t minimumTimeThreshold = Default::getConfiguredOrDefaultMs(config.position.broadcast_smart_minimum_interval_secs, 30);
+    const uint32_t minimumTimeThreshold =
+        Default::getConfiguredOrDefaultMs(config.position.broadcast_smart_minimum_interval_secs, 30);
 };
 
 struct SmartPosition {
