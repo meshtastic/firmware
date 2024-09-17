7715ce52d99335f0f1bd3b9ae24a8b8a7a765334
Thomas Göttgens <tgoettgens@gmail.com>
2024-09-17 15:57:01 +0200

    works without but this triggers my OCD

--- src/modules/Telemetry/AirQualityTelemetry.h
+++ src/modules/Telemetry/AirQualityTelemetry.h
@@ -44,7 +44,7 @@ class AirQualityTelemetryModule : private concurrency::OSThread, public Protobuf
   private:
     Adafruit_PM25AQI aqi;
     PM25_AQI_Data data = {0};
-    bool firstTime = 1;
+    bool firstTime = true;
     meshtastic_MeshPacket *lastMeasurementPacket;
     uint32_t sendToPhoneIntervalMs = SECONDS_IN_MINUTE * 1000; // Send to phone every minute
     uint32_t lastSentToMesh = 0;
