179f4cc9125c536d66501c3f0ff45fb714cb6fe7
Tom Fifield <tom@tomfifield.net>
2024-09-22 13:37:55 +0800

    Update variant.h

--- variants/diy/t-energy-s3_e22/variant.h
+++ variants/diy/t-energy-s3_e22/variant.h
@@ -22,25 +22,25 @@
 #define GPS_RX_PIN 44
 
 // LoRa
-#define USE_SX1262  // E22-900M30S, E22-900M22S, and E22-900MM22S (not E220!) use SX1262
-#define USE_SX1268  // E22-400M30S, E22-400M33S, E22-400M22S, and E22-400MM22S use SX1268
+#define USE_SX1262 // E22-900M30S, E22-900M22S, and E22-900MM22S (not E220!) use SX1262
+#define USE_SX1268 // E22-400M30S, E22-400M33S, E22-400M22S, and E22-400MM22S use SX1268
 
-#define SX126X_MAX_POWER 22  // SX126xInterface.cpp defaults to 22 if not defined, but here we define it for good practice
-#define SX126X_DIO3_TCXO_VOLTAGE 1.8  // E22 series TCXO reference voltage is 1.8V
+#define SX126X_MAX_POWER 22          // SX126xInterface.cpp defaults to 22 if not defined, but here we define it for good practice
+#define SX126X_DIO3_TCXO_VOLTAGE 1.8 // E22 series TCXO reference voltage is 1.8V
 
-#define SX126X_CS 5     // EBYTE module's NSS pin // FIXME: rename to SX126X_SS
-#define SX126X_SCK 6    // EBYTE module's SCK pin
-#define SX126X_MOSI 13  // EBYTE module's MOSI pin
-#define SX126X_MISO 4   // EBYTE module's MISO pin
-#define SX126X_RESET 1  // EBYTE module's NRST pin
-#define SX126X_BUSY 48  // EBYTE module's BUSY pin
-#define SX126X_DIO1 47  // EBYTE module's DIO1 pin
+#define SX126X_CS 5    // EBYTE module's NSS pin // FIXME: rename to SX126X_SS
+#define SX126X_SCK 6   // EBYTE module's SCK pin
+#define SX126X_MOSI 13 // EBYTE module's MOSI pin
+#define SX126X_MISO 4  // EBYTE module's MISO pin
+#define SX126X_RESET 1 // EBYTE module's NRST pin
+#define SX126X_BUSY 48 // EBYTE module's BUSY pin
+#define SX126X_DIO1 47 // EBYTE module's DIO1 pin
 
-#define SX126X_TXEN 10  // Schematic connects EBYTE module's TXEN pin to MCU
-#define SX126X_RXEN 12  // Schematic connects EBYTE module's RXEN pin to MCU
+#define SX126X_TXEN 10 // Schematic connects EBYTE module's TXEN pin to MCU
+#define SX126X_RXEN 12 // Schematic connects EBYTE module's RXEN pin to MCU
 
-#define LORA_CS SX126X_CS      // Compatibility with variant file configuration structure
-#define LORA_SCK SX126X_SCK    // Compatibility with variant file configuration structure
-#define LORA_MOSI SX126X_MOSI  // Compatibility with variant file configuration structure
-#define LORA_MISO SX126X_MISO  // Compatibility with variant file configuration structure
-#define LORA_DIO1 SX126X_DIO1  // Compatibility with variant file configuration structure
+#define LORA_CS SX126X_CS     // Compatibility with variant file configuration structure
+#define LORA_SCK SX126X_SCK   // Compatibility with variant file configuration structure
+#define LORA_MOSI SX126X_MOSI // Compatibility with variant file configuration structure
+#define LORA_MISO SX126X_MISO // Compatibility with variant file configuration structure
+#define LORA_DIO1 SX126X_DIO1 // Compatibility with variant file configuration structure
