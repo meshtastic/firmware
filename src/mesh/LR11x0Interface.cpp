26112ba0017606704f06127717810332ba3ee6dd
Thomas Göttgens <tgoettgens@gmail.com>
2024-09-25 18:56:17 +0200

    Support T3S3 LR1121 variant

--- src/mesh/LR11x0Interface.cpp
+++ src/mesh/LR11x0Interface.cpp
@@ -10,8 +10,14 @@
 
 // Particular boards might define a different max power based on what their hardware can do, default to max power output if not
 // specified (may be dangerous if using external PA and LR11x0 power config forgotten)
-#ifndef LR11X0_MAX_POWER
-#define LR11X0_MAX_POWER 22
+#ifndef LR1110_MAX_POWER
+#define LR1110_MAX_POWER 22
+#endif
+
+// the 2.4G part maxes at 13dBm
+
+#ifndef LR1120_MAX_POWER
+#define LR1120_MAX_POWER 13
 #endif
 
 template <typename T>
@@ -47,8 +53,12 @@ template <typename T> bool LR11x0Interface<T>::init()
 
     RadioLibInterface::init();
 
-    if (power > LR11X0_MAX_POWER) // Clamp power to maximum defined level
-        power = LR11X0_MAX_POWER;
+    if (power > LR1110_MAX_POWER) // Clamp power to maximum defined level
+        power = LR1110_MAX_POWER;
+
+    if ((power > LR1120_MAX_POWER) &&
+        (config.lora.region == meshtastic_Config_LoRaConfig_RegionCode_LORA_24)) // clamp again if wide freq range
+        power = LR1120_MAX_POWER;
 
     limitPower();
 
@@ -63,6 +73,18 @@ template <typename T> bool LR11x0Interface<T>::init()
         {LR11x0::MODE_TX, {HIGH, HIGH, LOW, HIGH}}, {LR11x0::MODE_TX_HP, {LOW, HIGH, LOW, HIGH}},
         {LR11x0::MODE_TX_HF, {LOW, LOW, LOW, LOW}}, {LR11x0::MODE_GNSS, {LOW, LOW, HIGH, LOW}},
         {LR11x0::MODE_WIFI, {LOW, LOW, LOW, LOW}},  END_OF_MODE_TABLE,
+    };
+
+#elif defined(TLORA_T3S3_V1)
+    static const uint32_t rfswitch_dio_pins[] = {RADIOLIB_LR11X0_DIO5, RADIOLIB_LR11X0_DIO6, RADIOLIB_NC, RADIOLIB_NC,
+                                                 RADIOLIB_NC};
+
+    static const Module::RfSwitchMode_t rfswitch_table[] = {
+        // mode                  DIO5  DIO6
+        {LR11x0::MODE_STBY, {LOW, LOW}},  {LR11x0::MODE_RX, {HIGH, LOW}},
+        {LR11x0::MODE_TX, {LOW, HIGH}},   {LR11x0::MODE_TX_HP, {LOW, HIGH}},
+        {LR11x0::MODE_TX_HF, {LOW, LOW}}, {LR11x0::MODE_GNSS, {LOW, LOW}},
+        {LR11x0::MODE_WIFI, {LOW, LOW}},  END_OF_MODE_TABLE,
     };
 
 #else
@@ -174,8 +196,10 @@ template <typename T> bool LR11x0Interface<T>::reconfigure()
     if (err != RADIOLIB_ERR_NONE)
         RECORD_CRITICALERROR(meshtastic_CriticalErrorCode_INVALID_RADIO_SETTING);
 
-    if (power > LR11X0_MAX_POWER) // This chip has lower power limits than some
-        power = LR11X0_MAX_POWER;
+    if (power > LR1110_MAX_POWER) // This chip has lower power limits than some
+        power = LR1110_MAX_POWER;
+    if ((power > LR1120_MAX_POWER) && (config.lora.region == meshtastic_Config_LoRaConfig_RegionCode_LORA_24)) // 2.4G power limit
+        power = LR1120_MAX_POWER;
 
     err = lora.setOutputPower(power);
     assert(err == RADIOLIB_ERR_NONE);
