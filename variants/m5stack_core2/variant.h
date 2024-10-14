cfb3bedd14115e0c2d46e13ca420637a71ab7311
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-10-14 07:26:16 +0200

    Update variant.h

--- variants/m5stack_core2/variant.h
+++ variants/m5stack_core2/variant.h
@@ -28,8 +28,10 @@
 #define TFT_OFFSET_Y 0
 #define TFT_BUSY -1
 #define TFT_OFFSET_ROTATION 0
+
 // LCD screens are slow, so slowdown the wipe so it looks better
 #define SCREEN_TRANSITION_FRAMERATE 30 // fps
+
 // Picomputer gets a white on black display
 #define TFT_MESH COLOR565(0xA0, 0xFF, 0x00) //(0x94, 0xEA, 0x67)
 #define ILI9341_SPI_HOST VSPI_HOST          // VSPI_HOST or HSPI_HOST
