ad214ea42a8c484f1ff90cd76dd58c89408e33fc
Johnathon Mohr <JohnathonMohr@users.noreply.github.com>
2024-10-15 17:08:49 -0700

    Add MQTT exception for private IP address server (#5072)
    Determines if the given IP address is a private address, i.e. not routable on the public internet.
    These are the ranges: 127.0.0.1, 10.0.0.0-10.255.255.255, 172.16.0.0-172.31.255.255, 192.168.0.0-192.168.255.255.
    
    If so, allow MQTT publication the same as existing localhost support.

--- src/mqtt/MQTT.h
+++ src/mqtt/MQTT.h
@@ -120,6 +120,10 @@ class MQTT : private concurrency::OSThread
     // returns true if this is a valid JSON envelope which we accept on downlink
     bool isValidJsonEnvelope(JSONObject &json);
 
+    /// Determines if the given IP address is a private address, i.e. not routable on the public internet.
+    /// These are the ranges: 127.0.0.1, 10.0.0.0-10.255.255.255, 172.16.0.0-172.31.255.255, 192.168.0.0-192.168.255.255.
+    bool isPrivateIpAddress(const char ip[]);
+
     /// Return 0 if sleep is okay, veto sleep if we are connected to pubsub server
     // int preflightSleepCb(void *unused = NULL) { return pubSub.connected() ? 1 : 0; }
 };
