6569437ae268978684c78192020e14fc3f7292d6
Ben Meadors <benmmeadors@gmail.com>
2024-11-05 19:32:50 -0600

    Add exception for RTC to not strip time from position

--- src/modules/PositionModule.cpp
+++ src/modules/PositionModule.cpp
@@ -251,7 +251,11 @@ meshtastic_MeshPacket *PositionModule::allocReply()
     if (getRTCQuality() < RTCQualityNTP) {
         LOG_INFO("Strip time %u from position send", p.time);
         p.time = 0;
-    } else {
+    } else if (rtc_found.address != ScanI2C::ADDRESS_NONE.address) {
+        LOG_INFO("Using RTC time %u for position send", p.time);
+        p.time = getValidTime(RTCQualityDevice);
+    }
+    else {
         p.time = getValidTime(RTCQualityNTP);
         LOG_INFO("Provide time to mesh %u", p.time);
     }
