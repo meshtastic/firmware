84a30c13bc46b32246b110e4e701ae83982ada55
mverch67 <manuel.verch@gmx.de>
2024-11-17 01:35:16 +0100

    fix DIO1 wakeup

--- variants/mesh-tab/variant.h
+++ variants/mesh-tab/variant.h
@@ -17,6 +17,9 @@
 // Button
 #define BUTTON_PIN 0
 
+// Button
+#define PIN_BUZZER 5
+
 // GPS
 #define GPS_RX_PIN 18
 #define GPS_TX_PIN 17
@@ -28,20 +31,26 @@
 #define SPI_CS 10
 #define SDCARD_CS 6
 
+// LORA MODULES
+#define USE_SX1262
+
 // LORA SPI
 #define LORA_SCK 36
 #define LORA_MISO 37
 #define LORA_MOSI 35
 #define LORA_CS 39
 
-// LORA MODULES
-#define USE_SX1262
-
 // LORA CONFIG
+#define LORA_DIO0 -1 // a No connect on the SX1262 module
+#define LORA_RESET 14
+#define LORA_DIO1 15 // SX1262 IRQ
+#define LORA_DIO2 40 // SX1262 BUSY
+#define LORA_DIO3    // Not connected on PCB, but internally on the TTGO SX1262
+
 #define SX126X_CS LORA_CS
-#define SX126X_DIO1 15
+#define SX126X_DIO1 LORA_DIO1
 #define SX126X_DIO2_AS_RF_SWITCH
-#define SX126X_BUSY 40
+#define SX126X_BUSY LORA_DIO2
 #define SX126X_RESET 14
 #define SX126X_RXEN 47
 #define SX126X_TXEN RADIOLIB_NC // Assuming that DIO2 is connected to TXEN pin
