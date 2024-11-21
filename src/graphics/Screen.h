9112fa481bf68ae89e8703efa9fc131fdd8efd32
Dylanliacc <dylan3000dylan@gmail.com>
2024-11-21 14:35:50 +0800

    add canned message and keyboard in indicator board

--- src/graphics/Screen.h
+++ src/graphics/Screen.h
@@ -554,7 +554,7 @@ class Screen : public concurrency::OSThread
 
     static void drawDebugInfoWiFiTrampoline(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
 
-#ifdef T_WATCH_S3
+#ifdef T_WATCH_S3 || defined(SENSECAP_INDICATOR)
     static void drawAnalogClockFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
 
     static void drawDigitalClockFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
