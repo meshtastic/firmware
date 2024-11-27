b339466e657fa263273a3c81e32efa5af4e9eab6
ThePanaceya <panaceya@users.noreply.github.com>
2024-11-27 20:48:27 +0200

    UPDATE: new 16 and 24 UA Fonts and fixes

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -19,7 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-#include "../userPrefs.h"
 #include "Screen.h"
 #include "PowerMon.h"
 #include "Throttle.h"
