2dc00eaa76ee378a8e90229cf972a724ee8c3017
medentem <info@medentem.com>
2024-12-15 15:26:38 -0600

    adding userpref option for decoupling the auto discovery channel from auto position updates

--- src/modules/NodeInfoModule.cpp
+++ src/modules/NodeInfoModule.cpp
@@ -53,6 +53,12 @@ void NodeInfoModule::sendOurNodeInfo(NodeNum dest, bool wantReplies, uint8_t cha
             LOG_DEBUG("Send ourNodeInfo to channel %d", channel);
             p->channel = channel;
         }
+#ifdef USERPREFS_CONFIG_DISCOVERY_CHANNEL
+        // If this is a broadcast over the default channel, we can safely change this to the discovery channel if defined
+        if (dest === NODENUM_BROADCAST && channel === 0) {
+            p->channel = USERPREFS_CONFIG_DISCOVERY_CHANNEL;
+        }
+#endif
 
         prevPacketId = p->id;
 
