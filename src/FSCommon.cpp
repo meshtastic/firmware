f324f3924394975bab8509d049e427a55eb437f0
Tavis <github@tavis.la>
2024-12-26 13:56:04 -0800

    fix for nrf52 lfs assert boot loop

--- src/FSCommon.cpp
+++ src/FSCommon.cpp
@@ -55,6 +55,11 @@ extern "C" void lfs_assert(const char *reason)
 {
     LOG_ERROR("LFS assert: %s", reason);
     lfs_assert_failed = true;
+
+    // CORRUPTED FILESYSTEM. This causes bootloop so
+    // might as well try formatting now.
+    LOG_ERROR("Trying FSCom.format()");
+    FSCom.format();
 }
 
 /**
