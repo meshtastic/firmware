ae84b6d8690c575778ab70ced934be0dc5a34493
Eric Severance <esev@esev.com>
2024-12-25 16:40:55 -0800

    Provide an initial time value

--- test/TestUtil.cpp
+++ test/TestUtil.cpp
@@ -1,10 +1,18 @@
-#include "TestUtil.h"
 #include "SerialConsole.h"
 #include "concurrency/OSThread.h"
+#include "gps/RTC.h"
+
+#include "TestUtil.h"
 
 void initializeTestEnvironment()
 {
     concurrency::hasBeenSetup = true;
-    concurrency::OSThread::setup();
     consoleInit();
+#if ARCH_PORTDUINO
+    struct timeval tv;
+    tv.tv_sec = time(NULL);
+    tv.tv_usec = 0;
+    perhapsSetRTC(RTCQualityNTP, &tv);
+#endif
+    concurrency::OSThread::setup();
 }
