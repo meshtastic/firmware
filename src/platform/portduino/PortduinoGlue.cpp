58ebd5bcdb02a1935238f6005d3c65a32209549d
Jonathan Bennett <jbennett@incomsystems.biz>
2024-12-31 00:46:03 -0600

    Actually use the MAC address from a ch341 (#5704)
    Co-authored-by: Tom Fifield <tom@tomfifield.net>

--- src/platform/portduino/PortduinoGlue.cpp
+++ src/platform/portduino/PortduinoGlue.cpp
@@ -18,6 +18,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -231,6 +232,9 @@ void portduinoSetup()
             dmac[3] = hash[3];
             dmac[4] = hash[4];
             dmac[5] = hash[5];
+            std::stringstream mactmp;
+            mactmp << std::hex << +dmac[0] << +dmac[1] << +dmac[2] << +dmac[3] << +dmac[4] << +dmac[5];
+            settingsStrings[mac_address] = mactmp.str();
         }
     }
 
