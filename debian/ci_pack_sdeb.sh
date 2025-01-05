d21d6c1301d2a3fd06faf17f0ca411e8d6d83d0f
Austin <vidplace7@gmail.com>
2025-01-05 14:24:05 -0500

    meshtasticd-debian: Build multiple series (#5756)

--- debian/ci_pack_sdeb.sh
+++ debian/ci_pack_sdeb.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/bash
+export DEBEMAIL="github-actions[bot]@users.noreply.github.com"
 export PLATFORMIO_LIBDEPS_DIR=pio/libdeps
 export PLATFORMIO_PACKAGES_DIR=pio/packages
 
 # Download libraries to `libdeps`
 platformio pkg install -e native
+
+package=$(dpkg-parsechangelog --show-field Source)
+pkg_version=$(dpkg-parsechangelog --show-field Version | cut -d- -f1)
+
+dch --create --distribution $SERIES --package $package --newversion $pkg_version-ppa${REVISION::7}~$SERIES \
+	"GitHub Actions Automatic packaging for $SERIES"
 
 # Build the source deb
 debuild -S -k$GPG_KEY_ID
