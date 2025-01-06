ca328898939edb22d50d0b3d62f475e352d29069
Jonathan Bennett <jbennett@incomsystems.biz>
2025-01-06 12:23:08 -0600

    Specify scons version

--- debian/ci_pack_sdeb.sh
+++ debian/ci_pack_sdeb.sh
@@ -6,7 +6,7 @@ export PLATFORMIO_CORE_DIR=pio/core
 
 # Download libraries to `libdeps`
 platformio pkg install -e native
-platformio pkg install -t tool-scons -e native
+platformio pkg install -t platformio/tool-scons@4.40801.0
 tar -cf pio.tar pio/
 rm -rf pio
 
