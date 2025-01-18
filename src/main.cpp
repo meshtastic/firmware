b353bcc04acf9eeab7d2f700e7ee977a6a0d7582
Thomas Göttgens <tgoettgens@gmail.com>
2025-01-18 14:10:13 +0100

    fix detection of lark weather station and add rain sensor (#5874)
    * fix detection of lark weather station
    * fix unit tests and add support for Dfrobot rain gauge
    * fix name display on bootup
    * fix gauge init logic
    * trunk fmt

--- src/main.cpp
+++ src/main.cpp
@@ -610,6 +610,7 @@ void setup()
     scannerToSensorsMap(i2cScanner, ScanI2C::DeviceType::ICM20948, meshtastic_TelemetrySensorType_ICM20948);
     scannerToSensorsMap(i2cScanner, ScanI2C::DeviceType::MAX30102, meshtastic_TelemetrySensorType_MAX30102);
     scannerToSensorsMap(i2cScanner, ScanI2C::DeviceType::CGRADSENS, meshtastic_TelemetrySensorType_RADSENS);
+    scannerToSensorsMap(i2cScanner, ScanI2C::DeviceType::DFROBOT_RAIN, meshtastic_TelemetrySensorType_DFROBOT_RAIN);
 
     i2cScanner.reset();
 #endif
