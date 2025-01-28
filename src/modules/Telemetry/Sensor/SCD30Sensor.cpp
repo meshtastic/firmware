f48efc59f6adc0cac515a08c8b01ffd162f6ac34
chris lu <chlu@pdx.edu>
2025-01-28 12:24:33 -0800

    cleaned up, added delays

--- src/modules/Telemetry/Sensor/SCD30Sensor.cpp
+++ src/modules/Telemetry/Sensor/SCD30Sensor.cpp
@@ -8,12 +8,8 @@
 #include <Adafruit_SCD30.h>
 #include <typeinfo>
 
-//TODO: fill out these functions, add I2C scanning for the specific port, (scani2ctwowire.cpp, scani2c.h), add to main.cpp so it autodetects
-
-//are we instatntaiting the class correctly?
 SCD30Sensor::SCD30Sensor() : TelemetrySensor(meshtastic_TelemetrySensorType_SCD30, "SCD30") {}
 
-// do the functions in here need to be run more than once or are we good
 int32_t SCD30Sensor::runOnce()
 {
   if(!scd30.begin())
@@ -22,19 +18,9 @@ int32_t SCD30Sensor::runOnce()
   }
   scd30.setMeasurementInterval(10);
   scd30.startContinuousMeasurement();
+  delay(1000); // Wait 2 seconds for initialization and calibration
   LOG_INFO("SCD30 setup:");
   LOG_INFO("%f, seconds",scd30.getMeasurementInterval());
-
-  // LOG_INFO("Init sensor: %s", sensorName);
-  // if (!hasSensor()) {
-  //   return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
-  // }
-  // status = scd30.begin(nodeTelemetrySensorsMap[sensorType].first, nodeTelemetrySensorsMap[sensorType].second);
-  //begin kicks off startcontinuos measurement through _init
-  // status &= scd30.selfCalibrationEnabled(true);
-  // status &= scd30.startContinuousMeasurement();
-  // status &= scd30.read();
-  // return initI2CSensor();
 }
 
 void SCD30Sensor::setup() {
@@ -45,15 +31,15 @@ bool SCD30Sensor::getMetrics(meshtastic_Telemetry *measurement)
   measurement->variant.environment_metrics.has_temperature = true;
   measurement->variant.environment_metrics.has_relative_humidity = true;
   measurement->variant.air_quality_metrics.has_co2 = true;
-
-  //is set_measurement_interval being set right at the right time
   if (!scd30.dataReady())
   {
-      LOG_DEBUG("scd30 data not ready yet, delaying");
-      delay(500);
+    LOG_DEBUG("scd30 data not ready yet, delaying");
+    delay(2000); 
+    return false;
   }
   if (!scd30.read()) {
       LOG_DEBUG("SCD30 read failed!");
+      delay(500);
       return false;
   }
   measurement->variant.environment_metrics.temperature = scd30.temperature;
