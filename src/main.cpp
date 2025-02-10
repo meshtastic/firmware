48b00188fb304f0885283759a70f92ae73514057
Manuel <71137295+mverch67@users.noreply.github.com>
2025-02-10 14:41:37 +0100

    Fix T-Deck SD card detection (#6023)
    * increase T-Deck PSRAM to avoid too early out-of-memory when messages fill up the storage
    
    * fix SDCard for T-Deck; allow SPI frequency config

--- src/main.cpp
+++ src/main.cpp
@@ -270,6 +270,15 @@ void setup()
     // TF Card , Display backlight(AW9364DNR) , AN48841B(Trackball) , ES7210(Decoder)
     pinMode(KB_POWERON, OUTPUT);
     digitalWrite(KB_POWERON, HIGH);
+    // T-Deck has all three SPI peripherals (TFT, SD, LoRa) attached to the same SPI bus
+    // We need to initialize all CS pins in advance otherwise there will be SPI communication issues
+    // e.g. when detecting the SD card
+    pinMode(LORA_CS, OUTPUT);
+    digitalWrite(LORA_CS, HIGH);
+    pinMode(SDCARD_CS, OUTPUT);
+    digitalWrite(SDCARD_CS, HIGH);
+    pinMode(TFT_CS, OUTPUT);
+    digitalWrite(TFT_CS, HIGH);
     delay(100);
 #endif
 
