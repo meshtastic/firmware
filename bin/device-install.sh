5d7bea163a996e33fe7448ceaf95562fb37f5bb1
gjelsoe <36234524+gjelsoe@users.noreply.github.com>
2025-03-03 16:11:03 +0100

    Fixes to device-install.sh
    Replace /bin/sh with /bin/bash for better string handling.
    Removed a SET that doesn't belong in the .sh file.
    Better checking for TFT and non TFT build, based on filename.
    Corrected a mix of TAB & SPACE indent.

--- bin/device-install.sh
+++ bin/device-install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 PYTHON=${PYTHON:-$(which python3 python | head -n 1)}
 WEB_APP=false
@@ -22,7 +22,7 @@ set -e
 
 # Usage info
 show_help() {
-	cat <<EOF
+    cat <<EOF
 Usage: $(basename $0) [-h] [-p ESPTOOL_PORT] [-P PYTHON] [-f FILENAME] [--web] [--tft] [--tft-16mb]
 Flash image file to device, but first erasing and writing system information"
 
@@ -37,7 +37,7 @@ Flash image file to device, but first erasing and writing system information"
 EOF
 }
 # Parse arguments using a single while loop
-while [[ $# -gt 0 ]]; do
+while [ $# -gt 0 ]; do
     case "$1" in
         -h|--help)
             show_help
@@ -77,13 +77,13 @@ while [[ $# -gt 0 ]]; do
 done
 
 [ -z "$FILENAME" -a -n "$1" ] && {
-	FILENAME=$1
-	shift
+    FILENAME=$1
+    shift
 }
 
 # Check if FILENAME contains "-tft-" and either TFT8 or TFT16 is 1 (--tft, -tft-16mb)
 if [[ "${FILENAME//-tft-/}" != "$FILENAME" ]]; then
-    SET TFT_BUILD=true
+    TFT_BUILD=true
     if [[ "$TFT8" != true && "$TFT16" != true ]]; then
         echo "Error: Either --tft or --tft-16mb must be set to use a TFT build."
         exit 1
@@ -101,61 +101,65 @@ if [ -f "${FILENAME}" ] && [ -n "${FILENAME##*"update"*}" ]; then
     # Default littlefs* offset (--web).
     OFFSET=0x300000
 
-	# Default OTA Offset
-	OTA_OFFSET=0x260000
+    # Default OTA Offset
+    OTA_OFFSET=0x260000
 
     # littlefs* offset for MUI 8mb (--tft) and OTA OFFSET.
-    if [ "$TFT8" = true ] && [ "$TFT_BUILD" = true ]; then
-        OFFSET=0x670000
-		OTA_OFFSET=0x340000
-    else
-        echo "Ignoring --tft, not a TFT Build."
+    if [ "$TFT8" = true ]; then
+        if [ "$TFT_BUILD" = true ]; then
+            OFFSET=0x670000
+            OTA_OFFSET=0x340000
+        else
+            echo "Ignoring --tft, not a TFT Build."
+        fi
     fi
 
     # littlefs* offset for MUI 16mb (--tft-16mb) and OTA OFFSET.
-    if [ "$TFT16" = true ] && [ "$TFT_BUILD" = true ]; then
-        OFFSET=0xc90000
-		OTA_OFFSET=0x650000
-    else
-        echo "Ignoring --tft-16mb, not a TFT Build."
+    if [ "$TFT16" = true ]; then
+        if [ "$TFT_BUILD" = true ]; then
+            OFFSET=0xc90000
+            OTA_OFFSET=0x650000
+        else
+            echo "Ignoring --tft-16mb, not a TFT Build."
+        fi
     fi
 
-	echo "Trying to flash ${FILENAME}, but first erasing and writing system information"
-	$ESPTOOL_CMD erase_flash
-	$ESPTOOL_CMD write_flash 0x00 "${FILENAME}"
-	# Account for S3 board's different OTA partition
-	if [ -n "${FILENAME##*"s3"*}" ] && [ -n "${FILENAME##*"-v3"*}" ] && [ -n "${FILENAME##*"t-deck"*}" ] && [ -n "${FILENAME##*"wireless-paper"*}" ] && [ -n "${FILENAME##*"wireless-tracker"*}" ] && [ -n "${FILENAME##*"station-g2"*}" ] && [ -n "${FILENAME##*"unphone"*}" ]; then
-		if [ -n "${FILENAME##*"esp32c3"*}" ]; then
-			$ESPTOOL_CMD write_flash $OTA_OFFSET bleota.bin
-		else
-			$ESPTOOL_CMD write_flash $OTA_OFFSET bleota-c3.bin
-		fi
-	else
-		$ESPTOOL_CMD write_flash $OTA_OFFSET bleota-s3.bin
-	fi
+    echo "Trying to flash ${FILENAME}, but first erasing and writing system information"
+    $ESPTOOL_CMD erase_flash
+    $ESPTOOL_CMD write_flash 0x00 "${FILENAME}"
+    # Account for S3 board's different OTA partition
+    if [ -n "${FILENAME##*"s3"*}" ] && [ -n "${FILENAME##*"-v3"*}" ] && [ -n "${FILENAME##*"t-deck"*}" ] && [ -n "${FILENAME##*"wireless-paper"*}" ] && [ -n "${FILENAME##*"wireless-tracker"*}" ] && [ -n "${FILENAME##*"station-g2"*}" ] && [ -n "${FILENAME##*"unphone"*}" ]; then
+        if [ -n "${FILENAME##*"esp32c3"*}" ]; then
+            $ESPTOOL_CMD write_flash $OTA_OFFSET bleota.bin
+        else
+            $ESPTOOL_CMD write_flash $OTA_OFFSET bleota-c3.bin
+        fi
+    else
+        $ESPTOOL_CMD write_flash $OTA_OFFSET bleota-s3.bin
+    fi
 
-	# Check if WEB_APP (--web) is enabled and add "littlefswebui-" to BASENAME else "littlefs-".
-	if [ "$WEB_APP" = true ]; then
-		# Check it the file exist before trying to write it.
+    # Check if WEB_APP (--web) is enabled and add "littlefswebui-" to BASENAME else "littlefs-".
+    if [ "$WEB_APP" = true ]; then
+        # Check it the file exist before trying to write it.
         if [ -f "littlefswebui-${BASENAME}" ]; then
             $ESPTOOL_CMD write_flash $OFFSET "littlefswebui-${BASENAME}"
         else
             echo "Error: file "littlefswebui-${BASENAME}" wasn't found, littlefs not written."
             exit 1
         fi
-	else
-		# Check it the file exist before trying to write it.
+    else
+        # Check it the file exist before trying to write it.
         if [ -f "littlefs-${BASENAME}" ]; then
             $ESPTOOL_CMD write_flash $OFFSET "littlefs-${BASENAME}"
         else
             echo "Error: file "littlefs-${BASENAME}" wasn't found, littlefs not written."
             exit 1
         fi
-	fi
+    fi
 
 else
-	show_help
-	echo "Invalid file: ${FILENAME}"
+    show_help
+    echo "Invalid file: ${FILENAME}"
 fi
 
 exit 0
