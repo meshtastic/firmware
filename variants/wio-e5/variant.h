70cb2d030d622b6dd9d1368279c2ac340cfd965a
vidplace7 <vidplace7@gmail.com>
2025-03-13 12:56:38 -0400

    Simplify MAX_NUM_NODES, with FLASH_MB macro

--- variants/wio-e5/variant.h
+++ variants/wio-e5/variant.h
@@ -13,7 +13,6 @@ Do not expect a working Meshtastic device with this target.
 #define _VARIANT_WIOE5_
 
 #define USE_STM32WLx
-#define MAX_NUM_NODES 10
 
 #define LED_PIN PB5
 #define LED_STATE_ON 1
