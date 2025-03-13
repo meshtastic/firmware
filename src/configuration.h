0b39199dab38654a018abae2cb7ed727739af4f9
Nasimovy <smeetswout@hotmail.com>
2025-03-13 12:27:06 +0100

    add TCA8418 to configuration.h
    added the TCA8418

--- src/configuration.h
+++ src/configuration.h
@@ -110,11 +110,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Define if screen should be mirrored left to right
 // #define SCREEN_MIRROR
 
-// I2C Keyboards (M5Stack, RAK14004, T-Deck)
+// I2C Keyboards (M5Stack, RAK14004, T-Deck, TCA8418)
 #define CARDKB_ADDR 0x5F
 #define TDECK_KB_ADDR 0x55
 #define BBQ10_KB_ADDR 0x1F
 #define MPR121_KB_ADDR 0x5A
+#define TCA8418_KB_ADDR 0x34
 
 // -----------------------------------------------------------------------------
 // SENSOR
