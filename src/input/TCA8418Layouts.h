1aefc1e252ee9772247f93fecf8609ff61c71ced
Nasimovy <smeetswout@hotmail.com>
2025-03-15 20:22:46 +0100

    Update TCA8418Layouts.h

--- src/input/TCA8418Layouts.h
+++ src/input/TCA8418Layouts.h
@@ -1,9 +1,6 @@
-#ifndef TCA8418LAYOUTS_H
-#define TCA8418LAYOUTS_H
+
 
 // Declare the arrays as externs to be defined in the corresponding .cpp file
-extern uint8_t TCA8418TapMod[];
+extern int TCA8418TapMod[];
 extern unsigned char TCA8418TapMap[][];
 extern unsigned char TCA8418LongPressMap[];
-
-#endif
