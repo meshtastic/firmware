cf5ccd5b6f149d15493df9c69acd7dfb1cc21ba3
Mark Trevor Birss <markbirss@gmail.com>
2025-03-21 15:49:49 +0200

    Update EInkDynamicDisplay.cpp

--- src/graphics/EInkDynamicDisplay.cpp
+++ src/graphics/EInkDynamicDisplay.cpp
@@ -312,9 +312,9 @@ void EInkDynamicDisplay::checkFrameMatchesPrevious()
 #endif
 
     // Not redrawn, not COSMETIC, not DEMAND_FAST
-#if defined(RAK_4631_D)
-            refresh = SKIPPED;
-#endif
+    #if defined(RAK_4631_D)
+         refresh = SKIPPED;
+    #endif
     reason = FRAME_MATCHED_PREVIOUS;
     LOG_DEBUG("refresh=SKIPPED, reason=FRAME_MATCHED_PREVIOUS, frameFlags=0x%x", frameFlags);
 }
