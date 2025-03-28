0645c4400618de3f4abfd58e2433e24742b1e853
Mikhael Skvortsov <mikhael.skvortsov@gmail.com>
2025-03-28 12:39:27 +0300

    Don't malloc wdt_config

--- src/platform/esp32/main-esp32.cpp
+++ src/platform/esp32/main-esp32.cpp
@@ -164,10 +164,11 @@ void esp32Setup()
 // #define APP_WATCHDOG_SECS 45
 #define APP_WATCHDOG_SECS 90
 
-    esp_task_wdt_config_t *wdt_config = (esp_task_wdt_config_t *)malloc(sizeof(esp_task_wdt_config_t));
-    wdt_config->timeout_ms = APP_WATCHDOG_SECS * 1000;
-    wdt_config->trigger_panic = true;
-    res = esp_task_wdt_init(wdt_config);
+    esp_task_wdt_config_t wdt_config = {
+        .timeout_ms = APP_WATCHDOG_SECS * 1000,
+        .trigger_panic = true,
+    };
+    res = esp_task_wdt_init(&wdt_config);
     assert(res == ESP_OK);
 
     res = esp_task_wdt_add(NULL);
