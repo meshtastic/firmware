ccd234ac0a7ccebb4e6f9e392b4efcbd72b98706
Woutvstk <119763111+Woutvstk@users.noreply.github.com>
2025-04-08 11:02:02 +0200

    Some more unused macros

--- variants/rak4631/variant.h
+++ variants/rak4631/variant.h
@@ -130,12 +130,6 @@ static const uint8_t SCK = PIN_SPI_SCK;
 #define HAS_SDCARD 1
 #define SDCARD_USE_SPI1 1
 
-#ifdef SDCARD_USE_SPI1
-#define SPI_MOSI PIN_SPI1_MOSI
-#define SPI_SCK PIN_SPI1_SCK
-#define SPI_MISO PIN_SPI1_MISO
-#endif
-
 #define SDCARD_CS (26)
 
 // Some settings for the SdFat library to optimize flash usage
