5d48d2c0a74b1a561246899e7786aa1460cf0ef1
Nivek-domo <123359286+Nivek-domo@users.noreply.github.com>
2025-04-20 12:06:39 +0200

    Add IP Address Frame (#6615)
    * Update Screen.cpp
    
    add ip on screen if has ethernet pcb like w5500 spi
    
    * Run Trunk Format and Translate Comments FR->EN
    
    ---------
    
    Co-authored-by: Tom Fifield <tom@tomfifield.net>

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -56,6 +56,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "mesh/wifi/WiFiAPClient.h"
 #endif
 
+#if HAS_ETHERNET
+#include "mesh/eth/ethClient.h"
+#endif
+
 #ifdef ARCH_ESP32
 #include "esp_task_wdt.h"
 #include "modules/StoreForwardModule.h"
@@ -230,6 +234,42 @@ static void drawOEMBootScreen(OLEDDisplay *display, OLEDDisplayUiState *state, i
     drawOEMIconScreen(region, display, state, x, y);
 }
 
+#endif
+
+#if HAS_ETHERNET
+static void drawEthernetFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
+{
+    display->setFont(FONT_SMALL);
+    display->setTextAlignment(TEXT_ALIGN_LEFT);
+
+    if (config.display.displaymode == meshtastic_Config_DisplayConfig_DisplayMode_INVERTED) {
+        display->fillRect(0 + x, 0 + y, x + display->getWidth(), y + FONT_HEIGHT_SMALL);
+        display->setColor(BLACK);
+    }
+
+    display->setColor(WHITE);
+
+    // Adjust vertical position verticale ajustée - starts higher
+    int16_t y_offset = y + 2; // Reduces space at top
+
+    // Left Alignement (x + small offset)
+    int16_t x_offset = x + 2;
+
+    // Display is not centered, align left
+    display->drawString(x_offset, y_offset, "Ethernet Config:");
+    y_offset += FONT_HEIGHT_SMALL + 2; // Slightly reduced spacing
+
+    display->drawString(x_offset, y_offset, "IP: " + Ethernet.localIP().toString());
+    y_offset += FONT_HEIGHT_SMALL;
+
+    display->drawString(x_offset, y_offset, "Mask: " + Ethernet.subnetMask().toString());
+    y_offset += FONT_HEIGHT_SMALL;
+
+    display->drawString(x_offset, y_offset, "GW: " + Ethernet.gatewayIP().toString());
+    // y_offset += FONT_HEIGHT_SMALL;
+
+    // display->drawString(x_offset, y_offset, "DNS: " + Ethernet.dnsServerIP().toString());
+}
 #endif
 
 void Screen::drawFrameText(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y, const char *message)
@@ -2179,6 +2219,12 @@ void Screen::setFrames(FrameFocus focus)
     if (isWifiAvailable()) {
         // call a method on debugInfoScreen object (for more details)
         normalFrames[numframes++] = &Screen::drawDebugInfoWiFiTrampoline;
+    }
+#endif
+
+#if HAS_ETHERNET
+    if (Ethernet.hardwareStatus() != EthernetNoHardware) {
+        normalFrames[numframes++] = drawEthernetFrame;
     }
 #endif
 
