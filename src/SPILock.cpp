8d14f228ec40edf9f48e98da055175215702e038
m1nl <mateusz@nalewajski.pl>
2025-04-24 00:33:48 +0200

    ensure acquired SPI lock prevents light-sleep

--- src/SPILock.cpp
+++ src/SPILock.cpp
@@ -1,12 +1,57 @@
 #include "SPILock.h"
+#include "PowerFSM.h"
 #include "configuration.h"
+#include "sleep.h"
 #include <Arduino.h>
 #include <assert.h>
 
 concurrency::Lock *spiLock;
 
+class SPILock : public concurrency::Lock
+{
+  public:
+    SPILock();
+    ~SPILock();
+
+    void lock() override;
+    void unlock() override;
+
+  private:
+    bool locked;
+
+    int preflightSleepCb(void *unused = NULL) { return locked ? 1 : 0; }
+
+    CallbackObserver<SPILock, void *> preflightSleepObserver =
+        CallbackObserver<SPILock, void *>(this, &SPILock::preflightSleepCb);
+};
+
+SPILock::SPILock() : Lock()
+{
+    locked = false;
+    preflightSleepObserver.observe(&preflightSleep);
+}
+
+SPILock::~SPILock()
+{
+    preflightSleepObserver.unobserve(&preflightSleep);
+}
+
+void SPILock::lock()
+{
+    powerFSM.trigger(EVENT_WAKE_TIMER);
+
+    Lock::lock();
+    locked = true;
+}
+
+void SPILock::unlock()
+{
+    locked = false;
+    Lock::unlock();
+}
+
 void initSPI()
 {
     assert(!spiLock);
-    spiLock = new concurrency::Lock();
+    spiLock = new SPILock();
 }
