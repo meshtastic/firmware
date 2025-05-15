4842b77a6e7358e4c6199dbd1d422e363c7dc91f
Jonathan Bennett <jbennett@incomsystems.biz>
2025-05-14 21:50:39 -0500

    MVP for HostMetrics module.

---
+++ src/modules/Telemetry/HostMetrics.cpp
@@ -0,0 +1,123 @@
+#include "HostMetrics.h"
+#include "../mesh/generated/meshtastic/telemetry.pb.h"
+#include "MeshService.h"
+#include "PortduinoGlue.h"
+#include <filesystem>
+
+int32_t HostMetricsModule::runOnce()
+{
+#if ARCH_PORTDUINO
+    sendMetrics();
+    return 60 * 1000 * 30; // setting to once per 30 minutes for initial testing
+#else
+    return disabled();
+#endif
+}
+
+bool HostMetricsModule::handleReceivedProtobuf(const meshtastic_MeshPacket &mp, meshtastic_Telemetry *t)
+{
+    // Don't worry about storing telemetry in NodeDB if we're a repeater
+    if (config.device.role == meshtastic_Config_DeviceConfig_Role_REPEATER)
+        return false;
+
+    if (t->which_variant == meshtastic_Telemetry_host_metrics_tag) {
+#ifdef DEBUG_PORT
+        const char *sender = getSenderShortName(mp);
+
+        LOG_INFO("(Received Host Metrics from %s): uptime=%u, diskfree=%lu, memory free=%lu, load=%04.2f, %04.2f, %04.2f", sender,
+                 t->variant.host_metrics.uptime_seconds, t->variant.host_metrics.diskfree1_bytes,
+                 t->variant.host_metrics.freemem_bytes, static_cast<float>(t->variant.host_metrics.load1) / 100,
+                 static_cast<float>(t->variant.host_metrics.load5) / 100,
+                 static_cast<float>(t->variant.host_metrics.load15) / 100);
+#endif
+    }
+    return false; // Let others look at this message also if they want
+}
+
+meshtastic_MeshPacket *HostMetricsModule::allocReply()
+{
+    if (currentRequest) {
+        auto req = *currentRequest;
+        const auto &p = req.decoded;
+        meshtastic_Telemetry scratch;
+        meshtastic_Telemetry *decoded = NULL;
+        memset(&scratch, 0, sizeof(scratch));
+        if (pb_decode_from_bytes(p.payload.bytes, p.payload.size, &meshtastic_HostMetrics_msg, &scratch)) {
+            decoded = &scratch;
+        } else {
+            LOG_ERROR("Error decoding HostMetrics module!");
+            return NULL;
+        }
+        // Check for a request for device metrics
+        if (decoded->which_variant == meshtastic_Telemetry_host_metrics_tag) {
+            LOG_INFO("Device telemetry reply to request");
+            return allocDataProtobuf(getHostMetrics());
+        }
+    }
+    return NULL;
+}
+
+meshtastic_Telemetry HostMetricsModule::getHostMetrics()
+{
+    std::string file_line;
+    meshtastic_Telemetry t = meshtastic_HostMetrics_init_zero;
+    t.which_variant = meshtastic_Telemetry_host_metrics_tag;
+
+    if (access("/proc/uptime", R_OK) == 0) {
+        std::ifstream proc_uptime("/proc/uptime");
+        if (proc_uptime.is_open()) {
+            std::getline(proc_uptime, file_line, ' ');
+            proc_uptime.close();
+            t.variant.host_metrics.uptime_seconds = stoi(file_line);
+        }
+    }
+
+    std::filesystem::space_info root = std::filesystem::space("/");
+    t.variant.host_metrics.diskfree1_bytes = root.available;
+
+    if (access("/proc/meminfo", R_OK) == 0) {
+        std::ifstream proc_meminfo("/proc/meminfo");
+        if (proc_meminfo.is_open()) {
+            do {
+                std::getline(proc_meminfo, file_line);
+            } while (file_line.find("MemAvailable") == std::string::npos);
+            proc_meminfo.close();
+            t.variant.host_metrics.freemem_bytes = stoi(file_line.substr(file_line.find_first_of("0123456789"))) * 1024;
+        }
+    }
+    if (access("/proc/loadavg", R_OK) == 0) {
+        std::ifstream proc_loadavg("/proc/loadavg");
+        if (proc_loadavg.is_open()) {
+            std::getline(proc_loadavg, file_line, ' ');
+            t.variant.host_metrics.load1 = stof(file_line) * 100;
+            LOG_INFO("load1: %u", t.variant.host_metrics.load1);
+            std::getline(proc_loadavg, file_line, ' ');
+            t.variant.host_metrics.load5 = stof(file_line) * 100;
+            LOG_INFO("load5: %u", t.variant.host_metrics.load5);
+            std::getline(proc_loadavg, file_line, ' ');
+            t.variant.host_metrics.load15 = stof(file_line) * 100;
+            LOG_INFO("load15: %u", t.variant.host_metrics.load15);
+            proc_loadavg.close();
+        }
+    }
+
+    return t;
+}
+
+bool HostMetricsModule::sendMetrics()
+{
+    meshtastic_Telemetry telemetry = getHostMetrics();
+    LOG_INFO("Send: uptime=%u, diskfree=%lu, memory free=%lu, load=%04.2f, %04.2f, %04.2f",
+             telemetry.variant.host_metrics.uptime_seconds, telemetry.variant.host_metrics.diskfree1_bytes,
+             telemetry.variant.host_metrics.freemem_bytes, static_cast<float>(telemetry.variant.host_metrics.load1) / 100,
+             static_cast<float>(telemetry.variant.host_metrics.load5) / 100,
+             static_cast<float>(telemetry.variant.host_metrics.load15) / 100);
+
+    meshtastic_MeshPacket *p = allocDataProtobuf(telemetry);
+    p->to = NODENUM_BROADCAST;
+    p->decoded.want_response = false;
+    p->priority = meshtastic_MeshPacket_Priority_BACKGROUND;
+    LOG_INFO("Send packet to mesh");
+    service->sendToMesh(p, RX_SRC_LOCAL, true);
+    return true;
+}
