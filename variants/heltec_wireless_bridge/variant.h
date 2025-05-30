ee4cb05a4e31eb2deafa729913738224f0274f94
Andreas 'count' Kotes <berlincount@users.noreply.github.com>
2025-05-30 15:16:47 +0200

    satisfy 'trunk fmt'

--- variants/heltec_wireless_bridge/variant.h
+++ variants/heltec_wireless_bridge/variant.h
@@ -10,24 +10,24 @@
 #define NO_EXT_GPIO 1
 #define NO_GPS 1
 
-#define HAS_GPS 0     // GPS is not equipped
+#define HAS_GPS 0 // GPS is not equipped
 #undef GPS_RX_PIN
 #undef GPS_TX_PIN
 
 // Green / Lora = PIN 22 / GPIO2, Yellow / Wifi = PIN 23 / GPIO0, Blue / BLE = PIN 25 / GPIO16
-#define LED_PIN  22
+#define LED_PIN 22
 #define WIFI_LED 23
-#define BLE_LED  25
+#define BLE_LED 25
 
 // ESP32-D0WDQ6 direct pins SX1276
 #define USE_RF95
 #define LORA_DIO0 26
 #define LORA_DIO1 35
 #define LORA_DIO2 34
-#define LORA_SCK  05
+#define LORA_SCK 05
 #define LORA_MISO 19
 #define LORA_MOSI 27
-#define LORA_CS   18
+#define LORA_CS 18
 
 // several things are not possible with JTAG enabled
 #ifndef USE_JTAG
