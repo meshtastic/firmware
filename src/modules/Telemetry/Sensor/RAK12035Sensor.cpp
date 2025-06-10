e945c73b08dbd41361a57ab9e01132486fd0f342
Justin E. Mann <Justin-Mann@users.noreply.github.com>
2025-06-10 09:52:56 -0600

    merge master, attempt to fix gps (RAK12500) pin conflict with RAK12023/12035

--- src/modules/Telemetry/Sensor/RAK12035Sensor.cpp
+++ src/modules/Telemetry/Sensor/RAK12035Sensor.cpp
@@ -22,10 +22,6 @@ int32_t RAK12035Sensor::runOnce()
     sensor.get_sensor_version(&data);
     if (data != 0) {
         LOG_INFO("Init sensor: %s", sensorName);
-
-        LOG_INFO("I2C_SCL_RAK12023: %i", I2C_SCL_RAK12023);
-        LOG_INFO("I2C_SDA_RAK12023: %i", I2C_SDA_RAK12023);
-
         LOG_INFO("RAK12035Sensor Init Succeed \nSensor1 Firmware version: %i, Sensor Name: %s", data, sensorName);
         status = true;
         sensor.sensor_sleep();
