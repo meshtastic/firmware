b731074e3a9e3397c7c6c9892db73dbe782f42e1
Jonathan Bennett <jbennett@incomsystems.biz>
2025-06-09 19:37:31 -0500

    Add Timezone picker

--- src/graphics/Screen.h
+++ src/graphics/Screen.h
@@ -618,6 +618,7 @@ class Screen : public concurrency::OSThread
             uint8_t memory = 255;
             uint8_t gps = 255;
             uint8_t home = 255;
+            uint8_t clock = 255;
         } positions;
 
         uint8_t frameCount = 0;
