7f497144684f21ecbfb6ae6ef06dcad28f5ec2cb
csrutil <keming.cao@gmail.com>
2025-06-12 18:54:13 +0800

    adjust the order of the HW_VENDOR defines

--- src/platform/nrf52/architecture.h
+++ src/platform/nrf52/architecture.h
@@ -49,6 +49,8 @@
 #define HW_VENDOR meshtastic_HardwareModel_RAK2560
 #elif defined(WISMESH_TAP)
 #define HW_VENDOR meshtastic_HardwareModel_WISMESH_TAP
+#elif defined(GAT562_MESH_TRIAL_TRACKER)
+#define HW_VENDOR meshtastic_HardwareModel_GAT562_MESH_TRIAL_TRACKER
 #elif defined(RAK4630)
 #define HW_VENDOR meshtastic_HardwareModel_RAK4631
 #elif defined(TTGO_T_ECHO)
@@ -89,8 +91,6 @@
 #define HW_VENDOR meshtastic_HardwareModel_NOMADSTAR_METEOR_PRO
 #elif defined(SEEED_WIO_TRACKER_L1)
 #define HW_VENDOR meshtastic_HardwareModel_SEEED_WIO_TRACKER_L1
-#elif defined(GAT562_MESH_TRIAL_TRACKER)
-#define HW_VENDOR meshtastic_HardwareModel_GAT562_MESH_TRIAL_TRACKER
 #else
 #define HW_VENDOR meshtastic_HardwareModel_NRF52_UNKNOWN
 #endif
