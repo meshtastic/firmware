c1882ab88fb68e755f8fe994ca98c44f79e00545
Marek-mk <118679709+Marek-mk@users.noreply.github.com>
2025-06-13 18:36:04 +0200

    PacketHistory debloat RAM allocations

--- src/mesh/PacketHistory.cpp
+++ src/mesh/PacketHistory.cpp
@@ -7,152 +7,314 @@
 #endif
 #include "Throttle.h"
 
-PacketHistory::PacketHistory()
+/** PacketHistory additional debug level selection */
+#define mx_PH_DEBUGLEVEL 0 // Set to 2 for debug messages, 1 for info messages, 0 for no debug messages
+
+/////
+#define mx_LOG_INFO(...)                                                                                                         \
+    if (mx_PH_DEBUGLEVEL >= 1) {                                                                                                 \
+        LOG_INFO(__VA_ARGS__);                                                                                                   \
+    }
+#define mx_LOG_DEBUG(...)                                                                                                        \
+    if (mx_PH_DEBUGLEVEL >= 2) {                                                                                                 \
+        LOG_DEBUG(__VA_ARGS__);                                                                                                  \
+    }
+
+PacketHistory::PacketHistory(uint32_t size) : recentPacketsCapacity(0), mx_recentPackets(NULL) // Initialize members
 {
-    recentPackets.reserve(MAX_NUM_NODES); // Prealloc the worst case # of records - to prevent heap fragmentation
-                                          // setup our periodic task
+    if (size < 4 || size > PACKETHISTORY_MAX) { // Copilot suggested - makes sense
+        // LOG_ERROR("PH:Invalid size %d, using default %d", size, PACKETHISTORY_MAX);
+        size = PACKETHISTORY_MAX; // Use default size if invalid
+    }
+
+    // Allocate memory for the recent packets array
+    recentPacketsCapacity = size;
+    mx_recentPackets = new PacketRecord[recentPacketsCapacity];
+    if (!mx_recentPackets) {       // No logging here, console/log probably uninitialized yet.
+        recentPacketsCapacity = 0; // mark allocation fail
+        return;                    // return early
+    }
+
+    // Initialize the recent packets array to zero
+    memset(mx_recentPackets, 0, sizeof(PacketRecord) * recentPacketsCapacity);
 }
 
-/**
- * Update recentBroadcasts and return true if we have already seen this packet
- */
+PacketHistory::~PacketHistory()
+{
+    recentPacketsCapacity = 0;
+    delete[] mx_recentPackets;
+    mx_recentPackets = NULL;
+}
+
+/** Update recentPackets and return true if we have already seen this packet */
 bool PacketHistory::wasSeenRecently(const meshtastic_MeshPacket *p, bool withUpdate, bool *wasFallback, bool *weWereNextHop)
 {
+    if (!initOk()) {
+        LOG_ERROR("PH:wSR NOT INITIALIZED, memory allocation failed?");
+        return false;
+    }
+
     if (p->id == 0) {
-        LOG_DEBUG("Ignore message with zero id");
+        LOG_INFO("PH:wSR id=0,Ignore");
         return false; // Not a floodable message ID, so we don't care
     }
 
     PacketRecord r;
+    memset(&r, 0, sizeof(PacketRecord)); // Initialize the record to zero
+
+    // Save basic info from checked packet
     r.id = p->id;
-    r.sender = getFrom(p);
-    r.rxTimeMsec = millis();
+    r.sender = getFrom(p); // If 0 then use our ID
     r.next_hop = p->next_hop;
     r.relayed_by[0] = p->relay_node;
-    // LOG_INFO("Add relayed_by 0x%x for id=0x%x", p->relay_node, r.id);
 
-    auto found = recentPackets.find(r);
-    bool seenRecently = (found != recentPackets.end()); // found not equal to .end() means packet was seen recently
+    r.rxTimeMsec = millis(); //
+    if (r.rxTimeMsec == 0)   // =0 every 49.7 days? 0 is special
+        r.rxTimeMsec = 1;
 
-    if (seenRecently &&
-        !Throttle::isWithinTimespanMs(found->rxTimeMsec, FLOOD_EXPIRE_TIME)) { // Check whether found packet has already expired
-        recentPackets.erase(found); // Erase and pretend packet has not been seen recently
-        found = recentPackets.end();
-        seenRecently = false;
+    mx_LOG_INFO("PH:wSR s=%08x id=%08x nh=%02x rn=%02x wU=%d wFb?%d wWNH?%d", r.sender, r.id, r.next_hop, p->relay_node,
+                withUpdate, wasFallback ? *wasFallback : -1, weWereNextHop ? *weWereNextHop : -1);
+
+    PacketRecord *found = PRfind(r.sender, r.id); // Find the packet record in the recentPackets array
+    bool seenRecently = (found != NULL);          // If found -> the packet was seen recently
+
+    // Expiring packets can be disabled, we have static allocation for history.
+#if FLOOD_EXPIRE_TIME > 0
+    if (seenRecently) { // Check whether found packet has already expired
+        if (!Throttle::isWithinTimespanMs(found->rxTimeMsec,
+                                          FLOOD_EXPIRE_TIME)) { // Check whether found packet has already expired
+            mx_LOG_INFO("PH:wSR s=%08x id=%08x nh=%02x rby=%02x %02x %02x age=%d>%d slot=%d/%d CLEAR", found->sender, found->id,
+                        found->next_hop, found->relayed_by[0], found->relayed_by[1], found->relayed_by[2],
+                        millis() - found->rxTimeMsec, FLOOD_EXPIRE_TIME, found - mx_recentPackets, recentPacketsCapacity);
+
+            memset(found, 0, sizeof(PacketRecord)); // Clear the record
+            found = NULL;
+            seenRecently = false;
+        } else {
+            mx_LOG_INFO("PH:wSR s=%08x id=%08x nh=%02x rby=%02x %02x %02x age=%d<%d slot=%d/%d KEEP", found->sender, found->id,
+                        found->next_hop, found->relayed_by[0], found->relayed_by[1], found->relayed_by[2],
+                        millis() - found->rxTimeMsec, FLOOD_EXPIRE_TIME, found - mx_recentPackets, recentPacketsCapacity);
+            // only debug log here
+        }
     }
+#endif
+
+    // LOG_DEBUG("PH:wSR Found record for fr=%08x id=%08x to=%08x",
+    //     p->from, p->id, p->to);     // But we don't save p->to. Keeping it as it was.
 
     if (seenRecently) {
-        LOG_DEBUG("Found existing packet record for fr=0x%x,to=0x%x,id=0x%x", p->from, p->to, p->id);
-        uint8_t ourRelayID = nodeDB->getLastByteOfNodeNum(nodeDB->getNodeNum());
+        uint8_t ourRelayID = nodeDB->getLastByteOfNodeNum(nodeDB->getNodeNum()); // Get our relay ID from our node number
+
         if (wasFallback) {
             // If it was seen with a next-hop not set to us and now it's NO_NEXT_HOP_PREFERENCE, and the relayer relayed already
             // before, it's a fallback to flooding. If we didn't already relay and the next-hop neither, we might need to handle
             // it now.
             if (found->sender != nodeDB->getNodeNum() && found->next_hop != NO_NEXT_HOP_PREFERENCE &&
-                found->next_hop != ourRelayID && p->next_hop == NO_NEXT_HOP_PREFERENCE && wasRelayer(p->relay_node, found) &&
-                !wasRelayer(ourRelayID, found) && !wasRelayer(found->next_hop, found)) {
+                found->next_hop != ourRelayID && p->next_hop == NO_NEXT_HOP_PREFERENCE && wasRelayer(p->relay_node, *found) &&
+                !wasRelayer(ourRelayID, *found) &&
+                !wasRelayer(
+                    found->next_hop,
+                    *found)) { // If we were not the next hop and the next hop is not us, and we are not relaying this packet
+                mx_LOG_DEBUG("PH:wSR f=%08x id=%08x nh=%02x rn=%02x oID=%02x, wasFbk=%d-set TRUE", p->from, p->id, p->next_hop,
+                             p->relay_node, ourRelayID, wasFallback ? *wasFallback : -1);
                 *wasFallback = true;
+            } else {
+                mx_LOG_DEBUG("PH:wSR f=%08x id=%08x nh=%02x rn=%02x oID=%02x, wasFbk=%d-no change", p->from, p->id, p->next_hop,
+                             p->relay_node, ourRelayID, wasFallback ? *wasFallback : -1);
+                // debug log only
             }
         }
 
         // Check if we were the next hop for this packet
         if (weWereNextHop) {
-            *weWereNextHop = found->next_hop == ourRelayID;
+            *weWereNextHop = (found->next_hop == ourRelayID);
+            mx_LOG_DEBUG("PH:wSR f=%08x id=%08x nh=%02x rn=%02x foundnh=%02x oID=%02x -> wWNH=%d", p->from, p->id, p->next_hop,
+                         p->relay_node, found->next_hop, ourRelayID, *weWereNextHop);
         }
     }
 
     if (withUpdate) {
-        if (found != recentPackets.end()) { // delete existing to updated timestamp and relayed_by (re-insert)
+        if (found != NULL) {
+            mx_LOG_DEBUG("PH:wSR s=%08x id=%08x nh=%02x rby=%02x %02x %02x age=%d wUpd BEFORE", found->sender, found->id,
+                         found->next_hop, found->relayed_by[0], found->relayed_by[1], found->relayed_by[2],
+                         millis() - found->rxTimeMsec);
+
             // Add the existing relayed_by to the new record
-            for (uint8_t i = 0; i < NUM_RELAYERS - 1; i++) {
-                if (found->relayed_by[i])
+            for (uint8_t i = 0; i < (NUM_RELAYERS - 1); i++) {
+                if (found->relayed_by[i] != 0)
                     r.relayed_by[i + 1] = found->relayed_by[i];
             }
             r.next_hop = found->next_hop; // keep the original next_hop (such that we check whether we were originally asked)
-            recentPackets.erase(found);   // as unsorted_set::iterator is const (can't update - so re-insert..)
+
+            mx_LOG_DEBUG("PH:wSR s=%08x id=%08x nh=%02x rby=%02x %02x %02x age=%d wUpd AFTER", r.sender, r.id, r.next_hop,
+                         r.relayed_by[0], r.relayed_by[1], r.relayed_by[2], millis() - r.rxTimeMsec);
         }
-        recentPackets.insert(r);
-        LOG_DEBUG("Add packet record fr=0x%x, id=0x%x", p->from, p->id);
+        PRinsert(r); // Insert or update the packet record in the history
     }
 
-    // Capacity is reerved, so only purge expired packets if recentPackets fills past 90% capacity
-    // Expiry is normally dealt with after having searched/found a packet (above)
-    if (recentPackets.size() > (MAX_NUM_NODES * 0.9)) {
-        clearExpiredRecentPackets();
-    }
+    LOG_INFO("PH:wSR src=%08x id=%08x to=%08x relby=%02x %02x %02x nxthop=%02x seenRecently?%d ageMs=%d", r.sender, r.id, p->to,
+             r.relayed_by[0], r.relayed_by[1], r.relayed_by[2], r.next_hop, seenRecently,
+             found ? (millis() - found->rxTimeMsec) : -1);
 
     return seenRecently;
 }
 
-/**
- * Iterate through all recent packets, and remove all older than FLOOD_EXPIRE_TIME
- */
-void PacketHistory::clearExpiredRecentPackets()
+/** Find a packet record in history.
+ * @return pointer to PacketRecord if found, NULL if not found */
+PacketHistory::PacketRecord *PacketHistory::PRfind(NodeNum sender, PacketId id)
 {
-    LOG_DEBUG("recentPackets size=%ld", recentPackets.size());
+    if (sender == 0 || id == 0) {
+        mx_LOG_DEBUG("PH:PRf s=%08x id=%08x sender/id=0->NOT FOUND", sender, id);
+        return NULL;
+    }
 
-    for (auto it = recentPackets.begin(); it != recentPackets.end();) {
-        if (!Throttle::isWithinTimespanMs(it->rxTimeMsec, FLOOD_EXPIRE_TIME)) {
-            it = recentPackets.erase(it); // erase returns iterator pointing to element immediately following the one erased
+    PacketRecord *it = NULL;
+    for (it = mx_recentPackets; it < (mx_recentPackets + recentPacketsCapacity); ++it) {
+        if (it->id == id && it->sender == sender) {
+            mx_LOG_DEBUG("PH:PRf s=%08x id=%08x FOUND nh=%02x rby=%02x %02x %02x age=%d slot=%d/%d", it->sender, it->id,
+                         it->next_hop, it->relayed_by[0], it->relayed_by[1], it->relayed_by[2], millis() - (it->rxTimeMsec),
+                         it - mx_recentPackets, recentPacketsCapacity);
+            return it; // Return pointer to the found record
+        }
+    }
+    mx_LOG_DEBUG("PH:PRf s=%08x id=%08x NOT FOUND", sender, id);
+    return NULL; // Not found
+}
+
+/** Insert/Replace oldest PacketRecord in mx_recentPackets. */
+void PacketHistory::PRinsert(PacketRecord &r)
+{
+    uint32_t OldestrxTimeMsec = 0;
+    uint32_t now_millis = millis(); // Should not jump with time changes
+    uint32_t rxTimeMsec = 0;
+    PacketRecord *tu = NULL; // Will insert here.
+    PacketRecord *it = NULL;
+
+    // Find a free or oldest slot in the mx_recentPackets array
+    for (it = mx_recentPackets; it < (mx_recentPackets + recentPacketsCapacity); ++it) {
+        rxTimeMsec = it->rxTimeMsec;
+        if (it->id == 0 && it->sender == 0 && rxTimeMsec == 0) { // Record is empty
+            tu = it;                                             // Remember the free slot
+            mx_LOG_DEBUG("PH:PRi Free slot@ %d/%d", tu - mx_recentPackets, recentPacketsCapacity);
+            // We have that, Exit the loop
+            it = (mx_recentPackets + recentPacketsCapacity);
         } else {
-            ++it;
+            if (rxTimeMsec != 0 && (now_millis - rxTimeMsec) > OldestrxTimeMsec) { // 49.7 days rollover friendly
+                OldestrxTimeMsec = now_millis - rxTimeMsec;
+                tu = it; // remember the oldest packet
+            }
+        }
+    }
+    // Full loop was made - we have oldest used - clear it
+    if (it == (mx_recentPackets + recentPacketsCapacity)) {
+        if (tu != NULL) {
+            mx_LOG_DEBUG("PH:PRi Reuse slot@ %d/%d age=%d", tu - mx_recentPackets, recentPacketsCapacity,
+                         millis() - tu->rxTimeMsec); // mx
+            memset(tu, 0, sizeof(PacketRecord));     // Clear the record
         }
     }
 
-    LOG_DEBUG("recentPackets size=%ld (after clearing expired packets)", recentPackets.size());
+    if (tu == NULL) {
+        LOG_ERROR("PH:PRi No free slot, no old packet to clear?? NOT SAVING PACKET"); // mx
+        // assert(false); // This should never happen, we should always have at least one packet to clear
+        return; // Return early if we can't update the history
+    }
+
+    *tu = r; // store the packet
+
+    // debug info
+    if (it == (mx_recentPackets + recentPacketsCapacity)) { // reused
+        mx_LOG_INFO("PH:PRi s=%08x id=%08x nh=%02x rby=%02x %02x %02x age=%d Reuse@ %d/%d oldage=%d", tu->sender, tu->id,
+                    tu->next_hop, tu->relayed_by[0], tu->relayed_by[1], tu->relayed_by[2], millis() - (tu->rxTimeMsec),
+                    tu - mx_recentPackets, recentPacketsCapacity, millis() - OldestrxTimeMsec);
+    } else { // empty
+        mx_LOG_INFO("PH:PRi s=%08x id=%08x nh=%02x rby=%02x %02x %02x age=%d  save@ %d/%d New", tu->sender, tu->id, tu->next_hop,
+                    tu->relayed_by[0], tu->relayed_by[1], tu->relayed_by[2], millis() - (tu->rxTimeMsec), tu - mx_recentPackets,
+                    recentPacketsCapacity);
+    }
 }
 
 /* Check if a certain node was a relayer of a packet in the history given an ID and sender
  * @return true if node was indeed a relayer, false if not */
 bool PacketHistory::wasRelayer(const uint8_t relayer, const uint32_t id, const NodeNum sender)
 {
-    if (relayer == 0)
-        return false;
-
-    PacketRecord r = {.sender = sender, .id = id, .rxTimeMsec = 0, .next_hop = 0};
-    auto found = recentPackets.find(r);
-
-    if (found == recentPackets.end()) {
+    if (!initOk()) {
+        LOG_ERROR("PH:wRl NOT INITIALIZED, memory allocation failed?");
         return false;
     }
 
-    return wasRelayer(relayer, found);
+    if (relayer == 0) {
+        mx_LOG_DEBUG("PH:wRl s=%08x id=%08x rl=%02x=zero. NO", sender, id, relayer);
+        return false;
+    }
+
+    PacketRecord *found = PRfind(sender, id);
+
+    if (found == NULL) {
+        mx_LOG_DEBUG("PH:wRl s=%08x id=%08x (rl=%02x) PR not found. NO", sender, id, relayer);
+        return false;
+    }
+
+    // LOG_DEBUG("PH:wRl s=%08x id=%08x nh=%02x age=%d rls=%02x %02x %02x FOUND check:%02x",
+    //         found->sender, found->id, found->next_hop, millis() - found->rxTimeMsec,
+    //         found->relayed_by[0], found->relayed_by[1], found->relayed_by[2],
+    //         relayer);
+    return wasRelayer(relayer, *found);
 }
 
 /* Check if a certain node was a relayer of a packet in the history given iterator
  * @return true if node was indeed a relayer, false if not */
-bool PacketHistory::wasRelayer(const uint8_t relayer, std::unordered_set<PacketRecord, PacketRecordHashFunction>::iterator r)
+bool PacketHistory::wasRelayer(const uint8_t relayer, PacketRecord &r)
 {
     for (uint8_t i = 0; i < NUM_RELAYERS; i++) {
-        if (r->relayed_by[i] == relayer) {
+        if (r.relayed_by[i] == relayer) {
+            mx_LOG_INFO("PH:wRP s=%08x id=%08x rls=%02x %02x %02x rl=%02x? YES", r.sender, r.id, r.relayed_by[0], r.relayed_by[1],
+                        r.relayed_by[2], relayer);
             return true;
         }
     }
+    mx_LOG_INFO("PH:wRP s=%08x id=%08x rls=%02x %02x %02x rl=%02x? NO", r.sender, r.id, r.relayed_by[0], r.relayed_by[1],
+                r.relayed_by[2], relayer);
     return false;
 }
 
 // Remove a relayer from the list of relayers of a packet in the history given an ID and sender
 void PacketHistory::removeRelayer(const uint8_t relayer, const uint32_t id, const NodeNum sender)
 {
-    PacketRecord r = {.sender = sender, .id = id, .rxTimeMsec = 0, .next_hop = 0};
-    auto found = recentPackets.find(r);
-
-    if (found == recentPackets.end()) {
+    if (!initOk()) {
+        LOG_ERROR("PH:rRl NOT INITIALIZED, memory allocation failed?");
         return;
     }
-    // Make a copy of the found record
-    r.next_hop = found->next_hop;
-    r.rxTimeMsec = found->rxTimeMsec;
+    // LOG_DEBUG("PH:rRl s=%08x id=%08x rlayer=%02x",
+    //            sender, id, relayer);
 
-    // Only add the relayers that are not the one we want to remove
-    uint8_t j = 0;
-    for (uint8_t i = 0; i < NUM_RELAYERS; i++) {
-        if (found->relayed_by[i] != relayer) {
-            r.relayed_by[j] = found->relayed_by[i];
-            j++;
-        }
+    PacketRecord *found = PRfind(sender, id);
+    if (found == NULL) {
+        mx_LOG_DEBUG("PH:rRl s=%08x id=%08x (rl=%02x) NOT FOUND", sender, id, relayer);
+        return; // Nothing to remove
     }
 
-    recentPackets.erase(found);
-    recentPackets.insert(r);
+    mx_LOG_DEBUG("PH:rRl s=%08x id=%08x rby=%02x %02x %02x, rl:%02x BEFORE", found->sender, found->id, found->relayed_by[0],
+                 found->relayed_by[1], found->relayed_by[2], relayer);
+
+    // nexthop and rxTimeMsec too stay in found entry
+
+    uint8_t j = 0;
+    uint8_t i = 0;
+    for (; i < NUM_RELAYERS; i++) {
+        if (found->relayed_by[i] != relayer) {
+            found->relayed_by[j] = found->relayed_by[i];
+            j++;
+        } else
+            found->relayed_by[i] = 0;
+    }
+    for (; j < NUM_RELAYERS; j++) { // Clear the rest of the relayed_by array
+        found->relayed_by[j] = 0;
+    }
+
+    mx_LOG_DEBUG("PH:rRl s=%08x id=%08x rby=%02x %02x %02x, rl:%02x AFTER", found->sender, found->id, found->relayed_by[0],
+                 found->relayed_by[1], found->relayed_by[2], relayer);
+
+    LOG_INFO("PH:rRl src=%08x id=%08x relby=%02x %02x %02x  rl:%02x removed?%d", found->sender, found->id, found->relayed_by[0],
+             found->relayed_by[1], found->relayed_by[2], relayer, i != j);
 }
--- src/mesh/PacketHistory.h
+++ src/mesh/PacketHistory.h
@@ -1,36 +1,12 @@
 #pragma once
 
 #include "NodeDB.h"
-#include <unordered_set>
 
-/// We clear our old flood record 10 minutes after we see the last of it
-#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
-#define FLOOD_EXPIRE_TIME (5 * 1000L) // Don't allow too many packets to accumulate when fuzzing.
-#else
-#define FLOOD_EXPIRE_TIME (10 * 60 * 1000L)
-#endif
+#define PACKETHISTORY_MAX (int)(MAX_NUM_NODES * 2.0) // x2..3  Should suffice. Empirical setup. 16B per record malloc'ed.
+#define FLOOD_EXPIRE_TIME (10 * 60 * 1000L) // Is this needed anymore? If not set to 0.  No change for fuzzing. Should withstand.
 
 #define NUM_RELAYERS                                                                                                             \
     3 // Number of relayer we keep track of. Use 3 to be efficient with memory alignment of PacketRecord to 16 bytes
-
-/**
- * A record of a recent message broadcast
- */
-struct PacketRecord {
-    NodeNum sender;
-    PacketId id;
-    uint32_t rxTimeMsec;              // Unix time in msecs - the time we received it
-    uint8_t next_hop;                 // The next hop asked for this packet
-    uint8_t relayed_by[NUM_RELAYERS]; // Array of nodes that relayed this packet
-
-    bool operator==(const PacketRecord &p) const { return sender == p.sender && id == p.id; }
-};
-
-class PacketRecordHashFunction
-{
-  public:
-    size_t operator()(const PacketRecord &p) const { return (std::hash<NodeNum>()(p.sender)) ^ (std::hash<PacketId>()(p.id)); }
-};
 
 /**
  * This is a mixin that adds a record of past packets we have seen
@@ -38,12 +14,37 @@ class PacketRecordHashFunction
 class PacketHistory
 {
   private:
-    std::unordered_set<PacketRecord, PacketRecordHashFunction> recentPackets;
+    struct PacketRecord { // A record of a recent message broadcast, no need to be visible outside this class.
+        NodeNum sender;
+        PacketId id;
+        uint32_t rxTimeMsec;              // Unix time in msecs - the time we received it,  0 means empty
+        uint8_t next_hop;                 // The next hop asked for this packet
+        uint8_t relayed_by[NUM_RELAYERS]; // Array of nodes that relayed this packet
+    };                                    // 4B + 4B + 4B + 1B + 3B = 16B
 
-    void clearExpiredRecentPackets(); // clear all recentPackets older than FLOOD_EXPIRE_TIME
+    uint32_t recentPacketsCapacity =
+        0; // Can be set in constructor, no need to recompile. Used to allocate memory for mx_recentPackets.
+    PacketRecord *mx_recentPackets = NULL; // Simple and fixed in size. Debloat.
 
+    /** Find a packet record in history.
+     * @param sender NodeNum
+     * @param id PacketId
+     * @return pointer to PacketRecord if found, NULL if not found */
+    PacketRecord *PRfind(NodeNum sender, PacketId id);
+
+    /** Insert/Replace oldest PacketRecord in mx_recentPackets.
+     * @param r PacketRecord to insert or replace */
+    void PRinsert(PacketRecord &r);
+
+    /* Check if a certain node was a relayer of a packet in the history given iterator
+     * @return true if node was indeed a relayer, false if not */
+    bool wasRelayer(const uint8_t relayer, PacketRecord &r);
+
+    PacketHistory(const PacketHistory &);            // non construction-copyable
+    PacketHistory &operator=(const PacketHistory &); // non copyable
   public:
-    PacketHistory();
+    explicit PacketHistory(uint32_t size = PACKETHISTORY_MAX); // Constructor with size parameter, default to PACKETHISTORY_MAX
+    ~PacketHistory();
 
     /**
      * Update recentBroadcasts and return true if we have already seen this packet
@@ -59,10 +60,9 @@ class PacketHistory
      * @return true if node was indeed a relayer, false if not */
     bool wasRelayer(const uint8_t relayer, const uint32_t id, const NodeNum sender);
 
-    /* Check if a certain node was a relayer of a packet in the history given iterator
-     * @return true if node was indeed a relayer, false if not */
-    bool wasRelayer(const uint8_t relayer, std::unordered_set<PacketRecord, PacketRecordHashFunction>::iterator r);
-
     // Remove a relayer from the list of relayers of a packet in the history given an ID and sender
     void removeRelayer(const uint8_t relayer, const uint32_t id, const NodeNum sender);
+
+    // Can Check if the PacketHistory was initialized correctly by constructor
+    bool initOk(void) { return mx_recentPackets != NULL && recentPacketsCapacity != 0; }
 };
