f68c431d9dfc576441ff90f5c33b987484907c57
Todd Herbert <herbert.todd@gmail.com>
2025-06-15 04:00:13 +1200

    Fix file encoding > We’ve detected the file encoding as ISO-8859-1. When you commit changes we will transcode it to UTF-8.

--- variants/diy/nrf52_promicro_diy_tcxo/variant.h
+++ variants/diy/nrf52_promicro_diy_tcxo/variant.h
@@ -22,26 +22,26 @@ extern "C" {
 /*
 NRF52 PRO MICRO PIN ASSIGNMENT
 
-| Pin � | Function �  | �   | Pin � �  | Function � � | RF95  |
+| Pin   | Function    |     | Pin      | Function     | RF95  |
 | ----- | ----------- | --- | -------- | ------------ | ----- |
-| Gnd � | � � � � � � | �   | vbat � � | � � � � � �  |       |
-| P0.06 | Serial2 RX  | �   | vbat � � | � � � � � �  |       |
-| P0.08 | Serial2 TX  | �   | Gnd � �  | � � � � � �  |       |
-| Gnd � | � � � � � � | �   | reset �  | � � � � � �  |       |
-| Gnd � | � � � � � � | �   | ext_vcc  | *see 0.13 �  |       |
-| P0.17 | RXEN � � �  | �   | P0.31 �  | BATTERY_PIN  |       |
-| P0.20 | GPS_RX � �  | �   | P0.29 �  | BUSY � � � � | DIO0  |
-| P0.22 | GPS_TX � �  | �   | P0.02 �  | MISO         | MISO  |
-| P0.24 | GPS_EN � �  | �   | P1.15 �  | MOSI � � � � | MOSI  |
-| P1.00 | BUTTON_PIN  | �   | P1.13 �  | CS � � � � � | CS �  |
-| P0.11 | SCL � � � � | �   | P1.11 �  | SCK � � � �  | SCK   |
-| P1.04 | SDA � � � � | �   | P0.10 �  | DIO1/IRQ � � | DIO1  |
-| P1.06 | Free pin �  | �   | P0.09 �  | RESET � � �  | RST   |
-| � � � | � � � � � � | �   | � � � �  | � � � � � �  |       |
-| � � � | Mid board � | �   | � � � �  | Internal � � |       |
-| P1.01 | Free pin �  | �   | 0.15 � � | LED � � � �  |       |
-| P1.02 | Free pin �  | �   | 0.13 � � | 3V3_EN � � � |       |
-| P1.07 | Free pin �  | �   | � � � �  | � � � � � �  |       |
+| Gnd   |             |     | vbat     |              |       |
+| P0.06 | Serial2 RX  |     | vbat     |              |       |
+| P0.08 | Serial2 TX  |     | Gnd      |              |       |
+| Gnd   |             |     | reset    |              |       |
+| Gnd   |             |     | ext_vcc  | *see 0.13    |       |
+| P0.17 | RXEN        |     | P0.31    | BATTERY_PIN  |       |
+| P0.20 | GPS_RX      |     | P0.29    | BUSY         | DIO0  |
+| P0.22 | GPS_TX      |     | P0.02    | MISO         | MISO  |
+| P0.24 | GPS_EN      |     | P1.15    | MOSI         | MOSI  |
+| P1.00 | BUTTON_PIN  |     | P1.13    | CS           | CS    |
+| P0.11 | SCL         |     | P1.11    | SCK          | SCK   |
+| P1.04 | SDA         |     | P0.10    | DIO1/IRQ     | DIO1  |
+| P1.06 | Free pin    |     | P0.09    | RESET        | RST   |
+|       |             |     |          |              |       |
+|       | Mid board   |     |          | Internal     |       |
+| P1.01 | Free pin    |     | 0.15     | LED          |       |
+| P1.02 | Free pin    |     | 0.13     | 3V3_EN       |       |
+| P1.07 | Free pin    |     |          |              |       |
 */
 
 // Number of pins defined in PinDescription array
