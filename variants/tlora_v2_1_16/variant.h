29a30fdc655e165d3c1a48c217d9a06436f79a83
Andrew Yong <me@ndoo.sg>
2025-06-29 22:56:19 +0800

    Revert "tlora_v2_1_16: Unset BUTTON_PIN and BUTTON_NEED_PULLUP (#5535)"
    Because the button thread doesn't start without a BUTTON_PIN defined, user-defined button GPIO doesn't work, even after a reboot.
    
    Since there is no developer time to rework the button thread code, revert this for now for existing users that depend on this functionality, even though moving forward, variants without a button should not have it defined.
    
    This reverts commit 3ae85e2c829427b3a6a64506afa9b6ecdf3ad081.
    Fixes #6213

--- variants/tlora_v2_1_16/variant.h
+++ variants/tlora_v2_1_16/variant.h
@@ -8,7 +8,10 @@
 #define I2C_SDA 21 // I2C pins for this board
 #define I2C_SCL 22
 
-#define LED_PIN 25 // If defined we will blink this LED
+#define LED_PIN 25    // If defined we will blink this LED
+#define BUTTON_PIN 12 // If defined, this will be used for user button presses,
+
+#define BUTTON_NEED_PULLUP
 
 #define USE_RF95
 #define LORA_DIO0 26 // a No connect on the SX1262 module
