7176366cbb153fa95c5c6f99916d1c5b777094c9
Jason P <applewiz@mac.com>
2025-07-02 23:43:48 -0500

    Logic is hard when committing blind

--- src/graphics/draw/ClockRenderer.cpp
+++ src/graphics/draw/ClockRenderer.cpp
@@ -230,7 +230,7 @@ void drawDigitalClockFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int1
 
 #ifdef T_WATCH_S3
     float scale = 1.5;
-#elif CHATTER_2
+#elif defined(CHATTER_2)
     float scale = 1.1;
 #else
     float scale = 0.75;
