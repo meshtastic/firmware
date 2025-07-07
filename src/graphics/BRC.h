252023d68413cdc38f895e65c27e9f6d7f0b221f
Ted Schundler <tschundler@gmail.com>
2025-07-06 23:07:53 -0700

    fixed roads + script to make roads

--- src/graphics/BRC.h
+++ src/graphics/BRC.h
@@ -21,20 +21,58 @@ struct StreetInfo {
     const char* name;
 };
 
+/*
+# python code to generate the StreetInfo
+
+esp_center = 2500
+street_info = [
+  # name, width, preceeding block depth
+  ('Esp', 40, 60), # block size is fake
+  ('A',   30, 400),
+  ('B',   30, 250),
+  ('C',   30, 250),
+  ('D',   30, 250),
+  ('E',   40, 250),
+  ('F',   30, 450), # E-F block is exra deep
+  ('G',   30, 250),
+  ('H',   30, 250),
+  ('I',   30, 250),
+  ('J',   30, 150),
+  ('K',   50, 150),
+]
+
+street_center = esp_center - street_info[0][1] //2 - street_info[0][2]
+last_center = esp_center
+for (name, street_width, block_width) in street_info:
+    offset = (street_width + block_width) // 2
+    street_center += street_width //2 + block_width
+
+    dia = street_center * 2
+    dist = street_center - last_center
+
+    print(f"{{{street_center}, {offset}, \"{name}\"}},\t// +{dist}ft\tdia: {dia:,}ft")
+
+    last_center = street_center
+    street_center += street_width //2
+
+street_center += 50 # extra buffer after the edge of k to include walk-in camping parking
+print(f"{{{street_center}, 0, nullptr}},\t// +{street_center-last_center}ft")
+*/
+
 static const StreetInfo streets[] = {
-    {2500, 50, "Esp"},
-    {2940, 220, "A"},
-    {2940+290, 145, "B"},
-    {2940+290*2, 145, "C"},
-    {2940+290*3, 145, "D"},
-    {2940+290*4, 145, "E"},
-    {2940+290*4+490, 245, "F"},
-    {2940+290*5+490, 145, "G"},
-    {2940+290*6+490, 145, "H"},
-    {2940+290*7+490, 145, "I"},
-    {2940+290*7+490+190, 95, "J"},
-    {2940+290*7+490+190*2, 95, "K"},
-    {2940+290*7+490+190*2+75, 0, nullptr}
+    {2500, 50, "Esp"},	// +0ft	dia: 5,000ft
+    {2935, 215, "A"},	// +435ft	dia: 5,870ft
+    {3215, 140, "B"},	// +280ft	dia: 6,430ft
+    {3495, 140, "C"},	// +280ft	dia: 6,990ft
+    {3775, 140, "D"},	// +280ft	dia: 7,550ft
+    {4060, 145, "E"},	// +285ft	dia: 8,120ft
+    {4545, 240, "F"},	// +485ft	dia: 9,090ft
+    {4825, 140, "G"},	// +280ft	dia: 9,650ft
+    {5105, 140, "H"},	// +280ft	dia: 10,210ft
+    {5385, 140, "I"},	// +280ft	dia: 10,770ft
+    {5565, 90, "J"},	// +180ft	dia: 11,130ft
+    {5755, 100, "K"},	// +190ft	dia: 11,510ft
+    {5830, 0, nullptr},	// +75ft
 };
 
 static char* BRCAddress(int32_t lat, int32_t lon)
