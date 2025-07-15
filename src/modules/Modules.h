cd0be770a547bd693a91017cb7a5fa02b62d5330
KENT_ANDERSEN <bladerunner1@onepsi.com>
2025-07-15 18:43:24 -0500

    Add hammer_board with W5500 Ethernet on HSPI and second button on IO0

--- src/modules/Modules.h
+++ src/modules/Modules.h
@@ -2,5 +2,12 @@
 
 /**
  * Create module instances here.  If you are adding a new module, you must 'new' it here (or somewhere else)
+ * 
+ * 
  */
+// src/modules/Modules.h
+#ifdef USE_WS5500
+#include "EthernetModule/EthernetModule.h"
+extern EthernetModule ethernetModule; // Non-pointer
+#endif
 void setupModules();
