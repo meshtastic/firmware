8a4f5c15e706aeb194881396d69a4dea6122e8c5
oscgonfer <oscgonfer@gmail.com>
2025-07-29 14:34:09 +0200

    Add admin commands to SCD4X class

--- src/modules/Telemetry/Sensor/SCD4XSensor.h
+++ src/modules/Telemetry/Sensor/SCD4XSensor.h
@@ -6,10 +6,28 @@
 #include "TelemetrySensor.h"
 #include <SensirionI2cScd4x.h>
 
+#define SCD4X_I2C_CLOCK_SPEED 100000
+
 class SCD4XSensor : public TelemetrySensor
 {
   private:
     SensirionI2cScd4x scd4x;
+    TwoWire* bus;
+    uint8_t address;
+
+    bool performFRC(uint32_t targetCO2);
+    bool setASCBaseline(uint32_t targetCO2);
+    bool getASC(uint16_t &ascEnabled);
+    bool setASC(bool ascEnabled);
+    bool setTemperature(float tempReference);
+    bool getAltitude(uint16_t &altitude);
+    bool setAltitude(uint32_t altitude);
+    bool setAmbientPressure(uint32_t ambientPressure);
+    bool restoreClock(uint32_t currentClock);
+    bool factoryReset();
+
+    // Parameters
+    uint16_t ascActive;
 
   protected:
     virtual void setup() override;
@@ -18,6 +36,8 @@ class SCD4XSensor : public TelemetrySensor
     SCD4XSensor();
     virtual int32_t runOnce() override;
     virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
+    AdminMessageHandleResult handleAdminMessage(const meshtastic_MeshPacket &mp, meshtastic_AdminMessage *request,
+                                                meshtastic_AdminMessage *response) override;
 };
 
 #endif
