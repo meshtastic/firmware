7c485dd3f01c7c8507739ee5703deca5d2eb591c
oscgonfer <oscgonfer@gmail.com>
2025-08-04 14:55:56 +0200

    Add further admin commands and fixes.

--- src/modules/Telemetry/Sensor/SCD4XSensor.h
+++ src/modules/Telemetry/Sensor/SCD4XSensor.h
@@ -22,18 +22,30 @@ class SCD4XSensor : public TelemetrySensor
     bool setTemperature(float tempReference);
     bool getAltitude(uint16_t &altitude);
     bool setAltitude(uint32_t altitude);
+    bool getAmbientPressure(uint32_t &ambientPressure);
     bool setAmbientPressure(uint32_t ambientPressure);
-    bool restoreClock(uint32_t currentClock);
+#ifdef SCD4X_I2C_CLOCK_SPEED
+    uint32_t setI2CClock(uint32_t currentClock);
+#endif
     bool factoryReset();
+    bool setPowerMode(bool _lowPower);
+    bool startMeasurement();
+    bool stopMeasurement();
 
     // Parameters
     uint16_t ascActive;
+    bool lowPower = true;
 
   protected:
     virtual void setup() override;
 
   public:
     SCD4XSensor();
+    enum SCD4XState { SCD4X_OFF, SCD4X_IDLE, SCD4X_MEASUREMENT };
+    SCD4XState state = SCD4X_OFF;
+    SCD4xSensorVariant sensorVariant;
+    bool sleep();
+    bool wakeUp();
     virtual int32_t runOnce() override;
     virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
     AdminMessageHandleResult handleAdminMessage(const meshtastic_MeshPacket &mp, meshtastic_AdminMessage *request,
