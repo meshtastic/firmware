5529d4739d19bdd8809437fe9187e57128415eb4
Jake-B <jake-b@users.noreply.github.com>
2025-08-18 16:27:08 -0400

    Move heartbeat response before !available guard.

--- src/mesh/PhoneAPI.cpp
+++ src/mesh/PhoneAPI.cpp
@@ -192,13 +192,7 @@ bool PhoneAPI::handleToRadio(const uint8_t *buf, size_t bufLength)
 
 size_t PhoneAPI::getFromRadio(uint8_t *buf)
 {
-    if (!available()) {
-        return 0;
-    }
-    // In case we send a FromRadio packet
-    memset(&fromRadioScratch, 0, sizeof(fromRadioScratch));
-
-    // Respond to heartbeat by sending queue status
+     // Respond to heartbeat by sending queue status
     if (heartbeatReceived) {
         memset(&fromRadioScratch, 0, sizeof(fromRadioScratch));
         fromRadioScratch.which_payload_variant = meshtastic_FromRadio_queueStatus_tag;
@@ -208,6 +202,12 @@ size_t PhoneAPI::getFromRadio(uint8_t *buf)
         LOG_DEBUG("FromRadio=STATE_SEND_QUEUE_STATUS, numbytes=%u", numbytes);
         return numbytes;
     }
+    
+    if (!available()) {
+        return 0;
+    }
+    // In case we send a FromRadio packet
+    memset(&fromRadioScratch, 0, sizeof(fromRadioScratch));
 
     // Advance states as needed
     switch (state) {
