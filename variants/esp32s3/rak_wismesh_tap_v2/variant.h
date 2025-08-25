a2e5f28be127a558f6224a72e6caf18e05e9f0da
Daniel.Cao <daniel.cao@rakwireless.com>
2025-08-25 10:47:20 +0800

    Add initial variant and platformio configuration for rak wismesh tap v2

--- variants/esp32s3/rak_wismesh_tap_v2/variant.h
+++ variants/esp32s3/rak_wismesh_tap_v2/variant.h
@@ -42,39 +42,10 @@
 #define GPS_TX_PIN 43
 #define GPS_RX_PIN 44
 
-// #define BATTERY_PIN 10
-// #define ADC_CHANNEL ADC1_GPIO10_CHANNEL
-// #define ADC_MULTIPLIER   1.667
-
 #define SPI_MOSI (11)
 #define SPI_SCK (13)
 #define SPI_MISO (10)
 #define SPI_CS (12)
-
-// LORA SPI2
-
-#define ST7789_CS SPI_CS
-#define ST7789_BL 41
-#define SPI_FREQUENCY 40000000
-#define SPI_READ_FREQUENCY 40000000
-#define ST7789_SCK SPI_SCK   // Set SPI SCLK pin number
-#define ST7789_SDA SPI_MOSI  // Set SPI MOSI pin number
-#define ST7789_MISO SPI_MISO // Set SPI MISO pin number (-1 = disable)
-#define ST7789_RS 42
-#define ST7789_CS SPI_CS
-#define TFT_WIDTH 240
-#define TFT_HEIGHT 320
-#define TFT_OFFSET_X 0
-#define TFT_OFFSET_Y 0
-#define TFT_OFFSET_ROTATION 2
-#define ST7789_SPI_HOST SPI3_HOST
-
-#define SCREEN_ROTATE
-
-#define HAS_TOUCHSCREEN 1
-#define SCREEN_TOUCH_INT 39
-#define TOUCH_SLAVE_ADDRESS 0x38 // RAK14014_FT6336U
-#define TOUCH_I2C_PORT 0
 
 #define HAS_BUTTON 1
 #define BUTTON_PIN 0
@@ -88,10 +59,13 @@
 
 #define PIN_BUZZER 38
 
+#define HAS_SDCARD 1
 #define SDCARD_USE_SPI1 1
-// #define HAS_SDCARD 1
 #define SDCARD_CS  2
 
-#define SD_SPI_FREQUENCY 16000000
+#define SPI_FREQUENCY 40000000
+#define SPI_READ_FREQUENCY 16000000
+
+#define SD_SPI_FREQUENCY 50000000
 
 #endif
