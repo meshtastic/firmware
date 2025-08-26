03dc730f037307eb5d5415d00a77bd6973572931
RicInNewMexico <ric.letson@outlook.com>
2025-08-26 11:47:05 -0600

    Update M5Stack Core / Basic Pin Defs

--- variants/esp32/m5stack_core/variant.h
+++ variants/esp32/m5stack_core/variant.h
@@ -1,14 +1,13 @@
-// #define BUTTON_NEED_PULLUP // if set we need to turn on the internal CPU pullup during sleep
+// Note: The M5Stack Core is now referred to as the M5Stack "Basic"
 
 #define I2C_SDA 21
 #define I2C_SCL 22
 
-// #define BUTTON_PIN 39 // 38, 37
-// #define BUTTON_PIN 0
-#define BUTTON_NEED_PULLUP
 // #define EXT_NOTIFY_OUT 13 // Default pin to use for Ext Notify Plugin.
 
 #define BUTTON_PIN 38
+// #define BUTTON_NEED_PULLUP // if set we need to turn on the internal CPU pullup during sleep
+#define BUTTON_NEED_PULLUP
 
 #define PIN_BUZZER 25
 
@@ -23,8 +22,8 @@
 #define LORA_CS 5
 
 #define USE_RF95
-#define LORA_DIO0 36 // a No connect on the SX1262 module
-#define LORA_RESET 26
+#define LORA_DIO0 35 // IRQ
+#define LORA_RESET 25
 #define LORA_DIO1 RADIOLIB_NC // Not really used
 #define LORA_DIO2 RADIOLIB_NC // Not really used
 
