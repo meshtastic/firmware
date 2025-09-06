43bf978935a356294de1d4024881b93377c2322e
Lluís Batlle i Rossell <viric@viric.name>
2025-09-06 20:51:02 +0200

    Make RAK4631 nodes power back on deep sleep
    The devices will hang if the VBAT goes under 1.7V (Brown-out reset) and
    they will never come back unless power supply goes completely off.
    
    This kills unattended nodes.
    
    Using the SystemOff the LPCOMP we can get the nodes back again when
    power comes back, even if VBAT goes under 1.7V, which moreover is more
    unlikely because the device is off.

--- src/Power.cpp
+++ src/Power.cpp
@@ -903,13 +903,8 @@ void Power::readPowerStatus()
             low_voltage_counter++;
             LOG_DEBUG("Low voltage counter: %d/10", low_voltage_counter);
             if (low_voltage_counter > 10) {
-#ifdef ARCH_NRF52
-                // We can't trigger deep sleep on NRF52, it's freezing the board
-                LOG_DEBUG("Low voltage detected, but not trigger deep sleep");
-#else
                 LOG_INFO("Low voltage detected, trigger deep sleep");
                 powerFSM.trigger(EVENT_LOW_BATTERY);
-#endif
             }
         } else {
             low_voltage_counter = 0;
