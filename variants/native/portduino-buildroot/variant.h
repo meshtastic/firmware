62a8a4cf75a1c968265afb2278b1680da05ffe6c
Ben Meadors <benmmeadors@gmail.com>
2025-09-09 08:21:18 -0500

    Update variant.h

--- variants/native/portduino-buildroot/variant.h
+++ variants/native/portduino-buildroot/variant.h
@@ -2,4 +2,4 @@
 #define CANNED_MESSAGE_MODULE_ENABLE 1
 #define HAS_GPS 1
 #define MAX_RX_TOPHONE settingsMap[maxtophone]
-#define MAX_NUM_NODES settingsMap[maxnodes] // Runtime configurable values (for bounds checking and actual usage)
+#define MAX_NUM_NODES settingsMap[maxnodes]
