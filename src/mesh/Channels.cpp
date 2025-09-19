e2ce36978274b7d5661ee77ff34934de2b3932cc
Ben Meadors <benmmeadors@gmail.com>
2025-09-19 06:29:18 -0500

    Fixes

--- src/mesh/Channels.cpp
+++ src/mesh/Channels.cpp
@@ -428,7 +428,8 @@ bool Channels::setDefaultPresetCryptoForHash(ChannelHash channelHash)
     // Iterate all known presets
     for (int preset = _meshtastic_Config_LoRaConfig_ModemPreset_MIN; preset <= _meshtastic_Config_LoRaConfig_ModemPreset_MAX;
          ++preset) {
-        const char *name = DisplayFormatters::getModemPresetDisplayName((meshtastic_Config_LoRaConfig_ModemPreset)preset, false);
+        const char *name =
+            DisplayFormatters::getModemPresetDisplayName((meshtastic_Config_LoRaConfig_ModemPreset)preset, false, false);
         if (!name)
             continue;
         if (strcmp(name, "Invalid") == 0)
