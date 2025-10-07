668cc9fd64513fc41b94627bd6fc7c7ec3b69e63
Jonathan Bennett <jbennett@incomsystems.biz>
2025-10-07 05:58:39 -0500

    Do slightly better at threading the search for GPS hardware (#8240)
    * Do slightly better at threading the search for GPS hardware
    
    * Formatting and minor logic fix
    
    * Remove now-spam GPS Log messages
    
    ---------
    
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>

--- src/gps/GPS.h
+++ src/gps/GPS.h
@@ -151,6 +151,8 @@ class GPS : private concurrency::OSThread
     TinyGPSPlus reader;
     uint8_t fixQual = 0; // fix quality from GPGGA
     uint32_t lastChecksumFailCount = 0;
+    uint8_t currentStep = 0;
+    int32_t currentDelay = 2000;
 
 #ifndef TINYGPS_OPTION_NO_CUSTOM_FIELDS
     // (20210908) TinyGps++ can only read the GPGSA "FIX TYPE" field
