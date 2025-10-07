74e67d87187851f707f118c8b46f973b222ca388
Austin Lane <vidplace7@gmail.com>
2025-10-07 12:01:13 -0400

    Meshtastic build manifest

--- bin/build-esp32.sh
+++ bin/build-esp32.sh
@@ -5,7 +5,8 @@ set -e
 VERSION=`bin/buildinfo.py long`
 SHORT_VERSION=`bin/buildinfo.py short`
 
-OUTDIR=release/
+BUILDDIR=.pio/build/$1
+OUTDIR=release
 
 rm -f $OUTDIR/firmware*
 rm -r $OUTDIR/* || true
@@ -14,7 +15,7 @@ rm -r $OUTDIR/* || true
 platformio pkg install -e $1
 
 echo "Building for $1 with $PLATFORMIO_BUILD_FLAGS"
-rm -f .pio/build/$1/firmware.*
+rm -f $BUILDDIR/firmware*
 
 # The shell vars the build tool expects to find
 export APP_VERSION=$VERSION
@@ -22,16 +23,14 @@ export APP_VERSION=$VERSION
 basename=firmware-$1-$VERSION
 
 pio run --environment $1 # -v
-SRCELF=.pio/build/$1/firmware.elf
-cp $SRCELF $OUTDIR/$basename.elf
+
+cp $BUILDDIR/$basename.elf $OUTDIR/$basename.elf
 
 echo "Copying ESP32 bin file"
-SRCBIN=.pio/build/$1/firmware.factory.bin
-cp $SRCBIN $OUTDIR/$basename.bin
+cp $BUILDDIR/$basename.factory.bin $OUTDIR/$basename.factory.bin
 
 echo "Copying ESP32 update bin file"
-SRCBIN=.pio/build/$1/firmware.bin
-cp $SRCBIN $OUTDIR/$basename-update.bin
+cp $BUILDDIR/$basename.bin $OUTDIR/$basename.bin
 
 echo "Building Filesystem for ESP32 targets"
 # If you want to build the webui, uncomment the following lines
@@ -40,7 +39,13 @@ echo "Building Filesystem for ESP32 targets"
 # # Remove webserver files from the filesystem and rebuild
 # ls -l data/static # Diagnostic list of files
 # rm -rf data/static
-pio run --environment $1 -t buildfs
-cp .pio/build/$1/littlefs.bin $OUTDIR/littlefs-$1-$VERSION.bin
-cp bin/device-install.* $OUTDIR
-cp bin/device-update.* $OUTDIR
+pio run --environment $1 -t buildfs --disable-auto-clean
+cp $BUILDDIR/littlefs-$1-$VERSION.bin $OUTDIR/littlefs-$1-$VERSION.bin
+cp bin/device-install.* $OUTDIR/
+cp bin/device-update.* $OUTDIR/
+
+# Generate the manifest file
+echo "Generating Meshtastic manifest"
+TIMEFORMAT="Generated manifest in %E seconds"
+time pio run --environment $1 -t mtjson --silent --disable-auto-clean
+cp $BUILDDIR/$basename.mt.json $OUTDIR/$basename.mt.json
