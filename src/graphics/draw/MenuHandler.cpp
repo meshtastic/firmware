7537d28419d2bd2dc70519889b509cc1994789ff
l0g-lab <72365840+l0g-lab@users.noreply.github.com>
2025-10-12 09:25:15 -0400

    Nodelist: choice of long or short name (#7926)
    * update to use long names for pager
    
    * remove duplicate
    
    * add menu item
    
    * fix after conflict
    
    * menu name change. use sanitizeString
    
    * fix formatting issue. should pass trunk now.
    
    * remove auto-generated protobufs
    
    * remove log, add tdeck, improvements.
    
    ---------
    
    Co-authored-by: l0g-lab <l0g-lab@users.noreply.github.com>
    Co-authored-by: Tom Fifield <tom@tomfifield.net>
    Co-authored-by: Ben Meadors <benmmeadors@gmail.com>

--- src/graphics/draw/MenuHandler.cpp
+++ src/graphics/draw/MenuHandler.cpp
@@ -762,6 +762,31 @@ void menuHandler::nodeListMenu()
     screen->showOverlayBanner(bannerOptions);
 }
 
+void menuHandler::nodeNameLengthMenu()
+{
+    enum OptionsNumbers { Back, Long, Short };
+    static const char *optionsArray[] = {"Back", "Long", "Short"};
+    BannerOverlayOptions bannerOptions;
+    bannerOptions.message = "Node Name Length";
+    bannerOptions.optionsArrayPtr = optionsArray;
+    bannerOptions.optionsCount = 3;
+    bannerOptions.bannerCallback = [](int selected) -> void {
+        if (selected == Long) {
+            // Set names to long
+            LOG_INFO("Setting names to long");
+            config.display.use_long_node_name = true;
+        } else if (selected == Short) {
+            // Set names to short
+            LOG_INFO("Setting names to short");
+            config.display.use_long_node_name = false;
+        } else if (selected == Back) {
+            menuQueue = screen_options_menu;
+            screen->runNow();
+        }
+    };
+    screen->showOverlayBanner(bannerOptions);
+}
+
 void menuHandler::resetNodeDBMenu()
 {
     static const char *optionsArray[] = {"Back", "Confirm"};
@@ -1304,10 +1329,15 @@ void menuHandler::screenOptionsMenu()
     hasSupportBrightness = false;
 #endif
 
-    enum optionsNumbers { Back, Brightness, ScreenColor };
-    static const char *optionsArray[4] = {"Back"};
-    static int optionsEnumArray[4] = {Back};
+    enum optionsNumbers { Back, NodeNameLength, Brightness, ScreenColor };
+    static const char *optionsArray[5] = {"Back"};
+    static int optionsEnumArray[5] = {Back};
     int options = 1;
+
+#if defined(T_DECK) || defined(T_LORA_PAGER)
+    optionsArray[options] = "Show Long/Short Name";
+    optionsEnumArray[options++] = NodeNameLength;
+#endif
 
     // Only show brightness for B&W displays
     if (hasSupportBrightness) {
@@ -1332,6 +1362,9 @@ void menuHandler::screenOptionsMenu()
             screen->runNow();
         } else if (selected == ScreenColor) {
             menuHandler::menuQueue = menuHandler::tftcolormenupicker;
+            screen->runNow();
+        } else if (selected == NodeNameLength) {
+            menuHandler::menuQueue = menuHandler::node_name_length_menu;
             screen->runNow();
         } else {
             menuQueue = system_base_menu;
@@ -1609,6 +1642,9 @@ void menuHandler::handleMenuSwitch(OLEDDisplay *display)
         break;
     case brightness_picker:
         BrightnessPickerMenu();
+        break;
+    case node_name_length_menu:
+        nodeNameLengthMenu();
         break;
     case reboot_menu:
         rebootMenu();
