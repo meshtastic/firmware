b04fb5cb23d0e39fe2b4186871c643e08a71dce2
Manuel <71137295+mverch67@users.noreply.github.com>
2025-10-28 17:49:21 +0100

    create screen

--- src/main.cpp
+++ src/main.cpp
@@ -881,7 +881,7 @@ void setup()
 
 #if defined(ST7701_CS) || defined(ST7735_CS) || defined(USE_EINK) || defined(ILI9341_DRIVER) || defined(ILI9342_DRIVER) ||       \
     defined(ST7789_CS) || defined(HX8357_CS) || defined(USE_ST7789) || defined(ILI9488_CS) || defined(ST7796_CS) ||              \
-    defined(USE_SPISSD1306)
+    defined(USE_SPISSD1306) || defined(USE_EPD)
         screen = new graphics::Screen(screen_found, screen_model, screen_geometry);
 #elif defined(ARCH_PORTDUINO)
         if ((screen_found.port != ScanI2C::I2CPort::NO_I2C || portduino_config.displayPanel) &&
