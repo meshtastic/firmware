e324ca4cb458250a941d4cb2181346af79448d07
Ben Allfree <ben@benallfree.com>
2025-11-02 21:15:50 -0800

    wip

--- src/modules/EspruinoModule/EspruinoModule.h
+++ src/modules/EspruinoModule/EspruinoModule.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "mesh/MeshModule.h"
 #include "concurrency/OSThread.h"
 #include "configuration.h"
+#include <map>
+#include "espruino_embedded.h"
 
 #ifdef MESHTASTIC_INCLUDE_ESPRUINO
 
@@ -14,16 +17,34 @@ struct ejs;
  * 
  * This is an experimental module controlled by MESHTASTIC_INCLUDE_ESPRUINO flag.
  * 
- * Currently provides:
- * - console.log() (already built into Espruino)
- * - Full JavaScript ES5 interpreter
+ * Provides JavaScript API:
+ * - console.log() (built into Espruino)
+ * - Meshtastic.onTextMessage(callback)
+ * - Meshtastic.sendTextMessage(to, message)
+ * - Meshtastic.onMessage(portNum, callback)
+ * - Port number constants (TEXT_MESSAGE_APP, etc.)
  */
-class EspruinoModule : private concurrency::OSThread
+class EspruinoModule : public MeshModule
 {
   private:
-    bool firstTime = true;
     bool initialized = false;
     struct ejs *jsInstance = nullptr;
+    
+    // Initialization thread helper
+    class InitThread : public concurrency::OSThread {
+        EspruinoModule *parent;
+      public:
+        InitThread(EspruinoModule *_parent) : OSThread("EspruinoInit"), parent(_parent) {}
+      protected:
+        int32_t runOnce() override {
+            parent->initializeEspruino();
+            return disable(); // Run only once
+        }
+    };
+    InitThread *initThread = nullptr;
+    
+    // Callback storage: map from port number to JavaScript callback function
+    std::map<int, JsVar*> messageCallbacks;
 
   public:
     EspruinoModule();
@@ -35,13 +56,28 @@ class EspruinoModule : private concurrency::OSThread
      * @return true if execution was successful
      */
     bool executeJS(const char *code);
+    
+    /**
+     * Register a JavaScript callback for a specific port number
+     */
+    void registerCallback(int portNum, JsVar *callback);
+    
+    /**
+     * Invoke JavaScript callback for an incoming message
+     */
+    void invokeCallback(int portNum, uint32_t from, const uint8_t *payload, size_t payloadSize);
 
   protected:
-    virtual int32_t runOnce() override;
+    /** Called to handle a particular incoming message */
+    virtual ProcessMessage handleReceived(const meshtastic_MeshPacket &mp) override;
+    
+    /** @return true if we want to receive this packet */
+    virtual bool wantPacket(const meshtastic_MeshPacket *p) override;
     
   private:
     void initializeEspruino();
     void cleanupEspruino();
+    void setupJavaScriptAPI();
 };
 
 extern EspruinoModule *espruinoModule;
