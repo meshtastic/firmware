ea0f3b45380b4d9d2e9fb08292599a58c18de66d
Jennifer Morones <jennifermorones@mac.dhcp.nd.edu>
2025-11-20 18:17:04 -0500

    updated the .cpp and .h files to read data and passes the test when build

--- src/modules/Telemetry/Sensor/SoilMoistureSensor.cpp
+++ src/modules/Telemetry/Sensor/SoilMoistureSensor.cpp
@@ -1,90 +1,3 @@
-/* #include "configuration.h"
-
-#if !MESHTASTIC_EXCLUDE_ENVIRONMENTAL_SENSOR && __has_include(<I2CSoilMoistureSensor.h>)
-
-#include "../mesh/generated/meshtastic/telemetry.pb.h"
-#include "SoilMoistureSensor.h"
-#include "TelemetrySensor.h"
-#include <I2CSoilMoistureSensor.h>
-
-SoilMoistureSensor::SoilMoistureSensor()
-    : TelemetrySensor(meshtastic_TelemetrySensorType_SENSOR_UNSET, "SoilMoisture") {}
-
-int32_t SoilMoistureSensor::runOnce()
-{
-    LOG_INFO("Init sensor: %s\n", sensorName);
-
-    if (!hasSensor()) {
-        return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
-    }
-
-    // Get I2C bus and address from the sensor map
-    TwoWire *wirePort = nodeTelemetrySensorsMap[sensorType].second;
-    uint8_t i2cAddr = nodeTelemetrySensorsMap[sensorType].first;
-
-    LOG_DEBUG("Initializing soil moisture sensor at address 0x%02X\n", i2cAddr);
-
-    // Initialize the sensor
-    soilSensor.begin(true);
-
-    // Verify sensor is working by reading version
-    uint8_t version = soilSensor.getVersion();
-    if (version > 0) {
-        LOG_INFO("Soil Moisture Sensor found, version: 0x%02X\n", version);
-        status = 1;
-    } else {
-        LOG_WARN("Soil Moisture Sensor not responding\n");
-        status = 0;
-    }
-
-    return initI2CSensor();
-}
-
-void SoilMoistureSensor::setup()
-{
-    LOG_DEBUG("Soil moisture sensor setup complete\n");
-    // No special setup needed for this sensor
-}
-
-bool SoilMoistureSensor::getMetrics(meshtastic_Telemetry *measurement)
-{
-    if (!isInitialized() || !isRunning()) {
-        return false;
-    }
-
-    // Check if sensor is busy
-    if (soilSensor.isBusy()) {
-        LOG_DEBUG("Soil sensor busy, skipping read\n");
-        return false;
-    }
-
-    measurement->which_variant = meshtastic_Telemetry_environment_metrics_tag;
-    auto &env = measurement->variant.environment_metrics;
-
-    // Read soil moisture capacitance
-    unsigned int capacitance = soilSensor.getCapacitance();
-    
-    // Read temperature (sensor returns temperature * 10)
-    int rawTemp = soilSensor.getTemperature();
-    float temperature = rawTemp / 10.0f;
-
-    LOG_DEBUG("Soil - Capacitance: %d, Temperature: %.1f°C\n", capacitance, temperature);
-
-    // Set the metrics
-    env.has_temperature = true;
-    env.temperature = temperature;
-    
-    // Store soil moisture data
-    // Using relative_humidity field temporarily for moisture percentage
-    float moisturePercent = map(constrain(capacitance, 250, 1800), 250, 1800, 0, 100);
-    env.has_relative_humidity = true;
-    env.relative_humidity = moisturePercent;
-
-    return true;
-}
-
-#endif */
-
 #include "configuration.h"
 
 #if !MESHTASTIC_EXCLUDE_ENVIRONMENTAL_SENSOR && __has_include(<Adafruit_seesaw.h>)
@@ -93,5 +6,43 @@ bool SoilMoistureSensor::getMetrics(meshtastic_Telemetry *measurement)
 #include "SoilMoistureSensor.h"
 #include "TelemetrySensor.h"
 #include "main.h"
+
+SoilMoistureSensor::SoilMoistureSensor()
+    : TelemetrySensor(meshtastic_TelemetrySensorType_SHTC3, "SoilMoisture") // temporary type
+{
+}
+
+int32_t SoilMoistureSensor::runOnce()
+{
+    LOG_INFO("Init SoilMoistureSensor: %s", sensorName);
+
+    if (!hasSensor()) {
+        return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
+    }
+
+    if (!ss.begin(0x36)) { // default I2C address for STEMMA Soil Sensor
+        LOG_ERROR("SoilMoistureSensor not detected at I2C 0x36");
+        return DEFAULT_SENSOR_MINIMUM_WAIT_TIME_BETWEEN_READS;
+    }
+
+    // Initialize capacitive touch module for moisture reading
+    ss.pinMode(0, INPUT);
+    return initI2CSensor();
+}
+
+void SoilMoistureSensor::setup()
+{
+    LOG_INFO("SoilMoistureSensor setup complete");
+}
+
+bool SoilMoistureSensor::getMetrics(meshtastic_Telemetry *measurement)
+{
+    measurement->variant.environment_metrics.has_soil_moisture = true;
+
+    uint16_t moisture = ss.touchRead(0); // read capacitive moisture value from channel 0
+    measurement->variant.environment_metrics.soil_moisture = moisture;
+
+    return true;
+}
 
 #endif
--- src/modules/Telemetry/Sensor/SoilMoistureSensor.h
+++ src/modules/Telemetry/Sensor/SoilMoistureSensor.h
@@ -1,29 +1,3 @@
-/* #include "configuration.h"
-
-#if !MESHTASTIC_EXCLUDE_ENVIRONMENTAL_SENSOR && __has_include(<I2CSoilMoistureSensor.h>)
-
-#pragma once
-
-#include "../mesh/generated/meshtastic/telemetry.pb.h"
-#include "TelemetrySensor.h"
-#include <I2CSoilMoistureSensor.h>
-
-class SoilMoistureSensor : public TelemetrySensor
-{
-  private:
-    I2CSoilMoistureSensor soilSensor;
-
-  protected:
-    virtual void setup() override;
-
-  public:
-    SoilMoistureSensor();
-    virtual int32_t runOnce() override;
-    virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
-};
-
-#endif */
-
 #pragma once
 #include "configuration.h"
 
@@ -32,5 +6,19 @@ class SoilMoistureSensor : public TelemetrySensor
 #include "../mesh/generated/meshtastic/telemetry.pb.h"
 #include "TelemetrySensor.h"
 #include <Adafruit_seesaw.h>
+
+class SoilMoistureSensor : public TelemetrySensor
+{
+private:
+    Adafruit_seesaw ss;  // Adafruit seesaw object for the sensor
+
+protected:
+    virtual void setup() override;
+
+public:
+    SoilMoistureSensor();
+    virtual int32_t runOnce() override;
+    virtual bool getMetrics(meshtastic_Telemetry *measurement) override;
+};
 
 #endif
