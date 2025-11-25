65bcbd5bd301e5e8a22406e1348572d83c5ce971
Thomas Göttgens <tgoettgens@gmail.com>
2025-11-25 23:30:00 +0100

    fix 3401 detection

--- src/platform/nrf52/architecture.h
+++ src/platform/nrf52/architecture.h
@@ -57,11 +57,11 @@
 #define HW_VENDOR meshtastic_HardwareModel_NOMADSTAR_METEOR_PRO
 #elif defined(R1_NEO)
 #define HW_VENDOR meshtastic_HardwareModel_MUZI_R1_NEO
+#elif defined(RAK3401)
+#define HW_VENDOR meshtastic_HardwareModel_RAK3401
 // MAke sure all custom RAK4630 boards are defined before the generic RAK4630
 #elif defined(RAK4630)
 #define HW_VENDOR meshtastic_HardwareModel_RAK4631
-#elif defined(RAK3401)
-#define HW_VENDOR meshtastic_HardwareModel_RAK3401
 #elif defined(TTGO_T_ECHO)
 #define HW_VENDOR meshtastic_HardwareModel_T_ECHO
 #elif defined(T_ECHO_LITE)
