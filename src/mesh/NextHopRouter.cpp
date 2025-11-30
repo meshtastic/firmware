41d0ec0225eb44442088042ed2a72f6496545875
Sebastian <sszulc@gmail.com>
2025-12-01 00:03:24 +0100

    Add signal-based routing support with new SignalRoutingModule

--- src/mesh/NextHopRouter.cpp
+++ src/mesh/NextHopRouter.cpp
@@ -5,6 +5,7 @@
 #include "modules/TraceRouteModule.h"
 #endif
 #include "NodeDB.h"
+#include "SignalRoutingModule.h"
 
 NextHopRouter::NextHopRouter() {}
 
@@ -133,11 +134,15 @@ bool NextHopRouter::perhapsRebroadcast(const meshtastic_MeshPacket *p)
                     meshtastic_MeshPacket *tosend = packetPool.allocCopy(*p); // keep a copy because we will be sending it
                     LOG_INFO("Rebroadcast received message coming from %x", p->relay_node);
 
-                    // Use shared logic to determine if hop_limit should be decremented
-                    if (shouldDecrementHopLimit(p)) {
-                        tosend->hop_limit--; // bump down the hop count
+                    // Signal-based routing: do NOT decrement hop_limit
+                    if (signalRoutingModule && signalRoutingModule->shouldUseSignalBasedRouting(p)) {
+                        // Leave hop_limit untouched for signal-based routing
+                        LOG_DEBUG("Signal-based routing: preserving hop_limit for packet 0x%08x", p->id);
                     } else {
-                        LOG_INFO("favorite-ROUTER/CLIENT_BASE-to-ROUTER/CLIENT_BASE rebroadcast: preserving hop_limit");
+                        // Classic flood path: decrement hop_limit
+                        if (p->hop_limit > 0) {
+                            tosend->hop_limit--; // bump down the hop count
+                        }
                     }
 #if USERPREFS_EVENT_MODE
                     if (tosend->hop_limit > 2) {
---
+++ src/mesh/SignalRoutingModule.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "MeshModule.h"
+#include <vector>
+
+class Graph;
+
+class SignalRoutingModule : public MeshModule
+{
+public:
+    SignalRoutingModule();
+
+    /**
+     * Check if signal-based routing should be used for a packet
+     */
+    bool shouldUseSignalBasedRouting(const meshtastic_MeshPacket *p);
+
+    /**
+     * Get the next hop for signal-based routing
+     */
+    NodeNum getNextHop(NodeNum destination);
+
+    /**
+     * Update neighbor information
+     */
+    void updateNeighborInfo(NodeNum nodeId, int32_t rssi, int32_t snr, uint32_t lastRxTime);
+
+    /**
+     * Handle speculative retransmit for unicast packets
+     */
+    void handleSpeculativeRetransmit(const meshtastic_MeshPacket *p);
+
+protected:
+    /** Called to handle a particular incoming message */
+    virtual ProcessMessage handleReceived(const meshtastic_MeshPacket &mp) override;
+
+    /** Called every time our node info changes - update our neighbors */
+    virtual void onNodeInfoChanged() override;
+
+private:
+    Graph *routingGraph;
+    std::vector<meshtastic_NeighborLink> neighbors;
+    uint32_t lastGraphUpdate = 0;
+    static constexpr uint32_t GRAPH_UPDATE_INTERVAL_MS = 300 * 1000; // 300 seconds
+
+    /**
+     * Check if a node is signal-based routing capable
+     */
+    bool isSignalBasedCapable(NodeNum nodeId);
+
+    /**
+     * Calculate percentage of signal-based capable nodes
+     */
+    float getSignalBasedCapablePercentage();
+
+    /**
+     * Update our own signal_based_capable flag in NodeInfo
+     */
+    void updateSignalBasedCapable();
+};
+
+extern SignalRoutingModule *signalRoutingModule;
