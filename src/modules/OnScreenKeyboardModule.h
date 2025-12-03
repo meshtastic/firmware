ceb6d0b9729779a2c5936f06b8547bc82887d18a
whywilson <m.tools@qq.com>
2025-12-04 07:13:20 +0800

    Trunk fix

--- src/modules/OnScreenKeyboardModule.h
+++ src/modules/OnScreenKeyboardModule.h
@@ -16,7 +16,8 @@ class OnScreenKeyboardModule
   public:
     static OnScreenKeyboardModule &instance();
 
-    void start(const char *header, const char *initialText, uint32_t durationMs, std::function<void(const std::string &)> callback);
+    void start(const char *header, const char *initialText, uint32_t durationMs,
+               std::function<void(const std::string &)> callback);
 
     void stop(bool callEmptyCallback);
 
