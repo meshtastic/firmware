b28fc2df99d625f2c97f5f30304b51a7c771b9b1
Kristopher Clark <krisclarkdev@krisbox.org>
2025-12-24 12:10:51 -0600

    Implement proper POSIX semaphore and sleep default parameter
    - BinarySemaphorePosix: Replace delay() stub with pthread_cond_timedwait
    - doLightSleep: Add 30 second default parameter, matching THIRTY_SECONDS_MS
    - main_matrix.yml: Fetch branches before merge-base to fix shame job failure

--- src/concurrency/BinarySemaphorePosix.cpp
+++ src/concurrency/BinarySemaphorePosix.cpp
@@ -3,25 +3,67 @@
 
 #ifndef HAS_FREE_RTOS
 
-namespace concurrency
-{
+#include <errno.h>
+#include <sys/time.h>
 
-BinarySemaphorePosix::BinarySemaphorePosix() {}
+namespace concurrency {
 
-BinarySemaphorePosix::~BinarySemaphorePosix() {}
-
-/**
- * Returns false if we timed out
- */
-bool BinarySemaphorePosix::take(uint32_t msec)
-{
-    delay(msec); // FIXME
-    return false;
+BinarySemaphorePosix::BinarySemaphorePosix() : signaled(false) {
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cond, NULL);
 }
 
-void BinarySemaphorePosix::give() {}
+BinarySemaphorePosix::~BinarySemaphorePosix() {
+  pthread_cond_destroy(&cond);
+  pthread_mutex_destroy(&mutex);
+}
 
-IRAM_ATTR void BinarySemaphorePosix::giveFromISR(BaseType_t *pxHigherPriorityTaskWoken) {}
+/**
+ * Block until signaled or timeout expires.
+ * Returns true if we were signaled, false if we timed out.
+ */
+bool BinarySemaphorePosix::take(uint32_t msec) {
+  struct timespec ts;
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+  ts.tv_sec = tv.tv_sec + (msec / 1000);
+  ts.tv_nsec = (tv.tv_usec * 1000) + ((msec % 1000) * 1000000);
+  if (ts.tv_nsec >= 1000000000) {
+    ts.tv_sec++;
+    ts.tv_nsec -= 1000000000;
+  }
+
+  pthread_mutex_lock(&mutex);
+
+  while (!signaled) {
+    int rc = pthread_cond_timedwait(&cond, &mutex, &ts);
+    if (rc == ETIMEDOUT) {
+      pthread_mutex_unlock(&mutex);
+      return false;
+    }
+  }
+
+  signaled = false;
+  pthread_mutex_unlock(&mutex);
+  return true;
+}
+
+void BinarySemaphorePosix::give() {
+  pthread_mutex_lock(&mutex);
+  signaled = true;
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&mutex);
+}
+
+IRAM_ATTR void
+BinarySemaphorePosix::giveFromISR(BaseType_t *pxHigherPriorityTaskWoken) {
+  // POSIX doesn't distinguish ISR context; delegate to regular give()
+  give();
+  if (pxHigherPriorityTaskWoken) {
+    *pxHigherPriorityTaskWoken = pdTRUE;
+  }
+}
 
 } // namespace concurrency
 
