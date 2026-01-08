62bc1879ff8311d9cd697985751ee2427c061e69
Jonathan Bennett <jbennett@incomsystems.biz>
2026-01-08 12:07:14 -0600

    Trunk

--- variants/esp32s3/t-beam-1w/pins_arduino.h
+++ variants/esp32s3/t-beam-1w/pins_arduino.h
@@ -14,7 +14,7 @@ static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
 
 // Default SPI mapped to Radio/SD
-static const uint8_t SS = 15;   // LoRa CS
+static const uint8_t SS = 15; // LoRa CS
 static const uint8_t MOSI = 11;
 static const uint8_t MISO = 12;
 static const uint8_t SCK = 13;
