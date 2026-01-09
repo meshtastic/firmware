8b2d9e66bc5806f8bc2a31ba9d699241f93d55be
Ben Meadors <benmmeadors@gmail.com>
2026-01-09 08:35:37 -0600

    Increase default position broadcast intervals and enforce minimums for default channels

--- src/mesh/Default.h
+++ src/mesh/Default.h
@@ -13,7 +13,10 @@
 #define min_default_telemetry_interval_secs 30 * 60
 #define default_gps_update_interval IF_ROUTER(ONE_DAY, 2 * 60)
 #define default_telemetry_broadcast_interval_secs IF_ROUTER(ONE_DAY / 2, 60 * 60)
-#define default_broadcast_interval_secs IF_ROUTER(ONE_DAY / 2, 15 * 60)
+#define default_broadcast_interval_secs IF_ROUTER(ONE_DAY / 2, 60 * 60)
+#define default_broadcast_smart_minimum_interval_secs 5 * 60
+#define min_default_broadcast_interval_secs 60 * 60
+#define min_default_broadcast_smart_minimum_interval_secs 5 * 60
 #define default_wait_bluetooth_secs IF_ROUTER(1, 60)
 #define default_sds_secs IF_ROUTER(ONE_DAY, UINT32_MAX) // Default to forever super deep sleep
 #define default_ls_secs IF_ROUTER(ONE_DAY, 5 * 60)
