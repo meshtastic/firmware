23f82c92a7df72ec60aba889d5f77a62a1a5953e
oscgonfer <oscgonfer@gmail.com>
2026-01-10 11:26:50 +0100

    Move add sensor template to separate file

--- src/modules/Telemetry/AirQualityTelemetry.cpp
+++ src/modules/Telemetry/AirQualityTelemetry.cpp
@@ -17,39 +17,12 @@
 #include "main.h"
 #include "sleep.h"
 #include <Throttle.h>
+#include "Sensor/AddI2CSensorTemplate.h"
 
 // Sensors
 #ifdef VBLE_I2C_CLOCK_SPEED
 #include "Sensor/PMSA003ISensor.h"
 #endif
-
-#include <forward_list>
-
-static std::forward_list<TelemetrySensor *> sensors;
-
-template <typename T> void addSensor(ScanI2C *i2cScanner, ScanI2C::DeviceType type)
-{
-    ScanI2C::FoundDevice dev = i2cScanner->find(type);
-    if (dev.type != ScanI2C::DeviceType::NONE || type == ScanI2C::DeviceType::NONE) {
-        TelemetrySensor *sensor = new T();
-#if WIRE_INTERFACES_COUNT > 1
-        TwoWire *bus = ScanI2CTwoWire::fetchI2CBus(dev.address);
-        if (dev.address.port != ScanI2C::I2CPort::WIRE1 && sensor->onlyWire1()) {
-            // This sensor only works on Wire (Wire1 is not supported)
-            delete sensor;
-            return;
-        }
-#else
-        TwoWire *bus = &Wire;
-#endif
-        if (sensor->initDevice(bus, &dev)) {
-            sensors.push_front(sensor);
-            return;
-        }
-        // destroy sensor
-        delete sensor;
-    }
-}
 
 void AirQualityTelemetryModule::i2cScanFinished(ScanI2C *i2cScanner)
 {
