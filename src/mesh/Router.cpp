9e8399626a6b7cdabd9edfc4a4fd4500f53f41d8
Dane Evans <Dane@goneepic.com>
2026-01-12 16:52:55 +1100

    Revert "Drop clanker induced failures."
    This reverts commit 718edfa748eee5889d18fb56fdd533ee105fbd2a.

--- src/mesh/Router.cpp
+++ src/mesh/Router.cpp
@@ -12,7 +12,10 @@
 #include "mesh-pb-constants.h"
 #include "meshUtils.h"
 #include "modules/RoutingModule.h"
+#if (defined(ARCH_ESP32) || defined(ARCH_NRF52) || defined(ARCH_RP2040) || defined(ARCH_STM32WL)) &&                             \
+    !defined(CONFIG_IDF_TARGET_ESP32S2) && !defined(CONFIG_IDF_TARGET_ESP32C3)
 #include "modules/SerialModule.h"
+#endif
 #if !MESHTASTIC_EXCLUDE_MQTT
 #include "mqtt/MQTT.h"
 #endif
