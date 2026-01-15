e3ef82803b537e713a502e63b042ab94934c1203
Randolph Sapp <rs@ti.com>
2026-01-14 18:52:57 -0600

    fix(PortduinoGlue): spi_dev_int packing
    Operator precedence dictates that the addition will occur before the
    left shift unless we persuade the compiler to do otherwise.
    
    Fixes: 45c1b46bd ("Move native to spi_host to indicate spidev for LovyanGFX")
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- src/platform/portduino/PortduinoGlue.cpp
+++ src/platform/portduino/PortduinoGlue.cpp
@@ -678,7 +678,7 @@ bool loadConfig(const char *configPath)
                     // Pretty sure this is always true
                     if (x >= 0 && x < 10 && y >= 0 && y < 10) {
                         // I believe this bit of weirdness is specifically for the new GUI
-                        portduino_config.lora_spi_dev_int = x + y << 4;
+                        portduino_config.lora_spi_dev_int = x + (y << 4);
                         portduino_config.display_spi_dev_int = portduino_config.lora_spi_dev_int;
                         portduino_config.touchscreen_spi_dev_int = portduino_config.lora_spi_dev_int;
                     }
@@ -777,7 +777,7 @@ bool loadConfig(const char *configPath)
                     int x = portduino_config.display_spi_dev.at(11) - '0';
                     int y = portduino_config.display_spi_dev.at(13) - '0';
                     if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-                        portduino_config.display_spi_dev_int = x + y << 4;
+                        portduino_config.display_spi_dev_int = x + (y << 4);
                         portduino_config.touchscreen_spi_dev_int = portduino_config.display_spi_dev_int;
                     }
                 }
@@ -805,7 +805,7 @@ bool loadConfig(const char *configPath)
                     int x = portduino_config.touchscreen_spi_dev.at(11) - '0';
                     int y = portduino_config.touchscreen_spi_dev.at(13) - '0';
                     if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-                        portduino_config.touchscreen_spi_dev_int = x + y << 4;
+                        portduino_config.touchscreen_spi_dev_int = x + (y << 4);
                     }
                 }
             }
