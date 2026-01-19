733eb3a961da8dde589c1cf372fdb2e79e0ea003
Ben Meadors <benmmeadors@gmail.com>
2026-01-19 17:11:54 -0600

    Implement graduated scaling for NodeInfo send timeout based on active mesh size

--- src/modules/NodeInfoModule.cpp
+++ src/modules/NodeInfoModule.cpp
@@ -2,6 +2,7 @@
 #include "Default.h"
 #include "MeshService.h"
 #include "NodeDB.h"
+#include "NodeStatus.h"
 #include "RTC.h"
 #include "Router.h"
 #include "configuration.h"
@@ -129,13 +130,11 @@ meshtastic_MeshPacket *NodeInfoModule::allocReply()
         LOG_DEBUG("Skip send NodeInfo > 40%% ch. util");
         return NULL;
     }
-    // If we sent our NodeInfo less than 5 min. ago, don't send it again as it may be still underway.
-    if (!shorterTimeout && lastSentToMesh && Throttle::isWithinTimespanMs(lastSentToMesh, 5 * 60 * 1000)) {
-        LOG_DEBUG("Skip send NodeInfo since we sent it <5min ago");
-        ignoreRequest = true; // Mark it as ignored for MeshModule
-        return NULL;
-    } else if (shorterTimeout && lastSentToMesh && Throttle::isWithinTimespanMs(lastSentToMesh, 60 * 1000)) {
-        LOG_DEBUG("Skip send NodeInfo since we sent it <60s ago");
+
+    // Use graduated scaling based on active mesh size (10 minute base, scales with congestion coefficient)
+    uint32_t timeoutMs = Default::getConfiguredOrDefaultMsScaled(0, 10 * 60, nodeStatus->getNumOnline()) * 1000;
+    if (lastSentToMesh && Throttle::isWithinTimespanMs(lastSentToMesh, timeoutMs)) {
+        LOG_DEBUG("Skip send NodeInfo since we sent it <%us ago", timeoutMs / 1000);
         ignoreRequest = true; // Mark it as ignored for MeshModule
         return NULL;
     } else {
