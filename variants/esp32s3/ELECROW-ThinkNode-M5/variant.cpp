d8e1553cefb020c66aa3ec49786b7cfcc818d78e
Jonathan Bennett <jbennett@incomsystems.biz>
2026-02-03 00:04:42 -0600

    Don't double-init Wire

--- variants/esp32s3/ELECROW-ThinkNode-M5/variant.cpp
+++ variants/esp32s3/ELECROW-ThinkNode-M5/variant.cpp
@@ -1,11 +1,11 @@
 #include "variant.h"
 #include <PCA9557.h>
 
-PCA9557 io(0x18, &Wire);
+PCA9557 io(0x18, &Wire1);
 
 void earlyInitVariant()
 {
-    Wire.begin(48, 47);
+    Wire1.begin(48, 47);
     io.pinMode(PCA_PIN_EINK_EN, OUTPUT);
     io.pinMode(PCA_PIN_POWER_EN, OUTPUT);
     io.digitalWrite(PCA_PIN_POWER_EN, HIGH);
