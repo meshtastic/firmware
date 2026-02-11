6266ff9b9df7b7b0a8a0de8029021b4dfa333bea
Clive Blackledge <clive@ansible.org>
2026-02-10 20:44:39 -0800

    Adding station-g2 and portduino varients to be able to use this module.

--- variants/native/portduino/variant.h
+++ variants/native/portduino/variant.h
@@ -8,3 +8,13 @@
 
 // RAK12002 RTC Module
 #define RV3028_RTC (uint8_t)0b1010010
+
+// Enable Traffic Management Module for native/portduino
+#ifdef HAS_TRAFFIC_MANAGEMENT
+#undef HAS_TRAFFIC_MANAGEMENT
+#endif
+#define HAS_TRAFFIC_MANAGEMENT 1
+#ifdef TRAFFIC_MANAGEMENT_CACHE_SIZE
+#undef TRAFFIC_MANAGEMENT_CACHE_SIZE
+#endif
+#define TRAFFIC_MANAGEMENT_CACHE_SIZE 2048
