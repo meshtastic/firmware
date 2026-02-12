6d85a6826ad00b85e4950aaac783b04171ff0e80
Thomas Göttgens <tgoettgens@gmail.com>
2026-02-12 20:19:20 +0100

    simplify the observer pattern, since all the called functions are const getters.

--- src/modules/StatusLEDModule.cpp
+++ src/modules/StatusLEDModule.cpp
@@ -23,14 +23,13 @@ int StatusLEDModule::handleStatusUpdate(const meshtastic::Status *arg)
 {
     switch (arg->getStatusType()) {
     case STATUS_TYPE_POWER: {
-        const meshtastic::PowerStatus *pwrStatus = (const meshtastic::PowerStatus *)arg;
-        if (pwrStatus->getHasUSB() || pwrStatus->getIsCharging()) {
+        if (powerStatus->getHasUSB() || powerStatus->getIsCharging()) {
             power_state = charging;
-            if (pwrStatus->getBatteryChargePercent() >= 100) {
+            if (powerStatus->getBatteryChargePercent() >= 100) {
                 power_state = charged;
             }
         } else {
-            if (pwrStatus->getBatteryChargePercent() > 5) {
+            if (powerStatus->getBatteryChargePercent() > 5) {
                 power_state = discharging;
             } else {
                 power_state = critical;
@@ -39,8 +38,7 @@ int StatusLEDModule::handleStatusUpdate(const meshtastic::Status *arg)
         break;
     }
     case STATUS_TYPE_BLUETOOTH: {
-        const meshtastic::BluetoothStatus *btStatus = (const meshtastic::BluetoothStatus *)arg;
-        switch (btStatus->getConnectionState()) {
+        switch (bluetoothStatus->getConnectionState()) {
         case meshtastic::BluetoothStatus::ConnectionState::DISCONNECTED: {
             ble_state = unpaired;
             PAIRING_LED_starttime = millis();
