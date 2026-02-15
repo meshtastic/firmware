abc614740bb74631e8021b1e181e68d8f1ab036b
kuroanji <33121731+kuroanji@users.noreply.github.com>
2026-02-15 11:24:29 +0400

    Update PairingApplet.cpp
    InkHUD e-ink pairing display

--- src/graphics/niche/InkHUD/Applets/System/Pairing/PairingApplet.cpp
+++ src/graphics/niche/InkHUD/Applets/System/Pairing/PairingApplet.cpp
@@ -14,12 +14,24 @@ void InkHUD::PairingApplet::onRender(bool full)
     // Header
     setFont(fontMedium);
     printAt(X(0.5), Y(0.25), "Bluetooth", CENTER, BOTTOM);
-    setFont(fontSmall);
-    printAt(X(0.5), Y(0.25), "Enter this code", CENTER, TOP);
 
-    // Passkey
-    setFont(fontMedium);
-    printThick(X(0.5), Y(0.5), passkey.substr(0, 3) + " " + passkey.substr(3), 3, 2);
+    // Hide PIN for FIXED_PIN mode unless user explicitly enabled "Show PIN"
+    bool hidePin = (config.bluetooth.mode == meshtastic_Config_BluetoothConfig_PairingMode_FIXED_PIN) &&
+                   !config.bluetooth.device_show_fixed_pin;
+
+    if (hidePin) {
+        // Predefined PIN: don't reveal the code on screen
+        setFont(fontSmall);
+        printAt(X(0.5), Y(0.45), "Pairing with", CENTER, BOTTOM);
+        printAt(X(0.5), Y(0.55), "predefined PIN", CENTER, TOP);
+    } else {
+        setFont(fontSmall);
+        printAt(X(0.5), Y(0.25), "Enter this code", CENTER, TOP);
+
+        // Passkey
+        setFont(fontMedium);
+        printThick(X(0.5), Y(0.5), passkey.substr(0, 3) + " " + passkey.substr(3), 3, 2);
+    }
 
     // Device's bluetooth name, if it will fit
     setFont(fontSmall);
