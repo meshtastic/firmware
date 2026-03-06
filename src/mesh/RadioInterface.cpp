24dff373e8a78012fcf663b9e66fb29b4f067851
吴文峰 <kevin@lmve.net>
2026-03-06 18:08:47 +0800

    feat: add SETTING_MAX_POWER parameter to control max transmit power

--- src/mesh/RadioInterface.cpp
+++ src/mesh/RadioInterface.cpp
@@ -43,7 +43,13 @@ const RegionInfo regions[] = {
         https://link.springer.com/content/pdf/bbm%3A978-1-4842-4357-2%2F1.pdf
         https://www.thethingsnetwork.org/docs/lorawan/regional-parameters/
     */
-    RDEF(US, 902.0f, 928.0f, 100, 0, 30, true, false, false),
+    RDEF(US, 902.0f, 928.0f, 100, 0, 
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false),
 
     /*
         EN300220 ETSI V3.2.1 [Table B.1, Item H, p. 21]
@@ -51,7 +57,13 @@ const RegionInfo regions[] = {
         https://www.etsi.org/deliver/etsi_en/300200_300299/30022002/03.02.01_60/en_30022002v030201p.pdf
         FIXME: https://github.com/meshtastic/firmware/issues/3371
      */
-    RDEF(EU_433, 433.0f, 434.0f, 10, 0, 10, true, false, false),
+    RDEF(EU_433, 433.0f, 434.0f, 10, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        10
+        #endif
+        , true, false, false),
 
     /*
        https://www.thethingsnetwork.org/docs/lorawan/duty-cycle/
@@ -67,33 +79,63 @@ const RegionInfo regions[] = {
        AFA) to avoid a duty cycle. (Please refer to line P page 22 of this document.)
        https://www.etsi.org/deliver/etsi_en/300200_300299/30022002/03.01.01_60/en_30022002v030101p.pdf
      */
-    RDEF(EU_868, 869.4f, 869.65f, 10, 0, 27, false, false, false),
+    RDEF(EU_868, 869.4f, 869.65f, 10, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        27
+        #endif
+        , false, false, false),
 
     /*
         https://lora-alliance.org/wp-content/uploads/2020/11/lorawan_regional_parameters_v1.0.3reva_0.pdf
      */
-    RDEF(CN, 470.0f, 510.0f, 100, 0, 19, true, false, false),
+    RDEF(CN, 470.0f, 510.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        19
+        #endif
+        , true, false, false),
 
     /*
         https://lora-alliance.org/wp-content/uploads/2020/11/lorawan_regional_parameters_v1.0.3reva_0.pdf
         https://www.arib.or.jp/english/html/overview/doc/5-STD-T108v1_5-E1.pdf
         https://qiita.com/ammo0613/items/d952154f1195b64dc29f
      */
-    RDEF(JP, 920.5f, 923.5f, 100, 0, 13, true, false, false),
+    RDEF(JP, 920.5f, 923.5f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        13
+        #endif
+        , true, false, false),
 
     /*
         https://www.iot.org.au/wp/wp-content/uploads/2016/12/IoTSpectrumFactSheet.pdf
         https://iotalliance.org.nz/wp-content/uploads/sites/4/2019/05/IoT-Spectrum-in-NZ-Briefing-Paper.pdf
         Also used in Brazil.
      */
-    RDEF(ANZ, 915.0f, 928.0f, 100, 0, 30, true, false, false),
+    RDEF(ANZ, 915.0f, 928.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false),
 
     /*
         433.05 - 434.79 MHz, 25mW EIRP max, No duty cycle restrictions
         AU Low Interference Potential https://www.acma.gov.au/licences/low-interference-potential-devices-lipd-class-licence
         NZ General User Radio Licence for Short Range Devices https://gazette.govt.nz/notice/id/2022-go3100
      */
-    RDEF(ANZ_433, 433.05f, 434.79f, 100, 0, 14, true, false, false),
+    RDEF(ANZ_433, 433.05f, 434.79f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        14
+        #endif
+        , true, false, false),
 
     /*
         https://digital.gov.ru/uploaded/files/prilozhenie-12-k-reshenyu-gkrch-18-46-03-1.pdf
@@ -101,13 +143,25 @@ const RegionInfo regions[] = {
         Note:
             - We do LBT, so 100% is allowed.
      */
-    RDEF(RU, 868.7f, 869.2f, 100, 0, 20, true, false, false),
+    RDEF(RU, 868.7f, 869.2f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        20
+        #endif
+        , true, false, false),
 
     /*
         https://www.law.go.kr/LSW/admRulLsInfoP.do?admRulId=53943&efYd=0
         https://resources.lora-alliance.org/technical-specifications/rp002-1-0-4-regional-parameters
      */
-    RDEF(KR, 920.0f, 923.0f, 100, 0, 23, true, false, false),
+    RDEF(KR, 920.0f, 923.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        23
+        #endif
+        , true, false, false),
 
     /*
         Taiwan, 920-925Mhz, limited to 0.5W indoor or coastal, 1.0W outdoor.
@@ -115,42 +169,84 @@ const RegionInfo regions[] = {
         https://www.ncc.gov.tw/english/files/23070/102_5190_230703_1_doc_C.PDF
         https://gazette.nat.gov.tw/egFront/e_detail.do?metaid=147283
      */
-    RDEF(TW, 920.0f, 925.0f, 100, 0, 27, true, false, false),
+    RDEF(TW, 920.0f, 925.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        27
+        #endif
+        , true, false, false),
 
     /*
         https://lora-alliance.org/wp-content/uploads/2020/11/lorawan_regional_parameters_v1.0.3reva_0.pdf
      */
-    RDEF(IN, 865.0f, 867.0f, 100, 0, 30, true, false, false),
+    RDEF(IN, 865.0f, 867.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false),
 
     /*
          https://rrf.rsm.govt.nz/smart-web/smart/page/-smart/domain/licence/LicenceSummary.wdk?id=219752
          https://iotalliance.org.nz/wp-content/uploads/sites/4/2019/05/IoT-Spectrum-in-NZ-Briefing-Paper.pdf
       */
-    RDEF(NZ_865, 864.0f, 868.0f, 100, 0, 36, true, false, false),
+    RDEF(NZ_865, 864.0f, 868.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        36
+        #endif
+        , true, false, false),
 
     /*
        https://lora-alliance.org/wp-content/uploads/2020/11/lorawan_regional_parameters_v1.0.3reva_0.pdf
     */
-    RDEF(TH, 920.0f, 925.0f, 100, 0, 16, true, false, false),
+    RDEF(TH, 920.0f, 925.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        16
+        #endif
+        , true, false, false),
 
     /*
         433,05-434,7 Mhz 10 mW
         https://nkrzi.gov.ua/images/upload/256/5810/PDF_UUZ_19_01_2016.pdf
     */
-    RDEF(UA_433, 433.0f, 434.7f, 10, 0, 10, true, false, false),
+    RDEF(UA_433, 433.0f, 434.7f, 10, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        10
+        #endif
+        , true, false, false),
 
     /*
         868,0-868,6 Mhz 25 mW
         https://nkrzi.gov.ua/images/upload/256/5810/PDF_UUZ_19_01_2016.pdf
     */
-    RDEF(UA_868, 868.0f, 868.6f, 1, 0, 14, true, false, false),
+    RDEF(UA_868, 868.0f, 868.6f, 1, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        14
+        #endif
+        , true, false, false),
 
     /*
         Malaysia
         433 - 435 MHz at 100mW, no restrictions.
         https://www.mcmc.gov.my/skmmgovmy/media/General/pdf/Short-Range-Devices-Specification.pdf
     */
-    RDEF(MY_433, 433.0f, 435.0f, 100, 0, 20, true, false, false),
+    RDEF(MY_433, 433.0f, 435.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        20
+        #endif
+        , true, false, false),
 
     /*
         Malaysia
@@ -159,14 +255,26 @@ const RegionInfo regions[] = {
         Frequency hopping is used for 919 - 923 MHz.
         https://www.mcmc.gov.my/skmmgovmy/media/General/pdf/Short-Range-Devices-Specification.pdf
     */
-    RDEF(MY_919, 919.0f, 924.0f, 100, 0, 27, true, true, false),
+    RDEF(MY_919, 919.0f, 924.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        27
+        #endif
+        , true, true, false),
 
     /*
         Singapore
         SG_923 Band 30d: 917 - 925 MHz at 100mW, no restrictions.
         https://www.imda.gov.sg/-/media/imda/files/regulation-licensing-and-consultations/ict-standards/telecommunication-standards/radio-comms/imdatssrd.pdf
     */
-    RDEF(SG_923, 917.0f, 925.0f, 100, 0, 20, true, false, false),
+    RDEF(SG_923, 917.0f, 925.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        20
+        #endif
+        , true, false, false),
 
     /*
         Philippines
@@ -176,8 +284,20 @@ const RegionInfo regions[] = {
                 https://github.com/meshtastic/firmware/issues/4948#issuecomment-2394926135
     */
 
-    RDEF(PH_433, 433.0f, 434.7f, 100, 0, 10, true, false, false), RDEF(PH_868, 868.0f, 869.4f, 100, 0, 14, true, false, false),
-    RDEF(PH_915, 915.0f, 918.0f, 100, 0, 24, true, false, false),
+    RDEF(PH_433, 433.0f, 434.7f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+    10
+        #endif
+        , true, false, false), RDEF(PH_868, 868.0f, 869.4f, 100, 0, 14, true, false, false),
+    RDEF(PH_915, 915.0f, 918.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        24
+        #endif
+        , true, false, false),
 
     /*
         Kazakhstan
@@ -185,32 +305,68 @@ const RegionInfo regions[] = {
                 863 - 868 MHz <25 mW EIRP, 500kHz channels allowed, must not be used at airfields
                                 https://github.com/meshtastic/firmware/issues/7204
     */
-    RDEF(KZ_433, 433.075f, 434.775f, 100, 0, 10, true, false, false),
-    RDEF(KZ_863, 863.0f, 868.0f, 100, 0, 30, true, false, false),
+    RDEF(KZ_433, 433.075f, 434.775f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        10
+        #endif
+        , true, false, false),
+    RDEF(KZ_863, 863.0f, 868.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false),
 
     /*
         Nepal
         865 MHz to 868 MHz frequency band for IoT (Internet of Things), M2M (Machine-to-Machine), and smart metering use,
        specifically in non-cellular mode. https://www.nta.gov.np/uploads/contents/Radio-Frequency-Policy-2080-English.pdf
     */
-    RDEF(NP_865, 865.0f, 868.0f, 100, 0, 30, true, false, false),
+    RDEF(NP_865, 865.0f, 868.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false),
 
     /*
         Brazil
         902 - 907.5 MHz , 1W power limit, no duty cycle restrictions
         https://github.com/meshtastic/firmware/issues/3741
     */
-    RDEF(BR_902, 902.0f, 907.5f, 100, 0, 30, true, false, false),
+    RDEF(BR_902, 902.0f, 907.5f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false),
 
     /*
        2.4 GHZ WLAN Band equivalent. Only for SX128x chips.
     */
-    RDEF(LORA_24, 2400.0f, 2483.5f, 100, 0, 10, true, false, true),
+    RDEF(LORA_24, 2400.0f, 2483.5f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        10
+        #endif
+        , true, false, true),
 
     /*
         This needs to be last. Same as US.
     */
-    RDEF(UNSET, 902.0f, 928.0f, 100, 0, 30, true, false, false)
+    RDEF(UNSET, 902.0f, 928.0f, 100, 0,  
+        #ifdef SETTING_MAX_POWER
+        SETTING_MAX_POWER
+        #else
+        30
+        #endif
+        , true, false, false)
 
 };
 
