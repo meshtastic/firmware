46c1230c39bfdf1919715effb251558f21cc1176
Manuel <71137295+mverch67@users.noreply.github.com>
2026-04-13 15:29:47 +0200

    remove I2C0, add GPS

--- variants/esp32p4/crowpanel-advanced-p4/variant.h
+++ variants/esp32p4/crowpanel-advanced-p4/variant.h
@@ -1,7 +1,4 @@
-// I2C pins for touch screen or ext connector
-#define I2C_SDA 7
-#define I2C_SCL 8
-
+#define HAS_WIRE 0
 #define I2C_SDA1 45
 #define I2C_SCL1 46
 
@@ -9,6 +6,13 @@
 #define WAKE_ON_TOUCH
 #define SCREEN_TOUCH_INT 42
 #define SLEEP_TIME 180
+
+#if defined(CROWPANEL_ADV_P4_50)
+
+// use UART3-IN for GPS (UART1 can not work with lora)
+#define GPS_DEFAULT_NOT_PRESENT 1
+#define GPS_RX_PIN 28
+#define GPS_TX_PIN 27
 
 // LoRa
 #define USE_SX1262
@@ -24,3 +28,27 @@
 #define SX126X_RESET LORA_RESET
 #define SX126X_DIO2_AS_RF_SWITCH
 #define SX126X_DIO3_TCXO_VOLTAGE 3.3
+
+#elif defined(CROWPANEL_ADV_P4_101)
+
+// use UART1 for GPS
+#define GPS_DEFAULT_NOT_PRESENT 1
+#define GPS_RX_PIN 48
+#define GPS_TX_PIN 47
+
+// LoRa
+#define USE_SX1262
+#define LORA_SCK 8
+#define LORA_MISO 7
+#define LORA_MOSI 6
+#define LORA_CS 10
+#define LORA_RESET 54
+
+#define SX126X_CS LORA_CS
+#define SX126X_DIO1 53
+#define SX126X_BUSY 9
+#define SX126X_RESET LORA_RESET
+#define SX126X_DIO2_AS_RF_SWITCH
+#define SX126X_DIO3_TCXO_VOLTAGE 3.3
+
+#endif
