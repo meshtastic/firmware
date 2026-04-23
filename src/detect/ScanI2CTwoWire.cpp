f796e7b5c3da381cea7e243886f7e2946c25ba59
Masafumi Okada <mf@okadajp.org>
2026-04-23 20:36:38 +0900

    Revert "Avoid overwriting SHTXX address during I2C scan"
    This reverts commit 8d8ede4c576e4e6fe8f0e46c970e4bd262deaec8.

--- src/detect/ScanI2CTwoWire.cpp
+++ src/detect/ScanI2CTwoWire.cpp
@@ -511,18 +511,7 @@ void ScanI2CTwoWire::scanPort(I2CPort port, uint8_t *address, uint8_t asize)
 
                 break;
 
-            case SHTC3_ADDR: {
-                auto foundSHTXX = deviceAddresses.find(SHTXX);
-                if (foundSHTXX != deviceAddresses.end() &&
-                    (foundSHTXX->second.address == SHTXX_ADDR || foundSHTXX->second.address == SHTXX_ADDR_ALT)) {
-                    LOG_DEBUG("Ignore SHTXX at address 0x%x because SHTXX was already found at 0x%x", (uint8_t)addr.address,
-                              foundSHTXX->second.address);
-                } else {
-                    logFoundDevice("SHTXX", (uint8_t)addr.address);
-                    type = SHTXX;
-                }
-                break;
-            }
+                SCAN_SIMPLE_CASE(SHTC3_ADDR, SHTXX, "SHTXX", (uint8_t)addr.address)
             case RCWL9620_ADDR:
                 // get MAX30102 PARTID
                 registerValue = getRegisterValue(ScanI2CTwoWire::RegisterLocation(addr, 0xFF), 1);
