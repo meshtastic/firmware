6f35f8524f03b836c7b616dea34ef1812fc8fadd
jessm33 <root@example.com>
2026-05-06 14:58:31 -0400

    Refactor meshtasticd service files and update installation scripts for improved instance handling

--- bin/meshtasticd-start.sh
+++ bin/meshtasticd-start.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env sh
 
 INSTANCE=$1
-CONF_DIR="/etc/meshtasticd/config.d"
-VFS_DIR="/var/lib"
+CONF_DIR="/etc/meshtasticd/"
+VFS_DIR="/var/lib/meshtasticd/${INSTANCE}"
+OLD_VFS_DIR="/var/lib/meshtasticd-${INSTANCE}"
 
 # If no instance ID provided, start bare daemon and exit
-echo "no instance ID provided, starting bare meshtasticd service"
 if [ -z "${INSTANCE}" ]; then
+  echo "no instance ID provided, starting bare meshtasticd service"
   /usr/bin/meshtasticd
   exit 0
 fi
 
+if [ -d "${OLD_VFS_DIR}" ]; then
+  VFS_DIR="${OLD_VFS_DIR}"
+  echo "found old vfs dir for ${INSTANCE} at ${OLD_VFS_DIR}, using it as vfs dir for ${INSTANCE}"
+fi
+
 # Make VFS dir if it does not exist
-if [ ! -d "${VFS_DIR}/meshtasticd-${INSTANCE}" ]; then
+if [ ! -d "${VFS_DIR}" ]; then
   echo "vfs for ${INSTANCE} does not exist, creating it."
-  mkdir "${VFS_DIR}/meshtasticd-${INSTANCE}"
+  mkdir "${VFS_DIR}"
 fi
 
 # Abort if config for $INSTANCE does not exist
@@ -25,7 +31,7 @@ fi
 
 # Start meshtasticd with instance parameters
 printf "starting meshtasticd-%s..., ${INSTANCE}"
-if /usr/bin/meshtasticd --config="${CONF_DIR}/config-${INSTANCE}.yaml" --fsdir="${VFS_DIR}/meshtasticd-${INSTANCE}"; then
+if /usr/bin/meshtasticd --config="${CONF_DIR}/config-${INSTANCE}.yaml" --fsdir="${VFS_DIR}"; then
   echo "ok"
 else
   echo "failed"
