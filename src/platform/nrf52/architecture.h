40e191ac58c5d0b1c71a6c92c6d494c7a9a6fa3a
Thomas Göttgens <tgoettgens@gmail.com>
2026-05-19 08:10:09 +0200

    enable proper device model

--- src/platform/nrf52/architecture.h
+++ src/platform/nrf52/architecture.h
@@ -88,8 +88,7 @@
 #elif defined(TTGO_T_ECHO_PLUS)
 #define HW_VENDOR meshtastic_HardwareModel_T_ECHO_PLUS
 #elif defined(T_IMPULSE_PLUS)
-// #define HW_VENDOR meshtastic_HardwareModel_T_IMPULSE_PLUS
-#define HW_VENDOR meshtastic_HardwareModel_PRIVATE_HW
+#define HW_VENDOR meshtastic_HardwareModel_T_IMPULSE_PLUS
 #elif defined(ELECROW_ThinkNode_M1)
 #define HW_VENDOR meshtastic_HardwareModel_THINKNODE_M1
 #elif defined(ELECROW_ThinkNode_M3)
