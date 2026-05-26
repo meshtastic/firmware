712a34b021c0db93a8dba1224aec75722ace218c
Estlin Howland <estlinhowland@gmail.com>
2026-05-27 09:27:24 +1200

    First Commit

--- src/input/TCA8418Keyboard.cpp
+++ src/input/TCA8418Keyboard.cpp
@@ -1,4 +1,5 @@
 #include "TCA8418Keyboard.h"
+#include "modules/CannedMessageModule.h"
 
 #define _TCA8418_COLS 3
 #define _TCA8418_ROWS 4
@@ -10,36 +11,52 @@
 using Key = TCA8418KeyboardBase::TCA8418Key;
 
 // Num chars per key, Modulus for rotating through characters
-static uint8_t TCA8418TapMod[_TCA8418_NUM_KEYS] = {13, 7, 7, 7, 7, 7, 9, 7, 9, 2, 2, 2};
+static uint8_t TCA8418TapMod[_TCA8418_NUM_KEYS] = {13, 7, 7, 7, 7, 7, 9, 7, 9, 1, 2, 4};
 
 static unsigned char TCA8418TapMap[_TCA8418_NUM_KEYS][13] = {
     {'1', '.', ',', '?', '!', ':', ';', '-', '_', '\\', '/', '(', ')'}, // 1
-    {'2', 'a', 'b', 'c', 'A', 'B', 'C'},                                // 2
-    {'3', 'd', 'e', 'f', 'D', 'E', 'F'},                                // 3
-    {'4', 'g', 'h', 'i', 'G', 'H', 'I'},                                // 4
-    {'5', 'j', 'k', 'l', 'J', 'K', 'L'},                                // 5
-    {'6', 'm', 'n', 'o', 'M', 'N', 'O'},                                // 6
-    {'7', 'p', 'q', 'r', 's', 'P', 'Q', 'R', 'S'},                      // 7
-    {'8', 't', 'u', 'v', 'T', 'U', 'V'},                                // 8
-    {'9', 'w', 'x', 'y', 'z', 'W', 'X', 'Y', 'Z'},                      // 9
-    {'*', '+'},                                                         // *
-    {'0', ' '},                                                         // 0
-    {'#', '@'},                                                         // #
+    {'a', 'b', 'c', '2', 'A', 'B', 'C'},                                // 2
+    {'d', 'e', 'f', '3', 'D', 'E', 'F'},                                // 3
+    {'g', 'h', 'i', '4', 'G', 'H', 'I'},                                // 4
+    {'j', 'k', 'l', '5', 'J', 'K', 'L'},                                // 5
+    {'m', 'n', 'o', '6', 'M', 'N', 'O'},                                // 6
+    {'p', 'q', 'r', 's', '7', 'P', 'Q', 'R', 'S'},                      // 7
+    {'t', 'u', 'v', '8', 'T', 'U', 'V'},                                // 8
+    {'w', 'x', 'y', 'z', '9', 'W', 'X', 'Y', 'Z'},                      // 9
+    {Key::BSP},                                                         // *
+    {' ', '0'},                                                         // 0
+    {'#', '@', '*', '+'},                                               // #
 };
 
 static unsigned char TCA8418LongPressMap[_TCA8418_NUM_KEYS] = {
     Key::ESC,   // 1
     Key::UP,    // 2
-    Key::NONE,  // 3
+    Key::TAB,  // 3
     Key::LEFT,  // 4
-    Key::NONE,  // 5
+    Key::SELECT,  // 5
     Key::RIGHT, // 6
     Key::NONE,  // 7
     Key::DOWN,  // 8
     Key::NONE,  // 9
     Key::BSP,   // *
-    Key::NONE,  // 0
-    Key::NONE,  // #
+    Key::NONE,   // 0
+    Key::MUTE_TOGGLE  // #
+};
+
+// key assignment when not in a free text entering state
+static unsigned char TCA8418NavMap[_TCA8418_NUM_KEYS] = {
+    Key::ESC,   // 1
+    Key::UP,    // 2
+    Key::NONE,  // 3
+    Key::LEFT,  // 4
+    Key::SELECT,  // 5
+    Key::RIGHT, // 6
+    Key::NONE,  // 7
+    Key::DOWN,  // 8
+    Key::NONE,  // 9
+    Key::BT_TOGGLE,   // *
+    Key::GPS_TOGGLE,  // 0
+    Key::MUTE_TOGGLE  // #
 };
 
 TCA8418Keyboard::TCA8418Keyboard()
@@ -110,19 +127,27 @@ void TCA8418Keyboard::released()
         state = Idle;
         return;
     }
+
     uint32_t now = millis();
     int32_t held_interval = now - last_tap;
     last_tap = now;
-    if (tap_interval < _TCA8418_MULTI_TAP_THRESHOLD && should_backspace) {
-        queueEvent(BSP);
-    }
-    if (held_interval > _TCA8418_LONG_PRESS_THRESHOLD) {
-        queueEvent(TCA8418LongPressMap[last_key]);
-        // LOG_DEBUG("Long Press Key: %i Map: %i", last_key, TCA8418LongPressMap[last_key]);
+
+
+    if (cannedMessageModule && cannedMessageModule->isCharInputAllowed()) {
+        if (tap_interval < _TCA8418_MULTI_TAP_THRESHOLD && should_backspace && TCA8418TapMap[last_key][(char_idx % TCA8418TapMod[last_key])] != Key::BSP) {
+            queueEvent(BSP);
+        } 
+        if (held_interval > _TCA8418_LONG_PRESS_THRESHOLD) {
+            queueEvent(TCA8418LongPressMap[last_key]);
+            // LOG_DEBUG("Long Press Key: %i Map: %i", last_key, TCA8418LongPressMap[last_key]);
+        } else {
+            queueEvent(TCA8418TapMap[last_key][(char_idx % TCA8418TapMod[last_key])]);
+            // LOG_DEBUG("Key Press: %i Index:%i if %i Map: %c", last_key, char_idx, TCA8418TapMod[last_key],
+            //           TCA8418TapMap[last_key][(char_idx % TCA8418TapMod[last_key])]);
+        }
     } else {
-        queueEvent(TCA8418TapMap[last_key][(char_idx % TCA8418TapMod[last_key])]);
-        // LOG_DEBUG("Key Press: %i Index:%i if %i Map: %c", last_key, char_idx, TCA8418TapMod[last_key],
-        //           TCA8418TapMap[last_key][(char_idx % TCA8418TapMod[last_key])]);
+        queueEvent(TCA8418NavMap[last_key]);
+        // LOG_DEBUG("Long Press Key: %i Map: %i", last_key, TCA8418LongPressMap[last_key]);
     }
 }
 
