28dbb7cbe7bb0114271ca9e5bafedc27f8645db2
oscgonfer <oscgonfer@gmail.com>
2026-06-14 14:12:49 +0200

    Low prio debug fixes

--- src/modules/Telemetry/Sensor/SEN5XSensor.cpp
+++ src/modules/Telemetry/Sensor/SEN5XSensor.cpp
@@ -308,7 +308,7 @@ bool SEN5XSensor::vocStateToSensor()
     }
 
     if (!sendCommand(SEN5X_STOP_MEASUREMENT)) {
-        LOG_ERROR("%s: Error stoping measurement", sensorName);
+        LOG_ERROR("%s: Error stopping measurement", sensorName);
         return false;
     }
     delay(200); // From Sensirion Datasheet
@@ -500,7 +500,7 @@ bool SEN5XSensor::startCleaning()
 
     // Note that cleaning command can only be run when the sensor is in measurement mode
     if (!sendCommand(SEN5X_START_MEASUREMENT)) {
-        LOG_ERROR("%s: Error starting measurment mode", sensorName);
+        LOG_ERROR("%s: Error starting measurement mode", sensorName);
         return false;
     }
     delay(50); // From Sensirion Datasheet
