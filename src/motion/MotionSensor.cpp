93a2b6d8e958d6d2047800ff1e0b6c572df76711
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2026-06-20 15:59:55 -0700

    fix: include NodeDB for motion config access

--- src/motion/MotionSensor.cpp
+++ src/motion/MotionSensor.cpp
@@ -3,8 +3,8 @@
 #include "SPILock.h"
 #include "SafeFile.h"
 #include "concurrency/LockGuard.h"
-#include "configuration.h"
 #include "graphics/draw/CompassRenderer.h"
+#include "mesh/NodeDB.h"
 
 #if !defined(ARCH_STM32WL) && !MESHTASTIC_EXCLUDE_I2C
 
