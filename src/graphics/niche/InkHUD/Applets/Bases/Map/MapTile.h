9d2c1559e268f4d50fb2a5f48546937891290549
HarukiToreda <Skiper20002000@gmail.com>
2026-07-04 23:57:38 -0400

    compression improvement

--- src/graphics/niche/InkHUD/Applets/Bases/Map/MapTile.h
+++ src/graphics/niche/InkHUD/Applets/Bases/Map/MapTile.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <stdint.h>
 
-static const int map_tile_count = 0;
-static const int map_tile_zooms[] = {};
-static const int map_tile_tx[] = {};
-static const int map_tile_ty[] = {};
-static const int map_tile_sizes[] = {};
-static const uint8_t *map_tile_data[] = {};
+static const uint8_t map_tile_layout = 0;
+static const uint8_t map_tile_grid_cols = 0;
+static const uint8_t map_tile_grid_rows = 0;
+static const uint8_t map_tile_block_count = 0;
+static const uint8_t map_tile_count = 0;
+static const uint8_t map_tile_zooms[] = {};
+static const uint16_t map_tile_tx[] = {};
+static const uint16_t map_tile_ty[] = {};
+static const uint8_t map_tile_block_zooms[] = {};
+static const uint16_t map_tile_block_tx[] = {};
+static const uint16_t map_tile_block_ty[] = {};
+static const uint8_t map_tile_kinds[] = {};
+static const uint16_t map_tile_sizes[] = {};
+static const uint32_t map_tile_offsets[] = {};
+static const uint8_t map_tile_data[] = {};
