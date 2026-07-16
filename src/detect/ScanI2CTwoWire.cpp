29905d6467988fdfb89e223c31ec44be1331c4ac
Jorropo <jorropo.pgm@gmail.com>
2026-07-16 23:31:12 +0200

    ScanI2C: mark intentional fall-through to default probe

--- src/detect/ScanI2CTwoWire.cpp
+++ src/detect/ScanI2CTwoWire.cpp
@@ -416,6 +416,7 @@ void ScanI2CTwoWire::scanPort(I2CPort port, uint8_t *address, uint8_t asize)
                     if (type == DPS310 || type == SPA06) {
                         break;
                     }
+                    [[fallthrough]];
                 default:
                     registerValue = getRegisterValue(ScanI2CTwoWire::RegisterLocation(addr, 0x00), 1); // GET_ID
                     switch (registerValue) {
