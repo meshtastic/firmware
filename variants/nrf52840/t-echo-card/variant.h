4036cf564fc082c51e59063d8143b97493c572db
Clive Blackledge <clive@ansible.org>
2026-07-26 22:25:10 -0700

    fix(t-echo-card): correct GNSS power controls

--- variants/nrf52840/t-echo-card/variant.h
+++ variants/nrf52840/t-echo-card/variant.h
@@ -138,12 +138,13 @@ static const uint8_t A0 = PIN_A0;
 #define HAS_GPS 1
 
 #define PIN_GPS_EN (32 + 15) // GPS_EN: P1.15 - GPS power enable
-#define GPS_EN_ACTIVE 1
+#define GPS_EN_ACTIVE LOW
 #define PIN_GPS_STANDBY (0 + 25) // GPS_WAKE_UP: P0.25 - wakeup pin
 #define PIN_GPS_PPS (0 + 23)     // GPS_1PPS: P0.23
 #define GPS_RX_PIN (0 + 19)      // MCU RX ← GPS's TX (vendor GPS_UART_TX / P0.19)
 #define GPS_TX_PIN (0 + 21)      // MCU TX → GPS's RX (vendor GPS_UART_RX / P0.21)
-#define PIN_GPS_RESET (0 + 29)   // GPS_RF_EN: GPS RF enable / reset
+#define PIN_GPS_RESET (0 + 29)   // GPS_RF_EN: P0.29 - RF front-end shutdown
+#define GPS_RESET_MODE LOW
 
 #define GPS_THREAD_INTERVAL 50
 
