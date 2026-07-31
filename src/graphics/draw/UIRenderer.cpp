575788ae7bef62e8f7104cb89ad64cf83c98ede2
Jason P <applewiz@mac.com>
2026-07-30 23:54:54 -0500

    Implement Fixes for Meshnology W12's Two Color Display (#11288)
    * Round 1 Fixes
    
    * Update GPS icon
    
    * Update calculations for revised GPS icon after regression testing
    
    * Add BICOLOR_OLED_DISPLAY and actually use it
    
    ---------
    
    Co-authored-by: Austin <vidplace7@gmail.com>

--- src/graphics/draw/UIRenderer.cpp
+++ src/graphics/draw/UIRenderer.cpp
@@ -40,7 +40,7 @@ static bool gBootSplashBoldPass = false;
 
 static inline void drawSatelliteIcon(OLEDDisplay *display, int16_t x, int16_t y)
 {
-    int yOffset = (currentResolution == ScreenResolution::High) ? -5 : 1;
+    int yOffset = (currentResolution == ScreenResolution::High) ? 0 : 1;
     if (currentResolution == ScreenResolution::High) {
         NodeListRenderer::drawScaledXBitmap16x16(x, y + yOffset, imgGPS_width, imgGPS_height, imgGPS, display);
     } else {
@@ -521,9 +521,9 @@ void UIRenderer::drawGps(OLEDDisplay *display, int16_t x, int16_t y, const mesht
 {
     // Draw satellite image
     if (currentResolution == ScreenResolution::High) {
-        NodeListRenderer::drawScaledXBitmap16x16(x, y - 2, imgGPS_width, imgGPS_height, imgGPS, display);
+        NodeListRenderer::drawScaledXBitmap16x16(x, y + 1, imgGPS_width, imgGPS_height, imgGPS, display);
     } else {
-        display->drawXbm(x + 1, y + 1, imgGPS_width, imgGPS_height, imgGPS);
+        display->drawXbm(x + 1, y + 3, imgGPS_width, imgGPS_height, imgGPS);
     }
     char textString[10];
 
@@ -963,7 +963,7 @@ void UIRenderer::drawFavoriteNode(OLEDDisplay *display, OLEDDisplayUiState *stat
             curX += display->getStringWidth(hopCount) + 2;
 
             const int iconY = yPos + (FONT_HEIGHT_SMALL - hop_height) / 2;
-            display->drawXbm(curX, iconY, hop_width, hop_height, hop);
+            display->drawXbm(curX, iconY, hop_width, hop_height, imghop);
             curX += hop_width + 1;
         }
     }
@@ -1195,7 +1195,8 @@ void UIRenderer::drawDeviceFocused(OLEDDisplay *display, OLEDDisplayUiState *sta
         } else {
             displayLine = config.position.gps_mode == meshtastic_Config_PositionConfig_GpsMode_NOT_PRESENT ? "No GPS" : "GPS off";
         }
-        drawSatelliteIcon(display, x, getTextPositions(display)[line]);
+        int yOffset = (currentResolution == ScreenResolution::High) ? 0 : 2;
+        drawSatelliteIcon(display, x, getTextPositions(display)[line] + yOffset);
         int xOffset = (currentResolution == ScreenResolution::High) ? 6 : 0;
         display->drawString(x + 11 + xOffset, getTextPositions(display)[line], displayLine);
     } else {
@@ -1586,7 +1587,8 @@ void UIRenderer::drawCompassAndLocationScreen(OLEDDisplay *display, OLEDDisplayU
         } else {
             displayLine = config.position.gps_mode == meshtastic_Config_PositionConfig_GpsMode_NOT_PRESENT ? "No GPS" : "GPS off";
         }
-        drawSatelliteIcon(display, x, textPos[line]);
+        int yOffset = (currentResolution == ScreenResolution::High) ? 1 : 3;
+        drawSatelliteIcon(display, x, textPos[line] + yOffset);
         int xOffset = (currentResolution == ScreenResolution::High) ? 6 : 0;
         display->drawString(x + 11 + xOffset, textPos[line++], displayLine);
     } else {
