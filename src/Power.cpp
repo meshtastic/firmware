e584c54ccae948c90969d904e98df1d1c845358e
Ilya Esin <iyesin@users.noreply.github.com>
2026-08-01 18:23:00 +0200

    fix(heltec-v1): bring the board build back to life

--- src/Power.cpp
+++ src/Power.cpp
@@ -89,10 +89,10 @@
 #if defined(BATTERY_PIN) && defined(ARCH_ESP32)
 
 #ifndef BAT_MEASURE_ADC_UNIT // ADC1 is default
-static const adc_channel_t adc_channel = ADC_CHANNEL;
+static const adc_channel_t adc_channel = static_cast<adc_channel_t>(ADC_CHANNEL);
 static const adc_unit_t unit = ADC_UNIT_1;
 #else  // ADC2
-static const adc_channel_t adc_channel = ADC_CHANNEL;
+static const adc_channel_t adc_channel = static_cast<adc_channel_t>(ADC_CHANNEL);
 static const adc_unit_t unit = ADC_UNIT_2;
 #endif // BAT_MEASURE_ADC_UNIT
 
