21685fbcefe01b90dfbf8c1c65702dfa990a8587
Tadayoshi MIURA <11958457+t-miura@users.noreply.github.com>
2026-09-04 19:19:16 +0900

    add clean-up legacy arduino-pico packages for rp2040/rp2350

--- bin/build-rp2xx0.sh
+++ bin/build-rp2xx0.sh
@@ -11,6 +11,9 @@ OUTDIR=release
 rm -f $OUTDIR/firmware*
 rm -r $OUTDIR/* || true
 
+# Clean up legacy package from the base CI container if present
+rm -rf "${PLATFORMIO_CORE_DIR:-$HOME/.platformio}/packages/arduino-pico"
+
 # Important to pull latest version of libs into all device flavors, otherwise some devices might be stale
 platformio pkg install -e $1
 
