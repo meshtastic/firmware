abecf7d9ca37b00ddeeaa24508c9b4c422cb5408
Jason P <applewiz@mac.com>
2026-09-08 14:56:31 -0500

    Revert "Hide Navigation Bar on EInk Shutdown"
    This reverts commit 744812555b826496f9b8ae7a44d0d5aac1bde1a9.

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -1324,10 +1324,6 @@ void Screen::setScreensaverFrames(FrameCallback einkScreensaver)
     if (einkScreensaver != NULL) {
         screensaverFrame = einkScreensaver;
         ui->setFrames(&screensaverFrame, 1);
-
-        // Drop the nav bar before the sleep / shutdown screen is rendered
-        static OverlayCallback screensaverOverlays[] = {NotificationRenderer::drawBannercallback};
-        ui->setOverlays(screensaverOverlays, 1);
     }
 
     // Else, display the usual "overlay" screensaver
